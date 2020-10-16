60f8f4c5e04ad5961db99026be003c73c36655e4
Jan Six <six.jan@gmail.com>
2020-10-16 16:04:26 +0200

    fix nested tokens, white tokens

--- src/app/components/TokenButton.tsx
+++ src/app/components/TokenButton.tsx
@@ -4,6 +4,41 @@ import MoreButton from './MoreButton';
 import {useTokenState} from '../store/TokenContext';
 import Icon from './Icon';
 
+function lightOrDark(color) {
+    // Variables for red, green, blue values
+    let r;
+    let g;
+    let b;
+    let hsp;
+
+    // Check the format of the color, HEX or RGB?
+    if (color.match(/^rgb/)) {
+        // If RGB --> store the red, green, blue values in separate variables
+        color = color.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+(?:\.\d+)?))?\)$/);
+
+        r = color[1];
+        g = color[2];
+        b = color[3];
+    } else {
+        // If hex --> Convert it to RGB: http://gist.github.com/983661
+        color = +`0x${color.slice(1).replace(color.length < 5 && /./g, '$&$&')}`;
+
+        r = color >> 16;
+        g = (color >> 8) & 255;
+        b = color & 255;
+    }
+
+    // HSP (Highly Sensitive Poo) equation from http://alienryderflex.com/hsp.html
+    hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
+
+    // Using the HSP value, determine whether the color is light or dark
+    if (hsp > 245.5) {
+        return 'light';
+    }
+
+    return 'dark';
+}
+
 function colorByHashCode(value) {
     let hash = 0;
     if (value.length === 0) return hash;
@@ -121,6 +156,7 @@ const TokenButton = ({type, name, path, token, disabled, editMode, showForm}) =>
             ];
             style = {
                 '--backgroundColor': displayValue,
+                '--borderColor': lightOrDark(displayValue) === 'light' ? '#f5f5f5' : 'white',
             };
             buttonClass.push('button-property-color');
             if (state.displayType === 'LIST') {
