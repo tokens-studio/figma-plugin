080e7228ea78a1b293180f5b259d03c2d5407c86
Jan Six <six.jan@gmail.com>
2021-03-20 13:07:39 +0100

    add date to lastopened

--- src/utils/getLastOpened.ts
+++ src/utils/getLastOpened.ts
@@ -1,12 +1,13 @@
 export default async function getLastOpened(): Promise<Date> {
     let data;
     try {
-        data = (await figma.clientStorage.getAsync('lastOpened')) || Date.now();
+        // Set specific date as lastOpened if none existed (when we started the changelog)
+        data = (await figma.clientStorage.getAsync('lastOpened')) || 1616241985291;
         figma.clientStorage.setAsync('lastOpened', Date.now());
     } catch (e) {
         console.error('error retrieving lastOpened', e);
         figma.clientStorage.setAsync('lastOpened', Date.now());
-        data = Date.now();
+        data = 1616241985291;
     }
     return data;
 }
