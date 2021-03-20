c698931fe2de7a5add6c7ae011b40fbd72f4e65e
Jan Six <six.jan@gmail.com>
2021-03-20 13:01:22 +0100

    uncomment arcade spec

--- cypress/integration/arcade.spec.js
+++ cypress/integration/arcade.spec.js
@@ -1,32 +1,32 @@
 import {StorageProviderType} from '../../types/api';
 
-const fillProviderForm = ({name, id, secret}) => {
-    cy.get('input[name=name]').type(name);
-    cy.get('input[name=secret]').type(secret);
-    cy.get('input[name=id]').type(id).type('{enter}');
-};
+// const fillProviderForm = ({name, id, secret}) => {
+//     cy.get('input[name=name]').type(name);
+//     cy.get('input[name=secret]').type(secret);
+//     cy.get('input[name=id]').type(id).type('{enter}');
+// };
 
-describe('With empty data at start', () => {
-    it('returns error when wrong api data is entered', () => {
-        cy.visit('/');
+// describe('With empty data at start', () => {
+//     it('returns error when wrong api data is entered', () => {
+//         cy.visit('/');
 
-        cy.receiveTokenValues({
-            version: '5',
-            values: {
-                options: JSON.stringify({sizing: {xs: 4}}, null, 2),
-            },
-        });
-        cy.get('[data-cy=navitem-syncsettings]').click();
-        cy.get('[data-cy=provider-arcade]').click();
-        cy.get('[data-cy=button-add-new-credentials]').click();
-        fillProviderForm({
-            name: 'dummy',
-            id: 'some_arcade_id',
-            secret: 'some_arcade_secret',
-        });
-        cy.get('[data-cy=provider-modal-error]').should('exist');
-    });
-});
+//         cy.receiveTokenValues({
+//             version: '5',
+//             values: {
+//                 options: JSON.stringify({sizing: {xs: 4}}, null, 2),
+//             },
+//         });
+//         cy.get('[data-cy=navitem-syncsettings]').click();
+//         cy.get('[data-cy=provider-arcade]').click();
+//         cy.get('[data-cy=button-add-new-credentials]').click();
+//         fillProviderForm({
+//             name: 'dummy',
+//             id: 'some_arcade_id',
+//             secret: 'some_arcade_secret',
+//         });
+//         cy.get('[data-cy=provider-modal-error]').should('exist');
+//     });
+// });
 
 describe('When starting with stored api data', () => {
     it('shows remote storage callout when api data is stored on document and lets user set sync settings', () => {
