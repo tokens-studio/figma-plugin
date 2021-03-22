8b44bedf90addabc6b05d1d1cc75df6860a96b1d
Jan Six <six.jan@gmail.com>
2021-03-22 08:55:17 +0100

    fix: broken alias ending with 0

--- src/app/components/TokenData.tsx
+++ src/app/components/TokenData.tsx
@@ -6,7 +6,7 @@ import {notifyToUI} from '../../plugin/notifiers';
 import {SingleToken, TokenGroup, TokenObject, TokenProps, Tokens} from '../../../types/tokens';
 import {convertToRgb, checkAndEvaluateMath} from './utils';
 
-const aliasRegex = /(\$[^\s]+\w[^,. )/])/g;
+const aliasRegex = /(\$[^\s]+\w)/g;
 export default class TokenData {
     mergedTokens: TokenGroup;
 
