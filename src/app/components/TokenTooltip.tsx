ed563d01822115b7a229501c19b128a0ff7dad98
Jan Six <six.jan@gmail.com>
2021-07-02 23:43:45 +0200

    added tooltip catch

--- src/app/components/TokenTooltip.tsx
+++ src/app/components/TokenTooltip.tsx
@@ -2,28 +2,33 @@ import React from 'react';
 
 // Returns token value in display format
 export default function TokenTooltip({token, resolvedTokens, shouldResolve = false}) {
-    const valueToCheck = shouldResolve ? resolvedTokens.find((t) => t.name === token.name).value : token.value;
+    try {
+        const valueToCheck = shouldResolve ? resolvedTokens.find((t) => t.name === token.name).value : token.value;
 
-    if (token.type === 'typography') {
-        if (shouldResolve) {
+        if (token.type === 'typography') {
+            if (shouldResolve) {
+                return (
+                    <div>
+                        {valueToCheck.fontFamily} / {valueToCheck.fontWeight}
+                    </div>
+                );
+            }
             return (
                 <div>
-                    {valueToCheck.fontFamily} / {valueToCheck.fontWeight}
+                    <div>Font: {valueToCheck.fontFamily}</div>
+                    <div>Weight: {valueToCheck.fontWeight}</div>
+                    <div>Leading: {valueToCheck.lineHeight}</div>
+                    <div>Tracking: {valueToCheck.lineHeight}</div>
                 </div>
             );
         }
-        return (
-            <div>
-                <div>Font: {valueToCheck.fontFamily}</div>
-                <div>Weight: {valueToCheck.fontWeight}</div>
-                <div>Leading: {valueToCheck.lineHeight}</div>
-                <div>Tracking: {valueToCheck.lineHeight}</div>
-            </div>
-        );
-    }
-    if (typeof valueToCheck !== 'string' && typeof valueToCheck !== 'number') {
-        return <div>{JSON.stringify(valueToCheck, null, 2)}</div>;
-    }
+        if (typeof valueToCheck !== 'string' && typeof valueToCheck !== 'number') {
+            return <div>{JSON.stringify(valueToCheck, null, 2)}</div>;
+        }
 
-    return <div>{valueToCheck}</div>;
+        return <div>{valueToCheck}</div>;
+    } catch (e) {
+        console.log('Error rendering tooltip', token, e);
+        return null;
+    }
 }
