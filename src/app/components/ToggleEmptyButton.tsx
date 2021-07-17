7d7019c58a784616248daa48a2ab06c72ae71c55
Jan Six <six.jan@gmail.com>
2021-07-17 23:45:18 +0200

    fix styling of toggle empty groups btn

--- src/app/components/ToggleEmptyButton.tsx
+++ src/app/components/ToggleEmptyButton.tsx
@@ -6,7 +6,7 @@ export default function ToggleEmptyButton() {
     const {showEmptyGroups} = useTokenState();
     const {toggleShowEmptyGroups} = useTokenDispatch();
     return (
-        <div className="flex items-center justify-center mt-4">
+        <div className="flex items-center justify-center mt-4 mb-4">
             <Button variant="secondary" size="small" onClick={toggleShowEmptyGroups}>
                 {showEmptyGroups ? 'Hide' : 'Show'} empty groups
             </Button>
