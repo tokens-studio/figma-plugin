faadc7d5e236736df35795fcb2f5c445c361fdb9
Jan Six <six.jan@gmail.com>
2021-07-20 11:11:18 +0200

    remove name from sd output, fixes #167

--- src/utils/formatTokens.ts
+++ src/utils/formatTokens.ts
@@ -3,7 +3,8 @@ import set from 'set-value';
 export default function formatTokens(tokens, activeTokenSet) {
     const tokenObj = {};
     tokens[activeTokenSet].forEach((token) => {
-        set(tokenObj, token.name, token);
+        const {name, ...tokenWithoutName} = token;
+        set(tokenObj, token.name, tokenWithoutName);
     });
 
     return JSON.stringify(tokenObj, null, 2);
