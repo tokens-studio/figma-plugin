21c850e7f0b41c2331350c19e830ecb0b8dc0bbd
Jan Six <six.jan@gmail.com>
2021-07-30 23:57:30 +0200

    Fix default settings when none are available

--- src/utils/uiSettings.ts
+++ src/utils/uiSettings.ts
@@ -1,4 +1,5 @@
 import {notifyUISettings, notifyUI} from '@/plugin/notifiers';
+import {UpdateMode} from 'Types/state';
 
 // update credentials
 export async function updateUISettings(uiSettings) {
@@ -24,11 +25,11 @@ export async function getUISettings() {
             const parsedData = await JSON.parse(data);
             width = parsedData.width || 400;
             height = parsedData.height || 600;
-            updateMode = parsedData.updateMode;
-            updateRemote = parsedData.updateRemote;
-            updateOnChange = parsedData.updateOnChange;
-            updateStyles = parsedData.updateStyles;
-            ignoreFirstPartForStyles = parsedData.ignoreFirstPartForStyles;
+            updateMode = parsedData.updateMode || UpdateMode.PAGE;
+            updateRemote = parsedData.updateRemote || true;
+            updateOnChange = parsedData.updateOnChange || true;
+            updateStyles = parsedData.updateStyles || true;
+            ignoreFirstPartForStyles = parsedData.ignoreFirstPartForStyles || true;
             notifyUISettings({
                 width,
                 height,
