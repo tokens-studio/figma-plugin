f34a8f9875b5d009fc480a19047904ce09044aa7
Jan Six <six.jan@gmail.com>
2021-08-30 21:40:02 +0200

    resolves #227 (#230)

--- src/utils/formatTokens.ts
+++ src/utils/formatTokens.ts
@@ -4,7 +4,17 @@ export default function formatTokens(tokens, activeTokenSet) {
     const tokenObj = {};
     tokens[activeTokenSet].forEach((token) => {
         const {name, ...tokenWithoutName} = token;
-        set(tokenObj, token.name, tokenWithoutName);
+        if (token.type === 'typography') {
+            const expandedTypography = Object.entries(tokenWithoutName.value).reduce((acc, [key, val]) => {
+                acc[key] = {
+                    value: val,
+                };
+                return acc;
+            }, {});
+            set(tokenObj, token.name, {...expandedTypography});
+        } else {
+            set(tokenObj, token.name, tokenWithoutName);
+        }
     });
 
     return JSON.stringify(tokenObj, null, 2);
