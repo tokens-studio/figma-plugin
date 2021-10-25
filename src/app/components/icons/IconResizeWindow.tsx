160f938c0578dd271316b097755396a716893f54
Jan Six <six.jan@gmail.com>
2021-10-25 09:31:45 +0200

    add new resize icon

--- src/app/components/icons/IconResizeWindow.tsx
+++ src/app/components/icons/IconResizeWindow.tsx
@@ -2,9 +2,12 @@ import * as React from 'react';
 
 const IconResizeWindow = () => (
     <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M16 0V16H0L16 0Z" fill="white" />
-        <path d="M6.22577 16H3L16 3V6.22576L6.22577 16Z" fill="#8C8C8C" />
-        <path d="M11.8602 16H8.63441L16 8.63441V11.8602L11.8602 16Z" fill="#8C8C8C" />
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M5.02081 16H3.6066L16 3.6066V5.02081L5.02081 16ZM10.835 16H9.42078L16 9.42078V10.835L10.835 16Z"
+            fill="#8C8C8C"
+        />
     </svg>
 );
 
