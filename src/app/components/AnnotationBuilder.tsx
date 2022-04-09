14ac24909edbc594c6cd36c54594d8d7da56db82
Jan Six <six.jan@gmail.com>
2022-04-10 01:42:41 +0200

    Fix styling issues (#592)
    * fix callout
    
    * fix sync settings and inspect
    
    * fix group heading
    
    * fix context menu

--- src/app/components/AnnotationBuilder.tsx
+++ src/app/components/AnnotationBuilder.tsx
@@ -5,45 +5,48 @@ import { uiStateSelector } from '@/selectors';
 import createAnnotation from './createAnnotation';
 import Stack from './Stack';
 import Text from './Text';
+import Box from './Box';
 
 export default function AnnotationBuilder() {
   const uiState = useSelector(uiStateSelector, isEqual);
 
   return Object.entries(uiState.mainNodeSelectionValues).length > 0 ? (
-    <Stack direction="row" align="center" justify="between">
-      <Text bold>Add as annotation</Text>
-      <Stack direction="row" gap={0}>
-        <button
-          className="p-1 button button-secondary"
-          type="button"
-          onClick={() => createAnnotation(uiState.mainNodeSelectionValues, 'left')}
-        >
-          ←
-        </button>
-        <Stack direction="column">
+    <Box css={{ borderBottom: '1px solid $border', paddingBottom: '$4', marginBottom: '$4' }}>
+      <Stack direction="row" align="center" justify="between">
+        <Text bold>Add as annotation</Text>
+        <Stack direction="row" gap={0}>
           <button
             className="p-1 button button-secondary"
             type="button"
-            onClick={() => createAnnotation(uiState.mainNodeSelectionValues, 'top')}
+            onClick={() => createAnnotation(uiState.mainNodeSelectionValues, 'left')}
           >
-            ↑
+            ←
           </button>
+          <Stack direction="column">
+            <button
+              className="p-1 button button-secondary"
+              type="button"
+              onClick={() => createAnnotation(uiState.mainNodeSelectionValues, 'top')}
+            >
+              ↑
+            </button>
+            <button
+              className="p-1 button button-secondary"
+              type="button"
+              onClick={() => createAnnotation(uiState.mainNodeSelectionValues, 'bottom')}
+            >
+              ↓
+            </button>
+          </Stack>
           <button
             className="p-1 button button-secondary"
             type="button"
-            onClick={() => createAnnotation(uiState.mainNodeSelectionValues, 'bottom')}
+            onClick={() => createAnnotation(uiState.mainNodeSelectionValues, 'right')}
           >
-            ↓
+            →
           </button>
         </Stack>
-        <button
-          className="p-1 button button-secondary"
-          type="button"
-          onClick={() => createAnnotation(uiState.mainNodeSelectionValues, 'right')}
-        >
-          →
-        </button>
       </Stack>
-    </Stack>
+    </Box>
   ) : null;
 }
