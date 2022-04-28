48a970b5909ee6f42144e13d6d4e6c45af807c6c
Srinivas Bhamidipati <58106200+srinivas0210@users.noreply.github.com>
2022-04-29 00:30:05 +0530

    Feat: minimise plugin (#618)

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import SyncSettings from './SyncSettings';
 import Settings from './Settings';
 import Inspector from './Inspector';
 import Tokens from './Tokens';
@@ -15,6 +16,7 @@ import PushDialog from './PushDialog';
 import WindowResizer from './WindowResizer';
 import Box from './Box';
 import { activeTabSelector } from '@/selectors';
+import PluginResizerWrapper from './PluginResizer';
 
 function App() {
   const activeTab = useSelector(activeTabSelector);
@@ -23,27 +25,39 @@ function App() {
     <Box css={{ backgroundColor: '$bgDefault' }}>
       <Initiator />
       <LoadingBar />
-      <Box css={{
-        display: 'flex', flexDirection: 'column', height: '100%', overflow: 'hidden',
-      }}
-      >
-        <Box css={{
-          display: 'flex', flexDirection: 'column', flexGrow: 1, height: '100%', overflow: 'hidden',
-        }}
+      <PluginResizerWrapper>
+        <Box
+          css={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+            overflow: 'hidden',
+          }}
         >
-          {activeTab !== 'start' && <Navbar />}
-          {activeTab === 'start' && <StartScreen />}
-          <Tokens isActive={activeTab === 'tokens'} />
-          {activeTab === 'inspector' && <Inspector />}
-          {activeTab === 'settings' && <Settings />}
+          <Box
+            css={{
+              display: 'flex',
+              flexDirection: 'column',
+              flexGrow: 1,
+              height: '100%',
+              overflow: 'hidden',
+            }}
+          >
+            {activeTab !== 'start' && <Navbar />}
+            {activeTab === 'start' && <StartScreen />}
+            <Tokens isActive={activeTab === 'tokens'} />
+            {activeTab === 'inspector' && <Inspector />}
+            {activeTab === 'syncsettings' && <SyncSettings />}
+            {activeTab === 'settings' && <Settings />}
+          </Box>
+          <Footer />
+          <Changelog />
+          <ImportedTokensDialog />
+          <ConfirmDialog />
+          <PushDialog />
+          <WindowResizer />
         </Box>
-        <Footer />
-        <Changelog />
-        <ImportedTokensDialog />
-        <ConfirmDialog />
-        <PushDialog />
-        <WindowResizer />
-      </Box>
+      </PluginResizerWrapper>
     </Box>
   );
 }
