abbf4cf04b8b1d82cd026ff1f59afb4fc69feb33
Jan Six <six7@github.com>
2022-04-29 22:45:07 +0200

    change file structure

---
+++ token-transformer/transform.test.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+describe('token-transformer', () => {
+    it('generates dark theme files correctly', async () => {
+        var shouldBuf = fs.readFileSync('output/dark.json');
+        var testBuf = fs.readFileSync('temp/dark.json');
+        expect(shouldBuf.toString()).toEqual(testBuf.toString());
+    });
+    it('generates light theme files correctly', async () => {
+        var shouldBuf = fs.readFileSync('output/light.json');
+        var testBuf = fs.readFileSync('temp/light.json');
+        expect(shouldBuf.toString()).toEqual(testBuf.toString());
+    });
+    it('generates typography theme files correctly', async () => {
+        var shouldBuf = fs.readFileSync('output/typography.json');
+        var testBuf = fs.readFileSync('temp/typography.json');
+        expect(shouldBuf.toString()).toEqual(testBuf.toString());
+    });
+    it('generates light theme files correctly from folder', async () => {
+        var shouldBuf = fs.readFileSync('output/folder-light.json');
+        var testBuf = fs.readFileSync('temp/folder-light.json');
+        expect(shouldBuf.toString()).toEqual(testBuf.toString());
+    });
+});
