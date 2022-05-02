d8229c3035802241358369b86deabb8c1bee63d5
MightyDevKnight <takao.kamio@brandcode.dev>
2022-05-02 16:54:33 +0900

    resolve the conflict

--- src/app/components/TokenGroupHeading.tsx
+++ src/app/components/TokenGroupHeading.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import {
-  ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger,
-} from './ContextMenu';
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from './ContextMenu';
 import Stack from './Stack';
 import Button from './Button';
 import Heading from './Heading';
@@ -13,9 +11,9 @@ import useManageTokens from '../store/useManageTokens';
 import { editProhibitedSelector } from '@/selectors';
 
 type Props = {
-  id: string
-  label: string
-  path: string
+  id: string;
+  label: string;
+  path: string;
 };
 export default function TokenGroupHeading({ label, path, id }: Props) {
   const editProhibited = useSelector(editProhibitedSelector);
@@ -24,18 +22,15 @@ export default function TokenGroupHeading({ label, path, id }: Props) {
   const [newTokenGroupName, handleNewTokenGroupNameChange] = React.useState('');
   const { deleteGroup, renameGroup } = useManageTokens();
 
-  React.useEffect(() => {
-  }, []);
-  const handleRenameTokenGroupSubmit = React.useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const oldName: string = path.split('.').pop()?.toString();
-    const newPath = path.slice(0,(path.length-oldName.length));
-    setShowRenameTokenGroupField(false);
-    renameGroup( { path: newPath, oldName: oldName, newName: newTokenGroupName} );
-
-    setTokenGroupMarkedForChange('');
-    setShowRenameTokenGroupField(false);
-  }, [newTokenGroupName, tokenGroupMarkedForChange]);
+  const handleRenameTokenGroupSubmit = React.useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      renameGroup({ oldName: path, newName: newTokenGroupName });
+      setTokenGroupMarkedForChange('');
+      setShowRenameTokenGroupField(false);
+    },
+    [newTokenGroupName, tokenGroupMarkedForChange]
+  );
 
   const handleRenameTokenGroup = React.useCallback(() => {
     setShowRenameTokenGroupField(true);
@@ -59,7 +54,9 @@ export default function TokenGroupHeading({ label, path, id }: Props) {
     >
       <ContextMenu>
         <ContextMenuTrigger id={`group-heading-${path}-${label}-${id}`}>
-          <Heading muted size="small">{label}</Heading>
+          <Heading muted size="small">
+            {label}
+          </Heading>
         </ContextMenuTrigger>
         <ContextMenuContent>
           <ContextMenuItem disabled={editProhibited} onSelect={handleSelect}>
@@ -72,31 +69,32 @@ export default function TokenGroupHeading({ label, path, id }: Props) {
       </ContextMenu>
       <Modal isOpen={showRenameTokenGroupField} close={() => setShowRenameTokenGroupField(false)}>
         <Stack direction="column" justify="center" gap={4} css={{ textAlign: 'center' }}>
-          <Heading size="small">
-              Rename
-              {' '}
-              {tokenGroupMarkedForChange}
-            </Heading>
-            <form onSubmit={handleRenameTokenGroupSubmit}>
-              <Stack direction="column" gap={4}>
-                <Input
-                    full
-                    value={newTokenGroupName}
-                    onChange={(e) => handleNewTokenGroupNameChange(e.target.value)}
-                    type="text"
-                    name="tokengroupname"
-                    required
-                />
-                <Stack direction="row" gap={4}>
-                  <Button variant="secondary" size="large" onClick={() => setShowRenameTokenGroupField(false)}>
-                    Cancel
-                  </Button>
-                  <Button type="submit" variant="primary" size="large" disabled={tokenGroupMarkedForChange === newTokenGroupName}>
-                    Change
-                  </Button>
-                </Stack>
+          <Heading size="small">Rename {tokenGroupMarkedForChange}</Heading>
+          <form onSubmit={handleRenameTokenGroupSubmit}>
+            <Stack direction="column" gap={4}>
+              <Input
+                full
+                value={newTokenGroupName}
+                onChange={(e) => handleNewTokenGroupNameChange(e.target.value)}
+                type="text"
+                name="tokengroupname"
+                required
+              />
+              <Stack direction="row" gap={4}>
+                <Button variant="secondary" size="large" onClick={() => setShowRenameTokenGroupField(false)}>
+                  Cancel
+                </Button>
+                <Button
+                  type="submit"
+                  variant="primary"
+                  size="large"
+                  disabled={tokenGroupMarkedForChange === newTokenGroupName}
+                >
+                  Change
+                </Button>
               </Stack>
-            </form>
+            </Stack>
+          </form>
         </Stack>
       </Modal>
     </Box>
