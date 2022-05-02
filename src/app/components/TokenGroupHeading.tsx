e129383668f36dee28f88e3d74bcd7007411e6b4
MightyDevKnight <takao.kamio@brandcode.dev>
2022-05-02 16:38:41 +0900

    fixed bugs with Holding on

--- src/app/components/TokenGroupHeading.tsx
+++ src/app/components/TokenGroupHeading.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import {
-  ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger,
-} from './ContextMenu';
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuTrigger } from './ContextMenu';
 import Stack from './Stack';
 import Button from './Button';
 import Heading from './Heading';
@@ -13,9 +11,9 @@ import useManageTokens from '../store/useManageTokens';
 import { editProhibitedSelector } from '@/selectors';
 
 type Props = {
-  id: string
-  label: string
-  path: string
+  id: string;
+  label: string;
+  path: string;
 };
 export default function TokenGroupHeading({ label, path, id }: Props) {
   const editProhibited = useSelector(editProhibitedSelector);
@@ -23,10 +21,9 @@ export default function TokenGroupHeading({ label, path, id }: Props) {
   const [showRenameTokenGroupField, setShowRenameTokenGroupField] = React.useState(false);
   const [tokenGroupMarkedForChange, setTokenGroupMarkedForChange] = React.useState('');
   const [newTokenGroupName, handleNewTokenGroupNameChange] = React.useState('');
-  const [isDuplicated , setIsDuplicated] = React.useState(false);
+  const [isDuplicated, setIsDuplicated] = React.useState(false);
 
-  React.useEffect(() => {
-  }, []);
+  React.useEffect(() => {}, []);
 
   const handleDelete = React.useCallback(() => {
     deleteGroup(path);
@@ -34,25 +31,27 @@ export default function TokenGroupHeading({ label, path, id }: Props) {
 
   const handleDuplicate = React.useCallback(() => {
     const oldName = path.split('.').pop()?.toString();
-    const newPath = path.slice(0, (path.length - oldName.length));
-    duplicateGroup({newPath, oldName});
+    const newPath = path.slice(0, path.length - oldName.length);
+    duplicateGroup({ newPath, oldName });
     setIsDuplicated(true);
     setShowRenameTokenGroupField(true);
-  }, [path, duplicateGroup, setShowRenameTokenGroupField, isDuplicated]);  
+  }, [path, duplicateGroup, setShowRenameTokenGroupField]);
 
-  const handleRenameTokenGroupSubmit = React.useCallback((e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const oldName: string = path.split('.').pop()?.toString();
-    const newPath = path.slice(0,(path.length-oldName.length));
-    setShowRenameTokenGroupField(false);
-    if(!isDuplicated){
-      renameGroup( { path: newPath, oldName: oldName, newName: newTokenGroupName} );
-    }
-    else{
-      renameGroup( { path: newPath, oldName: `${oldName}-copy`, newName: newTokenGroupName} );
-    }
-    setIsDuplicated(false);
-  }, [newTokenGroupName, tokenGroupMarkedForChange, isDuplicated]);
+  const handleRenameTokenGroupSubmit = React.useCallback(
+    (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const oldName: string = path.split('.').pop()?.toString();
+      const newPath = path.slice(0, path.length - oldName.length);
+      setShowRenameTokenGroupField(false);
+      if (!isDuplicated) {
+        renameGroup({ path: newPath, oldName: oldName, newName: newTokenGroupName });
+      } else {
+        renameGroup({ path: newPath, oldName: `${oldName}-copy`, newName: newTokenGroupName });
+      }
+      setIsDuplicated(false);
+    },
+    [newTokenGroupName, tokenGroupMarkedForChange, isDuplicated]
+  );
 
   const handleRenameTokenGroup = React.useCallback(() => {
     setShowRenameTokenGroupField(true);
@@ -73,7 +72,9 @@ export default function TokenGroupHeading({ label, path, id }: Props) {
     >
       <ContextMenu>
         <ContextMenuTrigger id={`group-heading-${path}-${label}-${id}`}>
-          <Heading muted size="small">{label}</Heading>
+          <Heading muted size="small">
+            {label}
+          </Heading>
         </ContextMenuTrigger>
         <ContextMenuContent>
           <ContextMenuItem disabled={editProhibited} onSelect={handleDelete}>
@@ -89,31 +90,32 @@ export default function TokenGroupHeading({ label, path, id }: Props) {
       </ContextMenu>
       <Modal isOpen={showRenameTokenGroupField} close={() => setShowRenameTokenGroupField(false)}>
         <Stack direction="column" justify="center" gap={4} css={{ textAlign: 'center' }}>
-          <Heading size="small">
-              Rename
-              {' '}
-              {tokenGroupMarkedForChange}
-            </Heading>
-            <form onSubmit={handleRenameTokenGroupSubmit}>
-              <Stack direction="column" gap={4}>
-                <Input
-                    full
-                    value={newTokenGroupName}
-                    onChange={(e) => handleNewTokenGroupNameChange(e.target.value)}
-                    type="text"
-                    name="tokengroupname"
-                    required
-                />
-                <Stack direction="row" gap={4}>
-                  <Button variant="secondary" size="large" onClick={() => setShowRenameTokenGroupField(false)}>
-                    Cancel
-                  </Button>
-                  <Button type="submit" variant="primary" size="large" disabled={tokenGroupMarkedForChange === newTokenGroupName}>
-                    Change
-                  </Button>
-                </Stack>
+          <Heading size="small">Rename {tokenGroupMarkedForChange}</Heading>
+          <form onSubmit={handleRenameTokenGroupSubmit}>
+            <Stack direction="column" gap={4}>
+              <Input
+                full
+                value={newTokenGroupName}
+                onChange={(e) => handleNewTokenGroupNameChange(e.target.value)}
+                type="text"
+                name="tokengroupname"
+                required
+              />
+              <Stack direction="row" gap={4}>
+                <Button variant="secondary" size="large" onClick={() => setShowRenameTokenGroupField(false)}>
+                  Cancel
+                </Button>
+                <Button
+                  type="submit"
+                  variant="primary"
+                  size="large"
+                  disabled={tokenGroupMarkedForChange === newTokenGroupName}
+                >
+                  Change
+                </Button>
               </Stack>
-            </form>
+            </Stack>
+          </form>
         </Stack>
       </Modal>
     </Box>
--- src/app/store/useManageTokens.tsx
+++ src/app/store/useManageTokens.tsx
@@ -6,7 +6,7 @@ import useConfirm from '../hooks/useConfirm';
 import { BackgroundJobs } from '@/constants/BackgroundJobs';
 import { activeTokenSetSelector } from '@/selectors';
 import { TokenTypes } from '@/constants/TokenTypes';
-import { DeleteTokenPayload, UpdateTokenPayload  } from '@/types/payloads';
+import { DeleteTokenPayload, UpdateTokenPayload } from '@/types/payloads';
 
 type EditSingleTokenData = {
   parent: string;
@@ -31,117 +31,159 @@ export default function useManageTokens() {
   const dispatch = useDispatch<Dispatch>();
   const { confirm } = useConfirm();
   const {
-    editToken, createToken, deleteToken, duplicateToken, deleteTokenGroup, duplicateTokenGroup, renameTokenGroup
+    editToken,
+    createToken,
+    deleteToken,
+    duplicateToken,
+    deleteTokenGroup,
+    duplicateTokenGroup,
+    renameTokenGroup,
   } = dispatch.tokenState;
 
-  const editSingleToken = useCallback(async (data: EditSingleTokenData) => {
-    const {
-      parent, name, value, options, oldName, shouldUpdateDocument = true,
-    } = data;
-    dispatch.uiState.startJob({
-      name: BackgroundJobs.UI_EDITSINGLETOKEN,
-      isInfinite: true,
-    });
-
-    // should be a setting which users can toggle on / off to disable auto-sync after each token change
-    const shouldUpdate = true;
-
-    if (shouldUpdate) {
-      editToken({
-        parent,
-        name,
-        value,
-        options,
-        oldName,
-        shouldUpdate: shouldUpdateDocument,
-      });
-    }
-    dispatch.uiState.completeJob(BackgroundJobs.UI_EDITSINGLETOKEN);
-  }, [editToken, dispatch.uiState]);
-
-  const createSingleToken = useCallback(async (data: CreateSingleTokenData) => {
-    const {
-      parent, name, value, options, newGroup = false, shouldUpdateDocument = true,
-    } = data;
-    dispatch.uiState.startJob({
-      name: BackgroundJobs.UI_CREATESINGLETOKEN,
-      isInfinite: true,
-    });
-    // should be a setting which users can toggle on / off to disable auto-sync after each token change
-    const shouldUpdate = true;
-    if (shouldUpdate) {
-      createToken({
-        parent,
-        name,
-        value,
-        options,
-        newGroup,
-        shouldUpdate: shouldUpdateDocument,
-      });
-    }
-    dispatch.uiState.completeJob(BackgroundJobs.UI_CREATESINGLETOKEN);
-  }, [createToken, dispatch.uiState]);
-
-  const duplicateSingleToken = useCallback(async (data: UpdateTokenPayload) => {
-    dispatch.uiState.startJob({
-      name: BackgroundJobs.UI_DUPLICATETOKEN,
-      isInfinite: true,
-    });
-    duplicateToken(data);
-    dispatch.uiState.completeJob(BackgroundJobs.UI_DUPLICATETOKEN);
-  }, [duplicateToken, dispatch.uiState]);
-
-  const deleteSingleToken = useCallback(async (data: DeleteTokenPayload) => {
-    const userConfirmation = await confirm({
-      text: 'Delete token?',
-      description: 'Are you sure you want to delete this token?',
-    });
-    if (userConfirmation) {
+  const editSingleToken = useCallback(
+    async (data: EditSingleTokenData) => {
+      const { parent, name, value, options, oldName, shouldUpdateDocument = true } = data;
       dispatch.uiState.startJob({
-        name: BackgroundJobs.UI_DELETETOKEN,
+        name: BackgroundJobs.UI_EDITSINGLETOKEN,
         isInfinite: true,
       });
-      deleteToken(data);
-      dispatch.uiState.completeJob(BackgroundJobs.UI_DELETETOKEN);
-    }
-  }, [confirm, deleteToken, dispatch.uiState]);
 
-  const deleteGroup = useCallback(async (path: string) => {
-    const userConfirmation = await confirm({
-      text: 'Delete group?',
-      description: 'Are you sure you want to delete this group?',
-    });
-    if (userConfirmation) {
-      const activeTokenSet = activeTokenSetSelector(store.getState());
+      // should be a setting which users can toggle on / off to disable auto-sync after each token change
+      const shouldUpdate = true;
+
+      if (shouldUpdate) {
+        editToken({
+          parent,
+          name,
+          value,
+          options,
+          oldName,
+          shouldUpdate: shouldUpdateDocument,
+        });
+      }
+      dispatch.uiState.completeJob(BackgroundJobs.UI_EDITSINGLETOKEN);
+    },
+    [editToken, dispatch.uiState]
+  );
+
+  const createSingleToken = useCallback(
+    async (data: CreateSingleTokenData) => {
+      const { parent, name, value, options, newGroup = false, shouldUpdateDocument = true } = data;
       dispatch.uiState.startJob({
-        name: BackgroundJobs.UI_DELETETOKENGROUP,
+        name: BackgroundJobs.UI_CREATESINGLETOKEN,
         isInfinite: true,
       });
-      deleteTokenGroup({ parent: activeTokenSet, path });
-      dispatch.uiState.completeJob(BackgroundJobs.UI_DELETETOKENGROUP);
-    }
-  }, [store, confirm, deleteTokenGroup, dispatch.uiState]);
+      // should be a setting which users can toggle on / off to disable auto-sync after each token change
+      const shouldUpdate = true;
+      if (shouldUpdate) {
+        createToken({
+          parent,
+          name,
+          value,
+          options,
+          newGroup,
+          shouldUpdate: shouldUpdateDocument,
+        });
+      }
+      dispatch.uiState.completeJob(BackgroundJobs.UI_CREATESINGLETOKEN);
+    },
+    [createToken, dispatch.uiState]
+  );
 
-  const duplicateGroup = useCallback(async (data: {newPath: string, oldName: string}) => {
-    const {newPath, oldName} = data;
-    dispatch.uiState.startJob({
-      name: BackgroundJobs.UI_DUPLICATTOKENGROUP,
-      isInfinite: true,
-    });
-    const activeTokenSet = activeTokenSetSelector(store.getState());
-    duplicateTokenGroup({ parent: activeTokenSet, path: newPath, oldName });
-  },[store, duplicateTokenGroup, dispatch.uiState]);
-
-  const renameGroup = useCallback(async (data: {path: string, oldName: string, newName: string}) => {
-      const activeTokenSet = activeTokenSetSelector(store.getState());
-      const {path, oldName, newName} = data;
+  const duplicateSingleToken = useCallback(
+    async (data: UpdateTokenPayload) => {
       dispatch.uiState.startJob({
-          name: BackgroundJobs.UI_RENAMETOKENGROUP,
+        name: BackgroundJobs.UI_DUPLICATETOKEN,
+        isInfinite: true,
+      });
+      duplicateToken(data);
+      dispatch.uiState.completeJob(BackgroundJobs.UI_DUPLICATETOKEN);
+    },
+    [duplicateToken, dispatch.uiState]
+  );
+
+  const deleteSingleToken = useCallback(
+    async (data: DeleteTokenPayload) => {
+      const userConfirmation = await confirm({
+        text: 'Delete token?',
+        description: 'Are you sure you want to delete this token?',
+      });
+      if (userConfirmation) {
+        dispatch.uiState.startJob({
+          name: BackgroundJobs.UI_DELETETOKEN,
           isInfinite: true,
         });
-      renameTokenGroup({ parent: activeTokenSet, path: path, oldName: oldName, newName: newName});
+        deleteToken(data);
+        dispatch.uiState.completeJob(BackgroundJobs.UI_DELETETOKEN);
+      }
+    },
+    [confirm, deleteToken, dispatch.uiState]
+  );
+
+  const deleteGroup = useCallback(
+    async (path: string) => {
+      const userConfirmation = await confirm({
+        text: 'Delete group?',
+        description: 'Are you sure you want to delete this group?',
+      });
+      if (userConfirmation) {
+        const activeTokenSet = activeTokenSetSelector(store.getState());
+        dispatch.uiState.startJob({
+          name: BackgroundJobs.UI_DELETETOKENGROUP,
+          isInfinite: true,
+        });
+        deleteTokenGroup({ parent: activeTokenSet, path });
+        dispatch.uiState.completeJob(BackgroundJobs.UI_DELETETOKENGROUP);
+      }
+    },
+    [store, confirm, deleteTokenGroup, dispatch.uiState]
+  );
+
+  const duplicateGroup = useCallback(
+    async (data: { newPath: string; oldName: string }) => {
+      const { newPath, oldName } = data;
+      dispatch.uiState.startJob({
+        name: BackgroundJobs.UI_DUPLICATTOKENGROUP,
+        isInfinite: true,
+      });
+      const activeTokenSet = activeTokenSetSelector(store.getState());
+      duplicateTokenGroup({ parent: activeTokenSet, path: newPath, oldName });
+      dispatch.uiState.completeJob(BackgroundJobs.UI_DUPLICATTOKENGROUP);
+    },
+    [store, duplicateTokenGroup, dispatch.uiState]
+  );
+
+  const renameGroup = useCallback(
+    async (data: { path: string; oldName: string; newName: string }) => {
+      const activeTokenSet = activeTokenSetSelector(store.getState());
+      const { path, oldName, newName } = data;
+      dispatch.uiState.startJob({
+        name: BackgroundJobs.UI_RENAMETOKENGROUP,
+        isInfinite: true,
+      });
+      renameTokenGroup({ parent: activeTokenSet, path: path, oldName: oldName, newName: newName });
       dispatch.uiState.completeJob(BackgroundJobs.UI_RENAMETOKENGROUP);
-    },[store, renameTokenGroup, dispatch.uiState]);
-  return useMemo(() => ({
-    editSingleToken, createSingleToken, deleteSingleToken, deleteGroup, duplicateSingleToken, duplicateGroup, renameGroup
-  }), [editSingleToken, createSingleToken, deleteSingleToken, deleteGroup, duplicateSingleToken, duplicateGroup, renameGroup]);}
+    },
+    [store, renameTokenGroup, dispatch.uiState]
+  );
+  return useMemo(
+    () => ({
+      editSingleToken,
+      createSingleToken,
+      deleteSingleToken,
+      deleteGroup,
+      duplicateSingleToken,
+      duplicateGroup,
+      renameGroup,
+    }),
+    [
+      editSingleToken,
+      createSingleToken,
+      deleteSingleToken,
+      deleteGroup,
+      duplicateSingleToken,
+      duplicateGroup,
+      renameGroup,
+    ]
+  );
+}
