260e6a1c852974d9eb31a42ee89ee099dd87a3fc
MightyDevKnight <takao.kamio@brandcode.dev>
2022-05-04 15:30:28 +0900

    resolve conflict

--- src/app/components/PluginResizer.tsx
+++ src/app/components/PluginResizer.tsx
@@ -1,17 +1,7 @@
 import React from 'react';
-import { styled } from '@/stitches.config';
 import Maximize from '../assets/maximize.svg';
 import useMinimizeWindow from './useMinimizeWindow';
-import Tooltip from './Tooltip';
-
-const StyledButton = styled('button', {
-  all: 'unset',
-  border: 'none',
-  padding: '$2',
-  marginLeft: '$4',
-  borderRadius: '$button',
-  cursor: 'pointer',
-});
+import IconButton from './IconButton';
 
 type Props = {
   children: React.ReactElement;
@@ -21,11 +11,7 @@ const PluginResizerWrapper: React.FC<Props> = ({ children }) => {
   const { isPluginminimized, handleResize } = useMinimizeWindow();
 
   return isPluginminimized ? (
-    <Tooltip label="Maximize plugin">
-      <StyledButton type="button" onClick={handleResize}>
-        <Maximize />
-      </StyledButton>
-    </Tooltip>
+    <IconButton tooltip="Maximize plugin" onClick={handleResize} icon={<Maximize />} />
   ) : (
     children
   );
