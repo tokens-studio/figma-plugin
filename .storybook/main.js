d25ba9a1bc4afe204cc39f30348c8e206980216a
MightyDevKnight <takao.kamio@brandcode.dev>
2022-05-05 23:56:57 +0900

    storybook implementaion init

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,31 @@
+const path = require('path');
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/app/components/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: [ "../src/**/*.stories.mdx", "../src/app/components/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-docs',
+      options:{
+        configureJSX: true,
+      }
+    }
   ],
-  "framework": "@storybook/react"
+  framework: "@storybook/react",
+  webpackFinal: async (config, { configType }) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '../src/');
+    return config;
+  },
+  typescript: {
+    typescript: {
+      check: false,
+      checkOptions: {},
+      reactDocgen:  'react-docgen-typescript',
+      reactDocgenTypescriptOptions:{
+        shouldExtractLiteralValuesFromEnum: true,
+        propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
+      },
+    },
+  }
 }
