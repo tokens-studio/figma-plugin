33624983a98313456233adbe11cc7ce72d0c5fea
Liam Martens <hi@liammartens.com>
2022-05-11 21:04:19 -0400

    feat: LAUNCHDARKLY_FLAGS override

--- src/app/components/LaunchDarkly/withLDProvider.tsx
+++ src/app/components/LaunchDarkly/withLDProvider.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ProviderConfig, withLDProvider as withLDProviderRaw } from 'launchdarkly-react-client-sdk';
 
-export const withLDProvider = !process.env.LAUNCHDARKLY_ENABLED
+export const withLDProvider = process.env.LAUNCHDARKLY_FLAGS
   ? function withLDProvider<T = {}>(config: ProviderConfig) {
     console.log('LaunchDarkly config:', config);
     return (WrappedComponent: React.ComponentType<T>) => (
