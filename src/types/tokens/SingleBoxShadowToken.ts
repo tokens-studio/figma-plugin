fad012545d9f39a419d4fdb9e572da3c255f236b
Jan Six <six.jan@gmail.com>
2022-05-12 09:25:15 +0200

    fix typography and boxshadow tokens on alias level (#697)
    * fix typography and boxshadow tokens on alias level
    
    * moved sync to bottom for all providers
    
    * fix sync settings issues

--- src/types/tokens/SingleBoxShadowToken.ts
+++ src/types/tokens/SingleBoxShadowToken.ts
@@ -3,5 +3,5 @@ import { TokenBoxshadowValue } from '../values';
 import { SingleGenericToken } from './SingleGenericToken';
 
 export type SingleBoxShadowToken<Named extends boolean = true, P = unknown> = SingleGenericToken<TokenTypes.BOX_SHADOW, (
-  TokenBoxshadowValue | TokenBoxshadowValue[]
+  TokenBoxshadowValue | TokenBoxshadowValue[] | string
 ), Named, P>;
