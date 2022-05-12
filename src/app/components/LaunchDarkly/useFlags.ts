5016ec8c958ab384b6c977759b2811bb3fc144be
Liam Martens <hi@liammartens.com>
2022-05-11 21:10:12 -0400

    chore: Remove log

--- src/app/components/LaunchDarkly/useFlags.ts
+++ src/app/components/LaunchDarkly/useFlags.ts
@@ -1,6 +1,5 @@
 import { useFlags as useFlagsRaw } from 'launchdarkly-react-client-sdk';
 
-console.log(process.env.LAUNCHDARKLY_FLAGS);
 export const useFlags = process.env.LAUNCHDARKLY_FLAGS
   ? () => (
     Object.fromEntries(process.env.LAUNCHDARKLY_FLAGS!.split(',').map((flag) => (
