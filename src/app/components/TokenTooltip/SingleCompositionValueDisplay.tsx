2f7ff3a80b27dd555ce8dbeba3adca2d987080c0
hiroshi <robinhoodie0823@gmail.com>
2022-05-13 12:40:14 +0900

    CompositionValueDisplay modify

--- src/app/components/TokenTooltip/SingleCompositionValueDisplay.tsx
+++ src/app/components/TokenTooltip/SingleCompositionValueDisplay.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { TokenCompositionValue } from '@/types/values';
+import { TokenBoxshadowValue, TokenCompositionValue, TokenTypograpyValue } from '@/types/values';
 import Box from '../Box';
+import { TokenTypes } from '@/constants/TokenTypes';
 
 // @TODO confirm whether the Composition token values
 // can still have the .value property from legacy
@@ -10,11 +11,38 @@ type Props = {
 };
 
 export const SingleCompositionValueDisplay: React.FC<Props> = ({ value }) => {
+  const displayValue = React.useCallback((compositionValue: TokenBoxshadowValue | TokenTypograpyValue | string) => {
+    let returnValue: string;
+    if (typeof compositionValue !== 'string') {
+      returnValue = Object.entries(compositionValue).reduce<string>((acc, [, propertyValue]) => (
+        `${acc}${propertyValue.toString()}/`
+      ), '');  
+    }
+    else returnValue = compositionValue;
+    return returnValue;
+  }, []);
   return (
-    <Box css={{ color: '$bgDefault' }}>
-        {value?.property}
-        {' : '}
-        {value?.value.toString()}
+    <Box css={{ color: '$bgDefault', display: 'flex' }}>
+      <Box css={{ color: '$bgDefault' }}>
+        {value?.property + ':'}
+      </Box>
+      <Box css={{ color: '$bgDefault', display: 'grid' }}>
+        {
+          (Array.isArray(value.value)) ? (
+            value.value.map((singleValue) => {
+              return (
+                <Box>
+                  {displayValue(singleValue)}
+                </Box>
+              )
+            })
+          ) : (
+            <Box>
+              {displayValue(value.value)}
+            </Box>
+          )
+        }
+      </Box>
     </Box>
 
   );
