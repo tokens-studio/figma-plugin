8399c52952677d2de81a0637c8bd59dccea54c32
Liam Martens <hi@liammartens.com>
2022-05-13 09:11:19 -0400

    fix: Updated the JSONBin token schema validation (#704)

--- src/storage/schemas/multiFileSchema.ts
+++ src/storage/schemas/multiFileSchema.ts
@@ -7,5 +7,5 @@ export const multiFileSchema = z.record(z.record(z.object({
   id: z.string(),
   name: z.string(),
   selectedTokenSets: z.record(z.string()),
-  $figmaStyleReferences: z.record(z.number()).optional(),
+  $figmaStyleReferences: z.record(z.string()).optional(),
 })));
