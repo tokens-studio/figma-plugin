dc4b20b3b57f0c357e05a29aca9a5f0bc1d90c61
Nelson Menezes <nemzes@gmail.com>
2022-05-16 12:35:13 +0200

    Add ability to use units in math expressions

---
+++ src/utils/math/__tests__/checkAndEvaluateMath.test.ts
@@ -0,0 +1,25 @@
+import { checkAndEvaluateMath } from '../checkAndEvaluateMath';
+
+describe('checkAndEvaluateMath', () => {
+  it('solves simple unit-less expressions', () => {
+    expect(checkAndEvaluateMath('10 + 20 * 3')).toEqual('70');
+  });
+
+  it('solves simple expressions with units', () => {
+    expect(checkAndEvaluateMath('10px + 20px * 3')).toEqual('70px');
+    expect(checkAndEvaluateMath('1rem + 2rem * 3')).toEqual('7rem');
+    expect(checkAndEvaluateMath('60px / 2')).toEqual('30px');
+  });
+
+  it('solves complex expressions with units', () => {
+    expect(checkAndEvaluateMath('5px * (2 + 3)')).toEqual('25px');
+    expect(checkAndEvaluateMath('(5rem + 10rem * 2) / 2')).toEqual('12.5rem');
+    expect(checkAndEvaluateMath('5% - 2% * 2')).toEqual('1%');
+  });
+
+  it('does not solve expressions with mismatched units', () => {
+    expect(checkAndEvaluateMath('1rem + 1px')).toEqual('1rem + 1px');
+    expect(checkAndEvaluateMath('2px * 2px')).toEqual('2px * 2px');
+    expect(checkAndEvaluateMath('2px + 10%')).toEqual('2px + 10%');
+  });
+});
--- src/utils/math/checkAndEvaluateMath.ts
+++ src/utils/math/checkAndEvaluateMath.ts
@@ -1,11 +1,26 @@
 import { Parser } from 'expr-eval';
+import calcAstParser from 'postcss-calc-ast-parser';
 
 const parser = new Parser();
 
 export function checkAndEvaluateMath(expr: string) {
+  const calcParsed = calcAstParser.parse(expr);
+  const calcReduced = calcAstParser.reduceExpression(calcParsed);
+
+  if (!calcReduced) {
+    return expr;
+  }
+
+  let unitlessExpr = expr;
+  let unit = '';
+
+  if (calcReduced.type !== 'Number') {
+    unitlessExpr = expr.replace(new RegExp(calcReduced.unit, 'ig'), '');
+    unit = calcReduced.unit;
+  }
+
   try {
-    parser.evaluate(expr);
-    return +parser.evaluate(expr).toFixed(3);
+    return +parser.evaluate(unitlessExpr).toFixed(3) + unit;
   } catch (ex) {
     return expr;
   }
