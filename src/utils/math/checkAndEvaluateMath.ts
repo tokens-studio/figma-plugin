c60c67606293d763ea7cef3d615e99a63c54a193
Nelson Menezes <nemzes@gmail.com>
2022-05-19 22:33:44 +0200

    Add ability to use units in math expressions (#715)
    * Add ability to use units in math expressions
    
    * Return number when input is number

--- src/utils/math/checkAndEvaluateMath.ts
+++ src/utils/math/checkAndEvaluateMath.ts
@@ -1,12 +1,34 @@
 import { Parser } from 'expr-eval';
+import calcAstParser from 'postcss-calc-ast-parser';
+import { Root } from 'postcss-calc-ast-parser/dist/types/ast';
 
 const parser = new Parser();
 
 export function checkAndEvaluateMath(expr: string) {
+  let calcParsed: Root;
+
   try {
-    parser.evaluate(expr);
-    return +parser.evaluate(expr).toFixed(3);
+    calcParsed = calcAstParser.parse(expr);
   } catch (ex) {
     return expr;
   }
+
+  const calcReduced = calcAstParser.reduceExpression(calcParsed);
+  let unitlessExpr = expr;
+  let unit = '';
+
+  if (calcReduced && calcReduced.type !== 'Number') {
+    unitlessExpr = expr.replace(new RegExp(calcReduced.unit, 'ig'), '');
+    unit = calcReduced.unit;
+  }
+
+  let evaluated: number;
+
+  try {
+    evaluated = parser.evaluate(unitlessExpr);
+  } catch (ex) {
+    return expr;
+  }
+
+  return unit ? `${evaluated}${unit}` : Number.parseFloat(evaluated.toFixed(3));
 }
