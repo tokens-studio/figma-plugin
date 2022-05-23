08ed3f16cd723bb4f63a51da611978bf56b9cb04
Jakob Engelbrecht <jakob@basher.dk>
2022-05-24 00:25:41 +0200

    Fix Token Transformer references on typography and boxshadow tokens (#686)
    * Token Transformer add resolveReferences option
    
    * test: add token concatenation test
    
    * docs: add `resolveReferences` parameter
    
    * fix: convert yargs command option to correct type
    
    * fix: convert `sets|excludes` string args to arrays
    
    * fix: references on typography and boxshadow tokens
    
    * fix: ensure raw number values are number type
    
    * test: update snapshot
    
    * test: update snapshot
    
    * fix: keep aliases inside css functions+list values
    
    * test: update snapshot
    
    * test: add tests for aliases inside css functions
    
    * test: add tests for aliases inside list value
    
    * docs: add token examples for resolveReferences
    
    Co-authored-by: Jan Six <six.jan@gmail.com>

---
+++ src/utils/getValueWithReferences.ts
@@ -0,0 +1,68 @@
+import { TokenTypes } from '@/constants/TokenTypes';
+import { SingleToken } from '@/types/tokens';
+import { TokenBoxshadowValue, TokenTypograpyValue } from '@/types/values';
+import { TransformerOptions } from './types';
+
+function getSimpleValue(resolvedValue: string | number, rawValue: string | number, options: TransformerOptions) {
+  let value = resolvedValue;
+  /*
+   * ***************************************************************************
+   * TOKEN EXAMPLES WHEN USING --resolveReferences='math'
+   * ***************************************************************************
+   * {spacing.xs} * 2                 =>  MATH EXPRESSION       => RESOLVE
+   * {spacing.xs} * {spacing.scale}   =>  MATH EXPRESSION       => RESOLVE
+   *
+   * {spacing.xs}                     =>  SINGLE TOKEN          => DON'T RESOLVE
+   * {spacing.xs}rem                  =>  SINGLE TOKEN + UNIT   => DON'T RESOLVE
+   * {spacing.xs}{spacing.unit}       =>  MULTIPLE TOKENS       => DON'T RESOLVE
+   *
+   * rgba(255, 0, 0, {opacity.low})   =>  CSS FUNCTION          => DON'T RESOLVE
+   * calc({spacing.xl} * 2)           =>  CSS FUNCTION          => DON'T RESOLVE
+   * 20% {border-radius.smooth}       =>  CSS LIST VALUE        => DON'T RESOLVE
+   * ***************************************************************************
+   */
+
+  if (typeof rawValue === 'string' && resolvedValue.toString() !== rawValue) {
+    if (options.resolveReferences === false) {
+      value = rawValue;
+    }
+    if (options.resolveReferences === 'math') {
+      const singleAliasRegEx = /^{[^}]*}$|^\$[^$]*$/;
+      const oneOrMoreAliasRegEx = /{[^}]*}|\$[\w.-]*/g;
+      const aliasRegEx = typeof resolvedValue === 'number' ? singleAliasRegEx : oneOrMoreAliasRegEx;
+      if (aliasRegEx.test(rawValue)) {
+        value = rawValue;
+      }
+    }
+  }
+  return value as string; // TODO: remove `as string` when SingleGenericToken supports value as `string|number`
+}
+
+function getComplexValue<T extends TokenTypograpyValue | TokenBoxshadowValue>(
+  resolvedValue: T,
+  rawValue: T,
+  options: TransformerOptions,
+) {
+  type IndexedValueType = Record<string, string | number>;
+  return Object.entries(resolvedValue).reduce((acc, [key, val]) => {
+    const rawVal = (rawValue as IndexedValueType)[key];
+    acc[key] = getSimpleValue(val, rawVal, options);
+    return acc;
+  }, {} as IndexedValueType) as T;
+}
+
+export function getValueWithReferences(token: SingleToken, options: TransformerOptions) {
+  if (token.rawValue === undefined) {
+    return token.value;
+  }
+
+  if (token.type === TokenTypes.TYPOGRAPHY || token.type === TokenTypes.BOX_SHADOW) {
+    if (Array.isArray(token.value)) {
+      const rawValue = token.rawValue as TokenBoxshadowValue[];
+      return token.value.map((shadow, index) => getComplexValue(shadow, rawValue[index], options));
+    }
+    return getComplexValue(token.value, token.rawValue as TokenTypograpyValue | TokenBoxshadowValue, options);
+  }
+
+  return getSimpleValue(token.value, token.rawValue, options);
+}
