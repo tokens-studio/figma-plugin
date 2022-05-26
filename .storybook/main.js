f4c54ce809d7e9e2047a7e50da3671632574306a
Mighty Knight <102773603+MightyDevKnight@users.noreply.github.com>
2022-05-26 18:12:56 +0900

    Configure main js to add tailwind css (#759)
    * upgrade to SB 6.5.5
    
    * add tailwindcss configuration
    
    * configure preview.js
    
    * add tailwindcss configuration
    
    * Remove tsx storybook components (#752)
    
    * upgrade to SB 6.5.5
    
    * remove tsx SB components
    
    * add buton story
    
    * Error in console react modal (#749)
    
    * skip to start screen when create new file
    
    * resolve test error
    
    * add env
    
    * resolve react modal error
    
    * resolve test error
    
    Co-authored-by: hiroshi <robinhoodie0823@gmail.com>
    
    * add tailwindcss
    
    * update styles
    
    * fix bug with main.js
    
    * fix bug in tailwindcss
    
    Co-authored-by: SwordEdge <yamadera46@hotmail.com>
    Co-authored-by: hiroshi <robinhoodie0823@gmail.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,32 +1,51 @@
 const path = require('path');
 module.exports = {
-  stories: [ "../src/**/*.stories.mdx", "../src/stories/*.stories.@(js|jsx|ts|tsx)"],
-  core: {
-    builder: "webpack5",
-  },
+  stories: ["../src/**/*.stories.mdx", "../src/stories/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     {
       name: '@storybook/addon-docs',
-      options:{
+      options: {
         configureJSX: true,
       }
     }
   ],
+  features: {
+    interactionsDebugger: true,
+  },
+  core: {
+    builder: "webpack5",
+  },
   framework: "@storybook/react",
   webpackFinal: async (config, { configType }) => {
-    config.mode = 'none';
     config.resolve.alias['@'] = path.resolve(__dirname, '../src/');
+    // Remove the default .css webpack module rule
+    // This is necessary because we use both global CSS and CSS modules
+    // in the extension and in Storybook
+    config.module.rules = config.module.rules.filter((r) => {
+      if (".css".match(r.test)) {
+        return false;
+      }
+      return true;
+    });
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        "style-loader",
+        "css-loader",
+      ],
+    });
     return config;
   },
   typescript: {
     typescript: {
       check: false,
       checkOptions: {},
-      reactDocgen:  'react-docgen-typescript',
-      reactDocgenTypescriptOptions:{
+      reactDocgen: 'react-docgen-typescript',
+      reactDocgenTypescriptOptions: {
         shouldExtractLiteralValuesFromEnum: true,
         propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
       },
