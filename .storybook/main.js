22bb78d09604197e18390c9dcb6f36b41a13379e
Mighty Knight <102773603+MightyDevKnight@users.noreply.github.com>
2022-05-27 15:45:40 +0900

    Add stitches style to sb (#763)
    * add components
    
    * load svg icons
    
    * load svg icons and add icons storybook
    
    * bump release
    
    * Add email for LD (#737)
    
    * Fix Token Transformer references on typography and boxshadow tokens (#686)
    
    * Token Transformer add resolveReferences option
    
    * test: add token concatenation test
    
    * docs: add `resolveReferences` parameter
    
    * fix: convert yargs command option to correct type
    
    * fix: convert `sets|excludes` string args to arrays
    
    * fix: references on typography and boxshadow tokens
    
    * fix: ensure raw number values are number type
    
    * test: update snapshot
    
    * test: update snapshot
    
    * fix: keep aliases inside css functions+list values
    
    * test: update snapshot
    
    * test: add tests for aliases inside css functions
    
    * test: add tests for aliases inside list value
    
    * docs: add token examples for resolveReferences
    
    Co-authored-by: Jan Six <six.jan@gmail.com>
    
    * resolve conflict
    
    * optimize coding style and add storybook components
    
    Co-authored-by: Jan Six <six7@github.com>
    Co-authored-by: Razvan <rop_pazvan@yahoo.com>
    Co-authored-by: Jakob Engelbrecht <jakob@basher.dk>
    Co-authored-by: Jan Six <six.jan@gmail.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,31 +1,19 @@
 const path = require('path');
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/stories/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ['../src/**/*.stories.mdx', '../src/stories/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        configureJSX: true,
-      }
-    }
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  features: {
-    interactionsDebugger: true,
-  },
-  core: {
-    builder: "webpack5",
-  },
-  framework: "@storybook/react",
+  framework: '@storybook/react',
   webpackFinal: async (config, { configType }) => {
     config.resolve.alias['@'] = path.resolve(__dirname, '../src/');
     // Remove the default .css webpack module rule
     // This is necessary because we use both global CSS and CSS modules
     // in the extension and in Storybook
     config.module.rules = config.module.rules.filter((r) => {
-      if (".css".match(r.test)) {
+      if ('.css'.match(r.test)) {
         return false;
       }
       return true;
@@ -34,9 +22,28 @@ module.exports = {
     config.module.rules.push({
       test: /\.css$/,
       use: [
-        "style-loader",
-        "css-loader",
+        'style-loader',
+        'css-loader',
       ],
+    });
+
+    config.module.rules = config.module.rules.filter((r) => {
+      if ('.svg'.match(r.test)) {
+        return false;
+      }
+      return true;
+    });
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{
+        loader: '@svgr/webpack',
+        options: {
+          svgoConfig: {
+            plugins: [{ removeViewBox: false }]
+          }
+        }
+      }],
     });
     return config;
   },
