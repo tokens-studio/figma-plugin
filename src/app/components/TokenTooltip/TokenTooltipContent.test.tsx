067bbebe428a2e2063cee95b77c5437da3b9012f
MightyDevKnight <takao.kamio@brandcode.dev>
2022-06-02 21:05:32 +0900

    add alias test

--- src/app/components/TokenTooltip/TokenTooltipContent.test.tsx
+++ src/app/components/TokenTooltip/TokenTooltipContent.test.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { TokenTooltipContent } from './TokenTooltipContent';
 import { SingleToken } from '@/types/tokens';
 import { TokenTypes } from '@/constants/TokenTypes';
+import { BoxShadowTypes } from '@/constants/BoxShadowTypes';
 import { render } from '../../../../tests/config/setupTest';
+import { getAliasValue } from '@/utils/alias';
 
 const tokens: SingleToken[] = [
   {
@@ -11,46 +13,69 @@ const tokens: SingleToken[] = [
     value: '2',
   },
   {
-    value: '#f8fafc',
-    type: TokenTypes.COLOR,
+    name: 'size.alias',
+    type: TokenTypes.SIZING,
+    value: '{size.6}',
+  },
+  {
     name: 'color.slate.50',
+    type: TokenTypes.COLOR,
+    value: '#f8fafc',
   },
   {
-    value: '64px',
-    type: TokenTypes.BORDER_RADIUS,
+    name: 'color.alias',
+    type: TokenTypes.COLOR,
+    value: '{color.slate.50}',
+  },
+  {
     name: 'border-radius.0',
+    type: TokenTypes.BORDER_RADIUS,
+    value: '64px',
   },
   {
-    value: '10%',
-    type: TokenTypes.OPACITY,
+    name: 'border-radius.0',
+    type: TokenTypes.BORDER_RADIUS,
+    value: '{border-radius.0}',
+  },
+  {
     name: 'opacity.10',
+    type: TokenTypes.OPACITY,
+    value: '10%',
   },
   {
+    name: 'opacity.alias',
+    type: TokenTypes.OPACITY,
+    value: '{opacity.10}',
+  },
+  {
+    name: 'typography.headlines.small',
+    type: TokenTypes.BOX_SHADOW,
     value: {
       x: 2,
       y: 2,
       blur: 2,
       spread: 2,
       color: '#000000',
-      type: 'dropShadow',
+      type: BoxShadowTypes.DROP_SHADOW,
     },
+  },
+  {
+    name: 'typography.alias',
     type: TokenTypes.BOX_SHADOW,
-    name: 'typography.headlines.small',
+    value: '{typography.headlines.small}',
   },
   {
-    value: 'IBM Plex Serif',
-    type: TokenTypes.FONT_FAMILIES,
     name: 'font-family.serif',
+    type: TokenTypes.FONT_FAMILIES,
+    value: 'IBM Plex Serif',
   },
   {
-    name: 'compositiontoken',
-    value: {
-      sizing: '{size.12}',
-    },
-    type: TokenTypes.COMPOSITION,
+    name: 'font-family.alias',
+    type: TokenTypes.FONT_FAMILIES,
+    value: '{font-family.serif}',
   },
 ];
-describe('TokenTooltip', () => {
+describe('TokenTooltip value', () => {
   it('tooltip value test', () => {
     tokens.forEach((token) => {
       const { getByText } = render(<TokenTooltipContent token={token} />);
@@ -86,5 +111,62 @@ describe('TokenTooltip', () => {
           break;
       }
     });
+  });
+});
+describe('TokenTooltip alias', () => {
+  it('size alias test', () => {
+    const input: SingleToken = {
+      name: 'size.alias',
+      type: TokenTypes.SIZING,
+      value: '{size.6}',
+    };
+    expect(getAliasValue(input, tokens)).toEqual(2);
+  });
+  it('color alias test', () => {
+    const input: SingleToken = {
+      name: 'color.alias',
+      type: TokenTypes.COLOR,
+      value: '{color.slate.50}',
+    };
+    expect(getAliasValue(input, tokens)).toEqual('#f8fafc');
+  });
+  it('border-radius alias test', () => {
+    const input: SingleToken = {
+      name: 'border-radius.alias',
+      type: TokenTypes.BORDER_RADIUS,
+      value: '{border-radius.0}',
+    };
+    expect(getAliasValue(input, tokens)).toEqual('64px');
+  });
+  it('opacity alias test', () => {
+    const input: SingleToken = {
+      name: 'opacity.alias',
+      type: TokenTypes.OPACITY,
+      value: '{opacity.10}',
+    };
+    expect(getAliasValue(input, tokens)).toEqual('10%');
+  });
+  it('typography alias test', () => {
+    const input: SingleToken = {
+      name: 'typography.alias',
+      type: TokenTypes.BOX_SHADOW,
+      value: '{typography.headlines.small}',
+    };
+    expect(getAliasValue(input, tokens)).toEqual({
+      x: 2,
+      y: 2,
+      blur: 2,
+      spread: 2,
+      color: '#000000',
+      type: BoxShadowTypes.DROP_SHADOW,
+    });
+  });
+  it('font-family alias test', () => {
+    const input: SingleToken = {
+      name: 'font-family.alias',
+      type: TokenTypes.FONT_FAMILIES,
+      value: '{font-family.serif}',
+    };
+    expect(getAliasValue(input, tokens)).toEqual('IBM Plex Serif');
   });
 });
