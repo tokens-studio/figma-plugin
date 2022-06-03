5d8fc5949db77ee36c5fa89ecbb3d690c54fd899
Jan Six <six.jan@gmail.com>
2022-06-04 01:20:37 +0200

    update pr, bump release

--- src/app/store/providers/github/getGithubCreatePullRequestUrl.ts
+++ src/app/store/providers/github/getGithubCreatePullRequestUrl.ts
@@ -1,13 +1,14 @@
 /** Returns a URL to a page where the user can create a pull request with a given branch */
 export function getGithubCreatePullRequestUrl({
-  base = 'https://github.com', repo, branch,
+  base, repo, branch,
 }: {
   base?: string,
   repo: string,
   branch?: string
 }) {
+  const url = base && base !== '' ? base : 'https://github.com';
   // We need to remove the `api/v3` part of the URL for GHE instances
-  const baseUrl = base.replace('/api/v3', '');
+  const baseUrl = url.replace('/api/v3', '');
 
   return `${baseUrl}/${repo}/compare/${branch}?expand=1`;
 }
