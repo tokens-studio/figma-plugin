29035f832adad51b7ce1629bfc214d8715b279ff
SwordEdge <yamadera46@hotmail.com>
2022-06-02 23:58:34 -0700

    Bug composition tokens on hover showing box shadow as object object (#803)
    * skip to start screen when create new file
    
    * resolve test error
    
    * add env
    
    * fix: preview composition token for boxshadow and typography
    
    * add case when typoof value is number
    
    * modify css
    
    * code refresh
    
    * convert useCallback to useMemo
    
    Co-authored-by: hiroshi <robinhoodie0823@gmail.com>

--- src/app/components/TokenTooltip/SingleCompositionValueDisplay.tsx
+++ src/app/components/TokenTooltip/SingleCompositionValueDisplay.tsx
@@ -1,16 +1,52 @@
 import React from 'react';
-import { TokenBoxshadowValue } from '@/types/values';
+import { styled } from '@/stitches.config';
 import Box from '../Box';
+import { SingleToken } from '@/types/tokens';
 
 type Props = {
   property: string;
-  value: string | number | Array<TokenBoxshadowValue> | object
+  value: SingleToken['value'] | number
 };
 
-export const SingleCompositionValueDisplay: React.FC<Props> = ({ property, value }) => (
-  <Box css={{ color: '$fgToolTipMuted' }}>
-    {property}
-    {' : '}
-    {value.toString()}
-  </Box>
-);
+const StyledItemValue = styled('div', {
+  fontWeight: '$bold',
+  textOverflow: 'ellipsis',
+  overflow: 'hidden',
+  maxWidth: '300px',
+});
+
+const StyledItemName = styled('div', {
+  flexShrink: 0,
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+});
+
+export const SingleCompositionValueDisplay: React.FC<Props> = ({ property, value }) => {
+  const resolveValue = React.useMemo(() => {
+    let returnValue: string = '';
+    if (Array.isArray(value)) {
+      returnValue = value.reduce<string>((totalAcc, item) => {
+        const singleReturnValue = Object.entries(item).reduce<string>((acc, [, propertyValue]) => (
+          `${acc}${propertyValue.toString()}/`
+        ), '');
+        return `${totalAcc}${singleReturnValue},`;
+      }, '');
+    } else if (typeof value === 'object') {
+      returnValue = Object.entries(value).reduce<string>((acc, [, propertyValue]) => (
+        `${acc}${propertyValue.toString()}/`
+      ), '');
+    } else if (typeof value === 'number') {
+      returnValue = value.toString();
+    } else {
+      returnValue = value;
+    }
+    return returnValue;
+  }, [value]);
+
+  return (
+    <Box css={{ color: '$fgToolTipMuted', display: 'flex', gap: '$2' }}>
+      <StyledItemName>{`${property} : `}</StyledItemName>
+      <StyledItemValue>{resolveValue}</StyledItemValue>
+    </Box>
+  );
+};
