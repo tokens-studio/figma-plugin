3361ab9de3bff0323eb8afce3b45b4123841c099
Liam Martens <hi@liammartens.com>
2022-06-05 19:48:14 -0400

    fix: Fixed wrong property

--- src/figmaStorage/UiSettingsProperty.ts
+++ src/figmaStorage/UiSettingsProperty.ts
@@ -4,7 +4,7 @@ import { FigmaStorageProperty, FigmaStorageType } from './FigmaStorageProperty';
 
 export const UiSettingsProperty = new FigmaStorageProperty<Partial<SavedSettings>>(
   FigmaStorageType.CLIENT_STORAGE,
-  'apiProviders',
+  'uiSettings',
   (incoming) => JSON.stringify(incoming),
   (outgoing) => tryParseJson<Partial<SavedSettings>>(outgoing) ?? {},
 );
