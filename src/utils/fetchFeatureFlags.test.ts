67ffd4e4f58a4b139958ccd8ccdae7464f5e2eef
hiroshi <robinhoodie0823@gmail.com>
2022-06-09 20:16:47 +0900

    test coverage for fetchFeatureFlags

---
+++ src/utils/fetchFeatureFlags.test.ts
@@ -0,0 +1,47 @@
+import fetchFeatureFlags from './fetchFeatureFlags';
+
+const mockWaitUntilReady = jest.fn();
+const mockAllFlags = jest.fn();
+
+jest.mock('launchdarkly-js-client-sdk', () => ({
+  initialize: jest.fn().mockImplementation(() => ({
+    waitUntilReady: mockWaitUntilReady,
+    allFlags: mockAllFlags,
+  })),
+}));
+
+describe('fetchFeatureFlags', (() => {
+  it('return flags', (async () => {
+    const userData = {
+      userId: 'six7',
+      licenseKey: 'licenseKey',
+    };
+    global.fetch = jest.fn(() => (
+      Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => Promise.resolve({
+          email: 'six7@brandcode.dev',
+          entitlements: ['pro'],
+          plan: 'Tokens pro',
+        }),
+      }) as Promise<Response>
+    ));
+    mockWaitUntilReady.mockImplementationOnce(() => ({}));
+    mockAllFlags.mockImplementationOnce(() => (
+      {
+        'git-branch-selector': true,
+        'multi-file-sync': true,
+        'token-themes': true,
+      }
+    ));
+    const flags = await fetchFeatureFlags(userData);
+    expect(flags).toEqual(
+      {
+        gitBranchSelector: true,
+        multiFileSync: true,
+        tokenThemes: true,
+      },
+    );
+  }));
+}));
