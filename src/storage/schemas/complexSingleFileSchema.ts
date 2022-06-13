8f8e9abab522efb369142156d2b7db5e0e75e523
MightyDevKnight <takao.kamio@brandcode.dev>
2022-06-13 15:55:44 +0900

    resolve conflict

--- src/storage/schemas/complexSingleFileSchema.ts
+++ src/storage/schemas/complexSingleFileSchema.ts
@@ -1,14 +1,15 @@
 import z from 'zod';
+import { singleTokenSchema } from './singleTokenSchema';
 import { themeObjectSchema } from './themeObjectSchema';
-import { tokensMapSchema } from './tokensMapSchema';
-import type { DeepTokensMap, ThemeObjectsList } from '@/types';
+import type { SingleToken } from '@/types/tokens';
+import type { ThemeObjectsList } from '@/types';
 
 // @README this schema applies to Git type providers
 
 interface ComplexSingleFileFormat {
-  [K: string]: ThemeObjectsList | DeepTokensMap<false>
+  [K: string]: SingleToken<false> | ThemeObjectsList
 }
 
 export const complexSingleFileSchema: z.ZodType<ComplexSingleFileFormat> = z.lazy(() => (
-  (z.record(tokensMapSchema.or(z.array(themeObjectSchema))) as z.ZodType<ComplexSingleFileFormat>)
+  (z.record(z.record(singleTokenSchema).or(z.array(themeObjectSchema).optional())) as z.ZodType<ComplexSingleFileFormat>)
 ));
