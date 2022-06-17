3234b4df411d2a7e6c5961e115bb12698f4af009
MightyDevKnight <takao.kamio@brandcode.dev>
2022-06-18 00:34:52 +0900

    add test of create token set

--- cypress/integration/manage-themes.spec.js
+++ cypress/integration/manage-themes.spec.js
@@ -1,13 +1,56 @@
-describe('Themes', () => {
-    beforeEach(() => {
-      cy.visit('/', {
-        onBeforeLoad(win) {
-          cy.spy(win, 'postMessage').as('postMessage');
-        },
-      });
-      cy.waitForReact(1000);
-    });  
-    it('Can create a new token-set', () => {
-      
-    });
+function receiveRemoteComponents() {
+  cy.window().then(($window) => {
+    const message = {
+      pluginMessage: {
+        type: 'remotecomponents',
+        values: { remotes: [] },
+      },
+    };
+    $window.postMessage(message, '*');
   });
+}
+
+const createToken = ({ name }) => {
+  cy.get('[data-cy="button-new-token-set"]').click({ timeout: 1000 })
+      .get('[data-cy="token-set-input"]')
+      .type(name).type('{enter}');
+};
+
+describe('TokenListing', () => {
+  beforeEach(() => {
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        cy.spy(win, 'postMessage').as('postMessage');
+      },
+    });
+    cy.waitForReact(1000);
+  });
+
+  it('create token set', () => {
+    cy.receiveSetTokens({
+      version: '5',
+      values: {
+        options: [{
+          name: 'sizing.xs',
+          value: 4,
+          type: 'sizing'
+        }],
+        global: [{
+          name: 'sizing.xs',
+          value: 4,
+          type: 'sizing'
+        }],
+      },
+    });
+    cy.receiveStorageTypeLocal();
+    
+    createToken({ name: 'token1' });
+    createToken({ name: 'token2' });
+    createToken({ name: 'token3' });
+
+    
+    cy.get('@postMessage').should('be.calledTwice');
+    receiveRemoteComponents();
+  });
+  
+});
