304c2755cf2681bf8eb1923a7ad7c9d444b747e4
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-06-21 13:24:00 -0500

    add bitbucket to credentials logic in Initiator

--- src/app/components/Initiator.tsx
+++ src/app/components/Initiator.tsx
@@ -149,9 +149,7 @@ export function Initiator() {
             setStorageType({ provider: pluginMessage.storageType });
             break;
           case MessageFromPluginTypes.API_CREDENTIALS: {
-            const {
-              status, credentials, usedTokenSet, activeTheme, shouldPull, featureFlags,
-            } = pluginMessage;
+            const { status, credentials, usedTokenSet, activeTheme, shouldPull, featureFlags } = pluginMessage;
             if (status === true) {
               const receivedFlags: LDProps['flags'] = featureFlags;
               try {
@@ -174,7 +172,10 @@ export function Initiator() {
 
                   if (shouldPull) {
                     const remoteData = await pullTokens({
-                      context: credentials, featureFlags: receivedFlags, usedTokenSet, activeTheme,
+                      context: credentials,
+                      featureFlags: receivedFlags,
+                      usedTokenSet,
+                      activeTheme,
                     });
                     const existTokens = Object.values(remoteData?.tokens ?? {}).some((value) => value.length > 0);
                     if (existTokens) {
