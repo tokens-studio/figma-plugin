d91ef26cd85bcdc80769b127dbd9be3478ac8228
Matt Oliver <mattoliver.mattoliver@gmail.com>
2022-06-21 19:36:07 -0500

    repositoriesCreateSrcFileCommit type extension in progress

--- src/app/store/remoteTokens.tsx
+++ src/app/store/remoteTokens.tsx
@@ -133,7 +133,7 @@ export default function useRemoteTokens() {
       pullTokensFromJSONBin,
       pullTokensFromURL,
       pullTokensFromADO,
-    ]
+    ],
   );
 
   const restoreStoredProvider = useCallback(
@@ -173,7 +173,7 @@ export default function useRemoteTokens() {
       syncTokensWithGitLab,
       syncTokensWithBitbucket,
       syncTokensWithADO,
-    ]
+    ],
   );
 
   const pushTokens = useCallback(
@@ -200,7 +200,7 @@ export default function useRemoteTokens() {
           throw new Error('Not implemented');
       }
     },
-    [api, pushTokensToGitHub, pushTokensToGitLab, pushTokensToBitbucket, pushTokensToADO]
+    [api, pushTokensToGitHub, pushTokensToGitLab, pushTokensToBitbucket, pushTokensToADO],
   );
 
   const addNewProviderItem = useCallback(
@@ -260,7 +260,7 @@ export default function useRemoteTokens() {
       createNewJSONBin,
       pullTokensFromURL,
       setStorageType,
-    ]
+    ],
   );
 
   const addNewBranch = useCallback(
@@ -289,7 +289,7 @@ export default function useRemoteTokens() {
 
       return newBranchCreated;
     },
-    [createGithubBranch, createADOBranch, createBitbucketBranch]
+    [createGithubBranch, createADOBranch, createBitbucketBranch],
   );
 
   const fetchBranches = useCallback(
@@ -307,7 +307,7 @@ export default function useRemoteTokens() {
           return null;
       }
     },
-    [fetchGithubBranches, fetchGitLabBranches, fetchBitbucketBranches, fetchADOBranches]
+    [fetchGithubBranches, fetchGitLabBranches, fetchBitbucketBranches, fetchADOBranches],
   );
 
   const deleteProvider = useCallback((provider) => {
@@ -327,6 +327,6 @@ export default function useRemoteTokens() {
       fetchBranches,
       addNewBranch,
     }),
-    [restoreStoredProvider, deleteProvider, pullTokens, pushTokens, addNewProviderItem, fetchBranches, addNewBranch]
+    [restoreStoredProvider, deleteProvider, pullTokens, pushTokens, addNewProviderItem, fetchBranches, addNewBranch],
   );
 }
