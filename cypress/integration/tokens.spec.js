d330b258a37f2bc56eb11ba64ff1018d95b1c4a8
hiroshi <robinhoodie0823@gmail.com>
2022-06-24 22:14:13 +0900

    select composition token

--- cypress/integration/tokens.spec.js
+++ cypress/integration/tokens.spec.js
@@ -44,38 +44,195 @@ describe('TokenListing', () => {
     cy.waitForReact(1000);
   });
 
-  it('can add a new token', () => {
-    cy.receiveSetTokens({
-      version: '5',
-      values: {
-        options: [{
-          name: 'sizing.xs',
-          value: 4,
-          type: 'sizing'
-        }],
-        global: [{
-          name: 'sizing.xs',
-          value: 4,
-          type: 'sizing'
-        }],
-      },
-    });
-    cy.receiveStorageTypeLocal();
-    cy.get('[data-cy=tokenlisting-sizing] [data-cy=button-add-new-token]').click({ timeout: 1000 });
-    fillTokenForm({
-      name: 'sizing.sm',
-      value: '4',
-    });
-    cy.get('@postMessage').should('be.calledTwice');
-    receiveRemoteComponents();
-    cy.get('[data-cy=tokenlisting-sizing] [data-cy=button-add-new-token]').click({ timeout: 1000 });
-    fillTokenForm({
-      name: 'sizing.md',
-      value: '$sizing.sm * 2',
-    });
-  });
+  // it('can add a new token', () => {
+  //   cy.receiveSetTokens({
+  //     version: '5',
+  //     values: {
+  //       options: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //       global: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //     },
+  //   });
+  //   cy.receiveStorageTypeLocal();
+  //   cy.get('[data-cy=tokenlisting-sizing] [data-cy=button-add-new-token]').click({ timeout: 1000 });
+  //   fillTokenForm({
+  //     name: 'sizing.sm',
+  //     value: '4',
+  //   });
+  //   cy.get('@postMessage').should('be.calledTwice');
+  //   receiveRemoteComponents();
+  //   cy.get('[data-cy=tokenlisting-sizing] [data-cy=button-add-new-token]').click({ timeout: 1000 });
+  //   fillTokenForm({
+  //     name: 'sizing.md',
+  //     value: '$sizing.sm * 2',
+  //   });
+  // });
 
-  it('can add a new shadow token', () => {
+  // it('can add a new shadow token', () => {
+  //   cy.receiveSetTokens({
+  //     version: '5',
+  //     values: {
+  //       options: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //       global: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //     },
+  //   });
+  //   cy.receiveStorageTypeLocal();
+  //   cy.get('[data-cy=tokenlisting-boxShadow] [data-cy=button-add-new-token]').click({ timeout: 1000 });
+  //   fillInput({
+  //     input: 'name',
+  //     value: 'boxshadow.regular',
+  //   });
+  //   fillInput({
+  //     input: 'x',
+  //     value: '4',
+  //   });
+  //   fillInput({
+  //     input: 'y',
+  //     value: '4',
+  //   });
+  //   fillInput({
+  //     input: 'spread',
+  //     value: '0',
+  //   });
+  //   fillInput({
+  //     input: 'color',
+  //     value: '#ff0000',
+  //   });
+  //   fillInput({
+  //     input: 'blur',
+  //     value: '0',
+  //     submit: true,
+  //   });
+
+  //   cy.get('@postMessage').should('be.calledTwice');
+  //   receiveRemoteComponents();
+  // });
+
+  // it('can add multiple shadow tokens', () => {
+  //   cy.receiveSetTokens({
+  //     version: '5',
+  //     values: {
+  //       options: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //       global: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //     },
+  //   });
+  //   cy.receiveStorageTypeLocal();
+  //   cy.get('[data-cy=tokenlisting-boxShadow] [data-cy=button-add-new-token]').click({ timeout: 1000 });
+  //   fillInput({
+  //     input: 'name',
+  //     value: 'boxshadow.large',
+  //   });
+  //   fillInput({
+  //     input: 'x',
+  //     value: '4',
+  //   });
+  //   fillInput({
+  //     input: 'y',
+  //     value: '4',
+  //   });
+  //   fillInput({
+  //     input: 'spread',
+  //     value: '0',
+  //   });
+  //   fillInput({
+  //     input: 'color',
+  //     value: '#ff0000',
+  //   });
+  //   fillInput({
+  //     input: 'blur',
+  //     value: '0',
+  //   });
+  //   cy.get('[data-cy=button-shadow-add-multiple]').click({ timeout: 1000 });
+  //   fillInputNth({
+  //     input: 'x',
+  //     value: '4',
+  //     nth: 1,
+  //   });
+  //   fillInputNth({
+  //     input: 'y',
+  //     value: '8',
+  //     nth: 1,
+  //     submit: true,
+  //   });
+  //   cy.get('@postMessage').should('be.calledTwice');
+  //   receiveRemoteComponents();
+  // });
+
+  // it('can add a new typography token', () => {
+  //   cy.receiveSetTokens({
+  //     version: '5',
+  //     values: {
+  //       options: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //       global: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //     },
+  //   });
+  //   cy.receiveStorageTypeLocal();
+  //   cy.get('[data-cy=tokenlisting-typography] [data-cy=button-add-new-token]').click({ timeout: 1000 });
+  //   fillInput({
+  //     input: 'name',
+  //     value: 'typography.regular',
+  //   });
+  //   fillInput({
+  //     input: 'fontFamily',
+  //     value: 'Inter',
+  //   });
+  //   fillInput({
+  //     input: 'fontWeight',
+  //     value: 'Bold',
+  //   });
+  //   fillInput({
+  //     input: 'lineHeight',
+  //     value: '100%',
+  //   });
+  //   fillInput({
+  //     input: 'fontSize',
+  //     value: '14',
+  //   });
+  //   fillInput({
+  //     input: 'letterSpacing',
+  //     value: '0',
+  //   });
+  //   fillInput({
+  //     input: 'paragraphSpacing',
+  //     value: '0',
+  //     submit: true,
+  //   });
+  //   cy.get('@postMessage').should('be.calledTwice');
+  //   receiveRemoteComponents();
+  // });
+
+  it('can add a new composition token', () => {
     cy.receiveSetTokens({
       version: '5',
       values: {
@@ -88,201 +245,92 @@ describe('TokenListing', () => {
           name: 'sizing.xs',
           value: 4,
           type: 'sizing'
-        }],
+        }, {
+          name: 'opacity.30',
+          value: '30%',
+          type: 'opacity'
+        }, {
+          name: 'font-size.4',
+          value: '4px',
+          type: 'fontSizes'
+        },],
       },
     });
     cy.receiveStorageTypeLocal();
-    cy.get('[data-cy=tokenlisting-boxShadow] [data-cy=button-add-new-token]').click({ timeout: 1000 });
+    cy.get('[data-cy=tokenlisting-composition] [data-cy=button-add-new-token]').click({ timeout: 1000 });
     fillInput({
       input: 'name',
-      value: 'boxshadow.regular',
+      value: 'composition.regular',
     });
+    cy.get('[daty-cy=composition-property]').click();
+    cy.get('[daty-cy=composition-property--sizing]').click();
     fillInput({
-      input: 'x',
-      value: '4',
+      input: 'alias',
+      value: '{sizing.xs}',
     });
-    fillInput({
-      input: 'y',
-      value: '4',
-    });
-    fillInput({
-      input: 'spread',
-      value: '0',
-    });
-    fillInput({
-      input: 'color',
-      value: '#ff0000',
-    });
-    fillInput({
-      input: 'blur',
-      value: '0',
-      submit: true,
-    });
+    cy.get('[data-cy=button-style-add-multiple]').click();
+
+    cy.get('[daty-cy=composition-property]').eq(1).click();
+    cy.get('[daty-cy=composition-property--opacity]').eq(1).click();
+
+
 
     cy.get('@postMessage').should('be.calledTwice');
     receiveRemoteComponents();
   });
 
-  it('can add multiple shadow tokens', () => {
-    cy.receiveSetTokens({
-      version: '5',
-      values: {
-        options: [{
-          name: 'sizing.xs',
-          value: 4,
-          type: 'sizing'
-        }],
-        global: [{
-          name: 'sizing.xs',
-          value: 4,
-          type: 'sizing'
-        }],
-      },
-    });
-    cy.receiveStorageTypeLocal();
-    cy.get('[data-cy=tokenlisting-boxShadow] [data-cy=button-add-new-token]').click({ timeout: 1000 });
-    fillInput({
-      input: 'name',
-      value: 'boxshadow.large',
-    });
-    fillInput({
-      input: 'x',
-      value: '4',
-    });
-    fillInput({
-      input: 'y',
-      value: '4',
-    });
-    fillInput({
-      input: 'spread',
-      value: '0',
-    });
-    fillInput({
-      input: 'color',
-      value: '#ff0000',
-    });
-    fillInput({
-      input: 'blur',
-      value: '0',
-    });
-    cy.get('[data-cy=button-shadow-add-multiple]').click({ timeout: 1000 });
-    fillInputNth({
-      input: 'x',
-      value: '4',
-      nth: 1,
-    });
-    fillInputNth({
-      input: 'y',
-      value: '8',
-      nth: 1,
-      submit: true,
-    });
-    cy.get('@postMessage').should('be.calledTwice');
-    receiveRemoteComponents();
-  });
 
-  it('can add a new typography token', () => {
-    cy.receiveSetTokens({
-      version: '5',
-      values: {
-        options: [{
-          name: 'sizing.xs',
-          value: 4,
-          type: 'sizing'
-        }],
-        global: [{
-          name: 'sizing.xs',
-          value: 4,
-          type: 'sizing'
-        }],
-      },
-    });
-    cy.receiveStorageTypeLocal();
-    cy.get('[data-cy=tokenlisting-typography] [data-cy=button-add-new-token]').click({ timeout: 1000 });
-    fillInput({
-      input: 'name',
-      value: 'typography.regular',
-    });
-    fillInput({
-      input: 'fontFamily',
-      value: 'Inter',
-    });
-    fillInput({
-      input: 'fontWeight',
-      value: 'Bold',
-    });
-    fillInput({
-      input: 'lineHeight',
-      value: '100%',
-    });
-    fillInput({
-      input: 'fontSize',
-      value: '14',
-    });
-    fillInput({
-      input: 'letterSpacing',
-      value: '0',
-    });
-    fillInput({
-      input: 'paragraphSpacing',
-      value: '0',
-      submit: true,
-    });
-    cy.get('@postMessage').should('be.calledTwice');
-    receiveRemoteComponents();
-  });
+  // it('can add a new token in group', () => {
+  //   cy.receiveSetTokens({
+  //     version: '5',
+  //     values: {
+  //       options: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //       global: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //     },
+  //   });
+  //   cy.receiveStorageTypeLocal();
+  //   cy.get(
+  //     '[data-cy=tokenlisting-sizing] [data-cy=token-group-sizing] [data-cy=button-add-new-token-in-group]',
+  //   ).click({ timeout: 1000 });
+  //   fillTokenForm({
+  //     name: 'lg',
+  //     value: '8',
+  //   });
+  //   cy.get('@postMessage').should('be.calledTwice');
+  //   receiveRemoteComponents();
+  // });
 
-  it('can add a new token in group', () => {
-    cy.receiveSetTokens({
-      version: '5',
-      values: {
-        options: [{
-          name: 'sizing.xs',
-          value: 4,
-          type: 'sizing'
-        }],
-        global: [{
-          name: 'sizing.xs',
-          value: 4,
-          type: 'sizing'
-        }],
-      },
-    });
-    cy.receiveStorageTypeLocal();
-    cy.get(
-      '[data-cy=tokenlisting-sizing] [data-cy=token-group-sizing] [data-cy=button-add-new-token-in-group]',
-    ).click({ timeout: 1000 });
-    fillTokenForm({
-      name: 'lg',
-      value: '8',
-    });
-    cy.get('@postMessage').should('be.calledTwice');
-    receiveRemoteComponents();
-  });
-
-  it('token listing stays collapsed after creating a new token', () => {
-    cy.receiveSetTokens({
-      version: '5',
-      values: {
-        options: [{
-          name: 'sizing.xs',
-          value: 4,
-          type: 'sizing'
-        }],
-        global: [{
-          name: 'sizing.xs',
-          value: 4,
-          type: 'sizing'
-        }],
-      },
-    });
-    cy.receiveStorageTypeLocal();
-    cy.get('[data-cy=tokenlisting-header-sizing]').click({ timeout: 1000 });
-    cy.get('[data-cy=tokenlisting-opacity] [data-cy=button-add-new-token]').click({ timeout: 1000 });
-    fillTokenForm({
-      name: 'sizing.sm',
-      value: '4',
-    });
-    cy.get('[data-cy=tokenlisting-sizing-content]').should('have.class', 'hidden');
-  });
+  // it('token listing stays collapsed after creating a new token', () => {
+  //   cy.receiveSetTokens({
+  //     version: '5',
+  //     values: {
+  //       options: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //       global: [{
+  //         name: 'sizing.xs',
+  //         value: 4,
+  //         type: 'sizing'
+  //       }],
+  //     },
+  //   });
+  //   cy.receiveStorageTypeLocal();
+  //   cy.get('[data-cy=tokenlisting-header-sizing]').click({ timeout: 1000 });
+  //   cy.get('[data-cy=tokenlisting-opacity] [data-cy=button-add-new-token]').click({ timeout: 1000 });
+  //   fillTokenForm({
+  //     name: 'sizing.sm',
+  //     value: '4',
+  //   });
+  //   cy.get('[data-cy=tokenlisting-sizing-content]').should('have.class', 'hidden');
+  // });
 });
