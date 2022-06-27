7997c6f4c9d0650cd2b2128b8554b57177b538ed
Liam Martens <hi@liammartens.com>
2022-06-27 16:56:02 -0400

    fix: Check for array when applying sorting order from metadata file

--- src/utils/tokenset/applyTokenSetOrder.ts
+++ src/utils/tokenset/applyTokenSetOrder.ts
@@ -1,10 +1,14 @@
 import type { AnyTokenList } from '@/types/tokens';
 
-export function applyTokenSetOrder(tokenSets: Record<string, AnyTokenList>, order: string[]) {
+export function applyTokenSetOrder(tokenSets: Record<string, AnyTokenList>, order: (string[] | null | undefined) = null) {
+  if (!order || !Array.isArray(order)) {
+    return tokenSets;
+  }
+
   return Object.fromEntries(
     Object.entries(tokenSets).sort((a, b) => {
-      const indexOfA = order.indexOf(a[0]);
-      const indexOfB = order.indexOf(b[0]);
+      const indexOfA = order?.indexOf(a[0]) ?? -1;
+      const indexOfB = order?.indexOf(b[0]) ?? -1;
       return indexOfA < indexOfB ? -1 : 1;
     }),
   );
