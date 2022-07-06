8c88b6862d46eb91b8fc7cc0d5911e3d11ee3db6
hiroshi <robinhoodie0823@gmail.com>
2022-07-06 16:57:11 +0900

    add default tokens

---
+++ cypress/integration/inspector.spec.js
@@ -0,0 +1,40 @@
+describe('Inspector tokens', () => {
+  beforeEach(() => {
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        cy.spy(win, 'postMessage').as('postMessage');
+      },
+    });
+    cy.waitForReact(1000);
+    MockEnv();
+  });
+
+  it('successfully shows tokens applied on the node', () => {
+    cy.receiveSetTokens({
+      version: '5',
+      values: {
+        options: [{
+            name: 'sizing.xs',
+            value: 4,
+            type: 'sizing'
+          },
+          {
+            name: 'opacity.50',
+            value: '50%',
+            type: 'opacity'
+          },
+          {
+            name: 'font-size.12',
+            value: '12px',
+            type: 'fontSizes'
+          }
+        ],
+        global: [{
+          name: 'sizing.xs',
+          value: 4,
+          type: 'sizing'
+        }],
+      },
+    });
+  });
+});
