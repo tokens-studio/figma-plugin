d8fcd972a1d4440c9e58b70f14983cf07d745f6c
MightyDevKnight <takao.kamio@brandcode.dev>
2022-07-15 14:32:44 +0900

    update getformattedtokens func

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 import Box from './Box';
 import { Tabs } from '@/constants/Tabs';
 import Stack from './Stack';
@@ -7,9 +9,38 @@ import { NavbarUndoButton } from './NavbarUndoButton';
 import Minimize from '../assets/minimize.svg';
 import useMinimizeWindow from './useMinimizeWindow';
 import IconButton from './IconButton';
+import { IconFolder } from '@/icons';
+import useTokens from '@/app/store/useTokens';
+import { activeThemeSelector, themeOptionsSelector, usedTokenSetSelector } from '@/selectors';
 
 const Navbar: React.FC = () => {
   const { handleResize } = useMinimizeWindow();
+  const { getFormattedTokens } = useTokens();
+  const activeTheme = useSelector(activeThemeSelector);
+  const availableThemes = useSelector(themeOptionsSelector);
+  const usedTokenSet = useSelector(usedTokenSetSelector);
+
+  const handleOpenTokenFlowApp = useCallback(async () => {
+    const tokens = getFormattedTokens({
+      includeAllTokens: true,
+      includeParent: false,
+      expandTypography: false,
+      expandShadow: false,
+      expandComposition: false,
+    });
+    const tokenData = JSON.stringify(tokens, null, 2);
+    const response = await axios({
+      method: 'post',
+      url: 'http://localhost:3000/api/tokens',
+      data: {
+        tokenData,
+        activeTheme,
+        availableThemes,
+        usedTokenSet,
+      },
+    });
+    if (response.status === 200) window.open(`http://localhost:3000?id=${response.data.result}`);
+  }, [activeTheme, availableThemes, getFormattedTokens, usedTokenSet]);
 
   return (
     <Box
@@ -33,6 +64,7 @@ const Navbar: React.FC = () => {
         </div>
         <NavbarUndoButton />
       </Stack>
+      <IconButton tooltip="open tokenflow app" onClick={handleOpenTokenFlowApp} icon={<IconFolder />} />
       <Stack direction="row" align="center">
         <IconButton tooltip="Minimize plugin" onClick={handleResize} icon={<Minimize />} />
       </Stack>
