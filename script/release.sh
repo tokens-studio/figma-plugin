f8aceddf3e53bb9cf574adb0e10404915377b66f
Jan Six <six7@github.com>
2022-07-21 08:26:42 +0200

    bump release-script

--- script/release.sh
+++ script/release.sh
@@ -1,4 +1,4 @@
-VERSION=figma-tokens@112
+VERSION=figma-tokens@113
 sentry-cli releases -p figma-tokens files "$VERSION" upload-sourcemaps --ext ts --ext tsx --ext map --ext js --ignore-file .sentryignore .
 sentry-cli releases set-commits "$VERSION" --auto
 sentry-cli releases finalize "$VERSION"
