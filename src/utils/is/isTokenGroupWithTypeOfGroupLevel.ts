3184caeac85497eaaafee383ed11c4bff9465abc
hiroshi <robinhoodie0823@gmail.com>
2022-07-21 09:54:39 +0900

    code refresh

--- src/utils/is/isTokenGroupWithTypeOfGroupLevel.ts
+++ src/utils/is/isTokenGroupWithTypeOfGroupLevel.ts
@@ -1,18 +1,9 @@
 import { SingleToken } from '@/types/tokens';
 
 type SingleTokenValueObject = Pick<SingleToken, 'value'>;
-type TokenGroupWithType = Pick<SingleToken, 'value'> & {type: string};
+type TokenGroupWithType = Pick<SingleToken, 'value'> & { type: string };
 
-export function isTokenGroupWithTypeOfGroupLevel(token: SingleTokenValueObject | any): token is TokenGroupWithType  {
-  console.log("token", token, !!(
-    token
-    && typeof token === 'object'
-    && (!('value' in token) || ('value' in token && typeof token.value === 'object' && 'value' in token.value))
-    && 'type' in token
-    && (
-      typeof token.type === 'string'
-    )
-  ))
+export function isTokenGroupWithTypeOfGroupLevel(token: SingleTokenValueObject | any): token is TokenGroupWithType {
   return !!(
     token
     && typeof token === 'object'
