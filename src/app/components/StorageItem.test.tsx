a0cb710ec53803b5036391c84b72d01ea80280e6
hiroshi <robinhoodie0823@gmail.com>
2022-07-27 18:45:23 +0900

    fix test coverage

--- src/app/components/StorageItem.test.tsx
+++ src/app/components/StorageItem.test.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import * as reactRedux from 'react-redux';
 import { render } from '../../../tests/config/setupTest';
 import StorageItem from './StorageItem';
 
-const useSelectorMock = jest.spyOn(reactRedux, 'useSelector');
-const activeProvideer = {
-  id: 'defalut',
-  provider: 'github',
-  branch: 'main',
-  filePath: 'si7/figma-token',
-};
 const gitProvider = {
   id: 'other',
   provider: 'github',
@@ -19,27 +11,10 @@ const gitProvider = {
 const onEdit = () => {};
 
 describe('StorageItem', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
   it('should render storageItem', () => {
     const { getByText } = render(<StorageItem item={gitProvider} onEdit={onEdit} />);
     expect(getByText('Edit')).toBeInTheDocument();
     expect(getByText('Apply')).toBeInTheDocument();
-    expect(getByText('Delete local credentials')).toBeInTheDocument();
-  });
-
-  it('should render delete button when item is active', () => {
-    useSelectorMock.mockReturnValue({
-      id: 'defalut',
-      provider: 'github',
-      branch: 'main',
-      filePath: 'si7/figma-token',
-    });
-    const { getByText, queryByText } = render(<StorageItem item={activeProvideer} onEdit={onEdit} />);
-    expect(getByText('Edit')).toBeInTheDocument();
-    expect(queryByText('Apply')).not.toBeInTheDocument();
     expect(getByText('Delete local credentials')).toBeInTheDocument();
   });
 });
