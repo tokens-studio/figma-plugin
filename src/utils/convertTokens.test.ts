9bd504d3cb27687797427d52bb9e28e7d259e90a
hiroshi <robinhoodie0823@gmail.com>
2022-08-04 10:57:12 +0900

    add test coverage for composition token

--- src/utils/convertTokens.test.ts
+++ src/utils/convertTokens.test.ts
@@ -24,6 +24,22 @@ describe('convertToTokenArray', () => {
         },
       },
     };
+    const compositionTokens = {
+      input: {
+        value: {
+          sizing: '{global.groupWithType.small}',
+          opacity: '{global.opacity.50}',
+        },
+        type: 'composition',
+      },
+      output: {
+        value: {
+          sizing: '{global.groupWithType.small}',
+          opacity: '{global.opacity.50}',
+        },
+        type: 'composition',
+      },
+    };
     const basicTokens = {
       global: {
         withValue: {
@@ -35,6 +51,15 @@ describe('convertToTokenArray', () => {
           heading: {
             h2: typographyTokens.withValue.input,
           },
+        },
+        opacity: {
+          50: {
+            type: 'opacity',
+            value: '50%',
+          },
+        },
+        composition: {
+          heading: compositionTokens.input,
         },
         groupWithType: {
           type: 'sizing',
@@ -65,6 +90,8 @@ describe('convertToTokenArray', () => {
       { name: 'global.withValue', value: 'bar', type: 'other' },
       { name: 'global.basic', value: '#ff0000' },
       { ...typographyTokens.withValue.output, name: 'global.typography.heading.h2' },
+      { name: 'global.opacity.50', value: '50%', type: 'opacity' },
+      { ...compositionTokens.output, name: 'global.composition.heading' },
       {
         name: 'global.groupWithType.small', value: '12px', type: 'sizing', inheritTypeLevel: 3,
       },
@@ -81,6 +108,8 @@ describe('convertToTokenArray', () => {
       { name: 'global.withValue', value: 'bar', type: 'other' },
       { name: 'global.basic', value: '#ff0000' },
       { ...typographyTokens.withValue.output, name: 'global.typography.heading.h2' },
+      { name: 'global.opacity.50', value: '50%', type: 'opacity' },
+      { ...compositionTokens.output, name: 'global.composition.heading' },
       {
         name: 'global.groupWithType.small', value: '12px', type: 'sizing', inheritTypeLevel: 3,
       },
