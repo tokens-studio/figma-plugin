ce6d2945231a5b16470bf867e58b1fe96832a7a3
hiroshi <robinhoodie0823@gmail.com>
2022-08-12 12:30:52 +0900

    test for modals

--- src/app/components/SyncSettings.test.tsx
+++ src/app/components/SyncSettings.test.tsx
@@ -10,7 +10,7 @@ import { StorageProviderType } from '@/constants/StorageProviderType';
 import { GitStorageType, StorageTypeCredential } from '@/types/StorageType';
 
 describe('ConfirmDialog', () => {
-  const mockStore = createMockStore({
+  const defaultStore = {
     uiState: {
       localApiState: {
         branch: 'main',
@@ -33,9 +33,10 @@ describe('ConfirmDialog', () => {
         } as StorageTypeCredential<GitStorageType>,
       ],
     },
-  });
+  };
 
   it('should return sync settings and stored Providers', async () => {
+    const mockStore = createMockStore(defaultStore);
     const result = render(
       <Provider store={mockStore}>
         <SyncSettings />
@@ -51,7 +52,8 @@ describe('ConfirmDialog', () => {
     expect(result.queryByText('Add new credentials')).toBeInTheDocument();
   });
 
-  it('should return ConfirmLocalStorageModal', async () => {
+  it('should return ConfirmLocalStorageModal when seleting local storage', async () => {
+    const mockStore = createMockStore(defaultStore);
     const result = render(
       <Provider store={mockStore}>
         <SyncSettings />
@@ -63,5 +65,65 @@ describe('ConfirmDialog', () => {
     });
 
     expect(result.queryByText('Set to document storage?')).toBeInTheDocument();
+  });
+
+  it('can convert to localStorage', async () => {
+    const mockStore = createMockStore(defaultStore);
+    const result = render(
+      <Provider store={mockStore}>
+        <SyncSettings />
+      </Provider>,
+    );
+
+    await act(async () => {
+      result.queryByText('Local document')?.click();
+    });
+
+    await act(async () => {
+      result.queryByText('Yes, set to local.')?.click();
+    });
+
+    expect(mockStore.getState().uiState.localApiState).toEqual({
+      provider: 'local',
+    });
+    expect(mockStore.getState().uiState.storageType).toEqual({
+      provider: 'local',
+    });
+  });
+
+  it('should return EditStorageItemModal when edit remote storage', async () => {
+    const mockStore = createMockStore(defaultStore);
+    const result = render(
+      <Provider store={mockStore}>
+        <SyncSettings />
+      </Provider>,
+    );
+
+    await act(async () => {
+      result.queryByText('Edit')?.click();
+    });
+
+    expect(result.queryByText('Edit storage item')).toBeInTheDocument();
+  });
+
+  it('should return CreateStorageItemModal when create new remote storage', async () => {
+    const mockStore = createMockStore(defaultStore);
+    const result = render(
+      <Provider store={mockStore}>
+        <SyncSettings />
+      </Provider>,
+    );
+
+    await act(async () => {
+      result.queryByText('Add new credentials')?.click();
+    });
+
+    expect(result.queryAllByText('Add new credentials')).toHaveLength(2);
+    expect(result.queryByText('Personal Access Token')).toBeInTheDocument();
+    expect(result.queryByText('Repository (username/repo)')).toBeInTheDocument();
+    expect(result.queryByText('Default Branch')).toBeInTheDocument();
+    expect(result.queryByText('File Path (e.g. data/tokens.json)')).toBeInTheDocument();
+    expect(result.queryByText('baseUrl (optional)')).toBeInTheDocument();
+    expect(result.queryByText('Save')).toBeInTheDocument();
   });
 });
