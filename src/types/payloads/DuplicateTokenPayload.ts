d4490088e3cc2b4f3b811124980a9ffcfd098278
hiroshi <robinhoodie0823@gmail.com>
2022-08-17 01:35:50 +0900

    save value and description

--- src/types/payloads/DuplicateTokenPayload.ts
+++ src/types/payloads/DuplicateTokenPayload.ts
@@ -1,6 +1,12 @@
+import { TokenTypes } from '@/constants/TokenTypes';
+import { SingleToken } from '../tokens';
+
 export type DuplicateTokenPayload = {
   parent: string;
+  type: TokenTypes;
   newName: string;
+  value: SingleToken['value'];
+  description?: string;
   oldName?: string;
   shouldUpdate?: boolean;
 };
