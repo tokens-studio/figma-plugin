2956745ef8afea2830285f93a0572a8e1d671241
Jan Six <six.jan@gmail.com>
2022-08-19 09:24:28 +0200

    fix style id detach

--- src/app/store/models/reducers/__tests__/assignStyleIdsToTheme.test.ts
+++ src/app/store/models/reducers/__tests__/assignStyleIdsToTheme.test.ts
@@ -30,4 +30,69 @@ describe('assignStyleIdsToTheme', () => {
       },
     ]);
   });
+
+  it('keeps previously stored styleIds intact', () => {
+    const mockState = {
+      themes: [
+        {
+          id: 'light',
+          name: 'Light',
+          selectedTokenSets: {},
+          $figmaStyleReferences: {
+            'colors.brand.primary': 'S:1234',
+          },
+        },
+      ],
+    } as unknown as TokenState;
+
+    expect(assignStyleIdsToTheme(mockState, {
+      id: 'light',
+      styleIds: {
+        'colors.brand.secondary': 'S:2345',
+      },
+    }).themes).toEqual([
+      {
+        id: 'light',
+        name: 'Light',
+        selectedTokenSets: {},
+        $figmaStyleReferences: {
+          'colors.brand.primary': 'S:1234',
+          'colors.brand.secondary': 'S:2345',
+        },
+      },
+    ]);
+  });
+
+  it('overwrites existing styleids', () => {
+    const mockState = {
+      themes: [
+        {
+          id: 'light',
+          name: 'Light',
+          selectedTokenSets: {},
+          $figmaStyleReferences: {
+            'colors.brand.primary': 'S:1234',
+          },
+        },
+      ],
+    } as unknown as TokenState;
+
+    expect(assignStyleIdsToTheme(mockState, {
+      id: 'light',
+      styleIds: {
+        'colors.brand.primary': 'S:9999',
+        'colors.brand.secondary': 'S:2345',
+      },
+    }).themes).toEqual([
+      {
+        id: 'light',
+        name: 'Light',
+        selectedTokenSets: {},
+        $figmaStyleReferences: {
+          'colors.brand.primary': 'S:9999',
+          'colors.brand.secondary': 'S:2345',
+        },
+      },
+    ]);
+  });
 });
