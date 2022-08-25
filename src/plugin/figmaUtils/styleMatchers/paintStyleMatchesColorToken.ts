246c7bdf7e97346d1ec1f44de7f099a8e6f4ed13
Jakob Engelbrecht <jakob@basher.dk>
2022-08-25 13:08:05 +0200

    feat: add matcher for gradient paint

--- src/plugin/figmaUtils/styleMatchers/paintStyleMatchesColorToken.ts
+++ src/plugin/figmaUtils/styleMatchers/paintStyleMatchesColorToken.ts
@@ -1,4 +1,5 @@
 import { isPaintEqual } from '@/utils/isPaintEqual';
+import { convertStringToFigmaGradient } from '../../figmaTransforms/gradients';
 import { convertToFigmaColor } from '../../figmaTransforms/colors';
 
 export function paintStyleMatchesColorToken(paintStyle: PaintStyle | undefined, colorToken: string) {
@@ -6,6 +7,15 @@ export function paintStyleMatchesColorToken(paintStyle: PaintStyle | undefined, 
   if (stylePaint?.type === 'SOLID') {
     const { color, opacity } = convertToFigmaColor(colorToken);
     const tokenPaint: SolidPaint = { color, opacity, type: 'SOLID' };
+    return isPaintEqual(stylePaint, tokenPaint);
+  }
+  if (stylePaint?.type === 'GRADIENT_LINEAR') {
+    const { gradientStops, gradientTransform } = convertStringToFigmaGradient(colorToken);
+    const tokenPaint: GradientPaint = {
+      type: 'GRADIENT_LINEAR',
+      gradientTransform,
+      gradientStops,
+    };
     return isPaintEqual(stylePaint, tokenPaint);
   }
   return false;
