657c639d66084f95c1f5a2c990a2744300e3349b
hiroshi <robinhoodie0823@gmail.com>
2022-09-01 19:27:31 +0900

    code refresh

--- src/utils/is/isMatchingStyle.ts
+++ src/utils/is/isMatchingStyle.ts
@@ -1,5 +1,9 @@
 import { DeleteTokenPayload } from '@/types/payloads';
 
 export function isMatchingStyle(style: PaintStyle | TextStyle | EffectStyle, token: DeleteTokenPayload) {
-  return token.path === style.name || token.path === style.name.substring(style.name.indexOf('.') + 1) || token.path.substring(token.path.indexOf('.') + 1) === style.name;
+  const normalizedPath = style.name.split('/').join('.');
+  if (token.path.includes('.')) {
+    return token.path === normalizedPath || token.path === normalizedPath.substring(normalizedPath.indexOf('.') + 1) || token.path.substring(token.path.indexOf('.') + 1) === normalizedPath;
+  }
+  return token.path === normalizedPath || token.path === normalizedPath.substring(normalizedPath.indexOf('.') + 1) || normalizedPath === '';
 }
