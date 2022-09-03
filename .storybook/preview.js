487932bbe072afc7fada7668e2197c83e809a08f
Jan Six <six7@github.com>
2022-09-03 10:59:41 +0200

    remove unused code and move colorpicker to own file

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,7 @@ import '../src/app/styles/main.css';
 import '../src/app/styles/figma.css';
 import '../src/app/styles/preflight.css';
 import '../src/app/styles/figma-bridge.css';
+import '../src/app/styles/vendor.css';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
