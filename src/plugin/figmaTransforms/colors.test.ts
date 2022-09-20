aa69eca53a24a02d4b0b929b3c59017701eef5bc
hiroshi <robinhoodie0823@gmail.com>
2022-09-20 14:26:31 +0900

    add missing test

--- src/plugin/figmaTransforms/colors.test.ts
+++ src/plugin/figmaTransforms/colors.test.ts
@@ -1,5 +1,5 @@
 import { convertToRgb } from '@/utils/color';
-import { convertToFigmaColor, hslaToRgba } from './colors';
+import { convertToFigmaColor, hslaToRgba, RGBAToHexA } from './colors';
 
 describe('hslaToRgba', () => {
   it('converts hsla to rgba', () => {
@@ -7,6 +7,10 @@ describe('hslaToRgba', () => {
     expect(hsl).toEqual([64, 128, 191, 1]);
     const hsla = hslaToRgba([210, 50, 50, 0.5]);
     expect(hsla).toEqual([64, 128, 191, 0.5]);
+    expect(hslaToRgba([75, 50, 50])).toEqual([159, 191, 64, 1]);
+    expect(hslaToRgba([157, 50, 50])).toEqual([64, 191, 142, 1]);
+    expect(hslaToRgba([248, 50, 50])).toEqual([81, 64, 191, 1]);
+    expect(hslaToRgba([311, 50, 50])).toEqual([191, 64, 168, 1]);
   });
 });
 
@@ -48,6 +52,15 @@ describe('convertToFigmaColor', () => {
       },
       opacity: 0.8,
     });
+    const hsl = 'hsl(0, 100%, 50%)';
+    expect(convertToFigmaColor(hsl)).toEqual({
+      color: {
+        r: 1,
+        g: 0,
+        b: 0,
+      },
+      opacity: 1,
+    });
   });
 
   it('converts named colors to figma readable', () => {
@@ -78,5 +91,17 @@ describe('round-trip from plugin to figma', () => {
       },
       opacity: 0.5,
     });
+  });
+});
+
+describe('rgbaToHexA', () => {
+  it('converts rgba to hex', () => {
+    const color = {
+      red: 67,
+      green: 235,
+      blue: 111,
+      alpha: 0.85,
+    };
+    expect(RGBAToHexA(color.red, color.green, color.blue, color.alpha)).toBe('#43eb6fd9');
   });
 });
