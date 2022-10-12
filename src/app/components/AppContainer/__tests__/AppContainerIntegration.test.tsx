759689e8e556f0d6e254c0248c8a5549bb96263b
MightyDevKnight <strongdragon036@gmail.com>
2022-10-12 23:36:25 +0900

    fix back again test

--- src/app/components/AppContainer/__tests__/AppContainerIntegration.test.tsx
+++ src/app/components/AppContainer/__tests__/AppContainerIntegration.test.tsx
@@ -207,7 +207,7 @@ describe('AppContainer (integration)', () => {
               <AppContainer {...params} />
             </Provider>,
           );
-          expect(await result.findByText('Get Started')).not.toBeUndefined();
+          expect(await result.findByText('Getting started')).not.toBeUndefined();
           result.unmount();
         });
       })
