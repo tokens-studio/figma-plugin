ec8bdd4eb4b1f5d1bc626aa353d6382a82f1fb61
MightyDevKnight <strongdragon036@gmail.com>
2022-10-14 23:05:18 +0900

    make test when user lanch the plugin again

--- src/utils/getLastOpened.test.ts
+++ src/utils/getLastOpened.test.ts
@@ -1,4 +1,5 @@
 import getLastOpened from './getLastOpened';
+import { mockGetAsync } from '../../tests/__mocks__/figmaMock';
 
 describe('fetchChangelog', () => {
   it('user lanch the plugin for the first time', async () => {
@@ -8,5 +9,10 @@ describe('fetchChangelog', () => {
     } catch (e) {
       expect(await getLastOpened()).toEqual(0);
     }
+  });
+
+  it('user lanch the plugin for not the first time', async () => {
+    mockGetAsync.mockResolvedValueOnce('1664078400000');
+    expect(await getLastOpened()).toEqual(1664078400000);
   });
 });
