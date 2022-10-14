704d059cb39abb8938c514b41a70be23c1fd6753
MightyDevKnight <strongdragon036@gmail.com>
2022-10-14 22:38:33 +0900

    add new feactures in getlastopened test

--- src/utils/getLastOpened.test.ts
+++ src/utils/getLastOpened.test.ts
@@ -2,6 +2,11 @@ import getLastOpened from './getLastOpened';
 
 describe('fetchChangelog', () => {
   it('user lanch the plugin for the first time', async () => {
-    expect(await getLastOpened()).toEqual(0);
+    expect.assertions(1);
+    try {
+      expect(await getLastOpened()).toEqual(0);
+    } catch (e) {
+      expect(await getLastOpened()).toEqual(0);
+    }
   });
 });
