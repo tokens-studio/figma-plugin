473929debdf06fddbe961231859e427f3f6d4277
MightyDevKnight <strongdragon036@gmail.com>
2022-10-25 23:31:52 +0900

    fix the navbar test

--- src/app/components/Navbar.test.tsx
+++ src/app/components/Navbar.test.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
-import { render } from '../../../tests/config/setupTest';
+import { Provider } from 'react-redux';
+import { render, resetStore, createMockStore } from '../../../tests/config/setupTest';
 import { store } from '../store';
 import Navbar from './Navbar';
 
 describe('ProBadge', () => {
-  it('displays get pro badge if user is on free plan', () => {
+  beforeEach(() => {
+    resetStore();
+  });
+
+  it('displays the buttons on navbar', () => {
     const { getByText } = render(<Navbar />, { store });
     expect(getByText('Tokens')).toBeInTheDocument();
     expect(getByText('Inspect')).toBeInTheDocument();
     expect(getByText('Settings')).toBeInTheDocument();
   });
 
-  it('displays pro badge if user is on pro plan', () => {
-    const { getByTestId } = render(<Navbar />, { store });
-    store.dispatch.userState.setLicenseKey('test-key-123');
+  it('displays the token flow button if user is on pro plan', () => {
+    const mockStore = createMockStore({
+      userState: {
+        licenseKey: 'FIGMA-TOKENS',
+        licenseDetails: {
+          plan: 'Pro Plan',
+          clientEmail: 'example@domain.com',
+          entitlements: ['pro'],
+        },
+      },
+    });
+    const result = render(
+      <Provider store={mockStore}>
+        <Navbar />
+      </Provider>,
+    );
 
-    const tokenFlowButton = getByTestId('token-flow-button');
+    const tokenFlowButton = result.getByTestId('token-flow-button');
 
     expect(tokenFlowButton).toBeInTheDocument();
-    expect(tokenFlowButton).not.toBeDisabled();
   });
 });
