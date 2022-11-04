3f3759d5cc0e7853a91bc7c1e62076bb0b77d69a
hiroshi <robinhoodie0823@gmail.com>
2022-11-04 18:27:16 +0900

    fix test coverage

--- src/utils/recoverOptimizedThemes.ts
+++ src/utils/recoverOptimizedThemes.ts
@@ -2,16 +2,19 @@ import { TokenSetStatus } from '@/constants/TokenSetStatus';
 import { ThemeObjectsList } from '@/types';
 import { AnyTokenList } from '@/types/tokens';
 
-export default function recoverOptimizedThemes(themes: ThemeObjectsList, tokens: Record<string, AnyTokenList>): ThemeObjectsList {
-  const allAvailableTokenSets = Object.keys(tokens);
-  return themes.map((theme) => {
-    const updatedSelectedTokenSets = Object.fromEntries(
-      allAvailableTokenSets
-        .map((tokenSet) => ([tokenSet, theme.selectedTokenSets?.[tokenSet] ?? TokenSetStatus.DISABLED])),
-    );
-    return {
-      ...theme,
-      selectedTokenSets: updatedSelectedTokenSets,
-    };
-  });
+export default function recoverOptimizedThemes(themes: ThemeObjectsList, tokens: Record<string, AnyTokenList> | null): ThemeObjectsList {
+  if (tokens) {
+    const allAvailableTokenSets = Object.keys(tokens);
+    return themes.map((theme) => {
+      const updatedSelectedTokenSets = Object.fromEntries(
+        allAvailableTokenSets
+          .map((tokenSet) => ([tokenSet, theme.selectedTokenSets?.[tokenSet] ?? TokenSetStatus.DISABLED])),
+      );
+      return {
+        ...theme,
+        selectedTokenSets: updatedSelectedTokenSets,
+      };
+    });
+  }
+  return themes;
 }
