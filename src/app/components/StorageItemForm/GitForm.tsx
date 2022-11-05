a4ff48ad6ff18a3077b2bc2fbcab594dd7245a29
Jan Six <six7@github.com>
2022-11-05 23:08:52 +0100

    fix sync forms

--- src/app/components/StorageItemForm/GitForm.tsx
+++ src/app/components/StorageItemForm/GitForm.tsx
@@ -51,7 +51,7 @@ export default function GitForm({
   return (
     <form onSubmit={handleSubmit}>
       <Stack direction="column" gap={4}>
-        <Input full label="Name" value={values.name} onChange={onChange} type="text" name="name" required />
+        <Input autofocus full label="Name" value={values.name} onChange={onChange} type="text" name="name" required />
         <Box css={{ position: 'relative' }}>
           <Input
             full
@@ -102,12 +102,12 @@ export default function GitForm({
           name="baseUrl"
         />
         <Stack direction="row" gap={4}>
-          <Button variant="secondary" size="large" onClick={onCancel}>
+          <Button variant="secondary" onClick={onCancel}>
             Cancel
           </Button>
 
           <Button variant="primary" type="submit" disabled={!values.secret && !values.name}>
-            Save
+            Save credentials
           </Button>
         </Stack>
         {hasErrored && (
