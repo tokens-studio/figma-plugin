9d59380dfa4c8586abc7386008dc5a5a71436930
hiroshi <hinatatadashi0823@gmail.com>
2022-11-22 10:35:38 +0900

    use cache for performance

--- src/plugin/sendDocumentChange.ts
+++ src/plugin/sendDocumentChange.ts
@@ -1,9 +1,9 @@
+import compact from 'just-compact';
 import { defaultNodeManager } from './NodeManager';
-import { findAll } from '@/utils/findAll';
 
 export async function sendDocumentChange(event: DocumentChangeEvent) {
-  const allNodes = findAll([figma.root], false);
   const changeNodeIds = event.documentChanges.filter((change) => change.type === 'PROPERTY_CHANGE').map((change) => change.id);
-  const nodes = allNodes.filter((node) => changeNodeIds.includes(node.id));
+  const nodeManagers = await Promise.all(changeNodeIds.map((nodeId) => defaultNodeManager.getNode(nodeId)));
+  const nodes = compact(nodeManagers).map((nodeManager) => nodeManager?.node);
   await defaultNodeManager.update(nodes);
 }
