8732daec59c3842b170a0c900fa2c4fc9d359c37
hiroshi <hinatatadashi0823@gmail.com>
2022-12-19 18:04:52 +0900

    validdate the dimension value when entering a button

--- src/app/components/EditTokenForm.tsx
+++ src/app/components/EditTokenForm.tsx
@@ -60,9 +60,6 @@ function EditTokenForm({ resolvedTokens }: Props) {
       && (internalEditToken.value.hasOwnProperty('') || Object.keys(internalEditToken.value).length === 0)) {
       return false;
     }
-    if (internalEditToken.type === TokenTypes.DIMENSION) {
-      return isValidDimensionToken;
-    }
     return internalEditToken?.value && !error;
   }, [internalEditToken, error]);
 
@@ -114,15 +111,6 @@ function EditTokenForm({ resolvedTokens }: Props) {
       e.persist();
       if (internalEditToken) {
         setInternalEditToken({ ...internalEditToken, [e.target.name]: e.target.value });
-      }
-    },
-    [internalEditToken],
-  );
-
-  const handleBlur = React.useCallback<React.ChangeEventHandler<HTMLInputElement>>(
-    () => {
-      if (internalEditToken.type === TokenTypes.DIMENSION && !isValidDimensionToken) {
-        setError('Value must include either px or rem');
       }
     },
     [internalEditToken],
@@ -340,6 +328,10 @@ function EditTokenForm({ resolvedTokens }: Props) {
   const handleSubmit = React.useCallback(
     (e: React.FormEvent<HTMLFormElement>) => {
       e.preventDefault();
+      if (internalEditToken.type === TokenTypes.DIMENSION && !isValidDimensionToken) {
+        setError('Value must include either px or rem');
+        return;
+      }
       if (isValid && internalEditToken) {
         submitTokenValue(internalEditToken);
         dispatch.uiState.setShowEditForm(false);
@@ -428,7 +420,6 @@ function EditTokenForm({ resolvedTokens }: Props) {
               resolvedTokens={resolvedTokens}
               initialName={internalEditToken.initialName}
               handleChange={handleChange}
-              handleBlur={handleBlur}
               setInputValue={handleDownShiftInputChange}
               placeholder={
                 internalEditToken.type === 'color' ? '#000000, hsla(), rgba() or {alias}' : 'Value or {alias}'
