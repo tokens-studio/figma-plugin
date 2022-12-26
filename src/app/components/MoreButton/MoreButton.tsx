3773b5db7dfea6b6429288b5e7795dd83028f561
Volker Chartier <volker.chartier@rwe.com>
2022-12-26 15:24:43 +0100

    add copy button to submenu, universal copypaste component

--- src/app/components/MoreButton/MoreButton.tsx
+++ src/app/components/MoreButton/MoreButton.tsx
@@ -1,15 +1,11 @@
 import React, { useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ChevronRightIcon } from '@radix-ui/react-icons';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
 import { styled } from '@/stitches.config';
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuTriggerItem,
-  ContextMenuSeparator,
-  ContextMenuTrigger,
-} from '../ContextMenu';
+
+import * as ContextMenu from '../ContextMenu';
 import { activeTokenSetSelector, editProhibitedSelector } from '@/selectors';
 import { PropertyObject } from '@/types/properties';
 import { MoreButtonProperty } from './MoreButtonProperty';
@@ -41,15 +37,11 @@ const RightSlot = styled('div', {
 
 type Props = {
   token: SingleToken;
-  type: TokenTypes,
+  type: TokenTypes;
   showForm: (options: ShowFormOptions) => void;
 };
 
-export const MoreButton: React.FC<Props> = ({
-  token,
-  type,
-  showForm,
-}) => {
+export const MoreButton: React.FC<Props> = ({ token, type, showForm }) => {
   const tokensContext = React.useContext(TokensContext);
   const setNodeData = useSetNodeData();
   const dispatch = useDispatch<Dispatch>();
@@ -57,16 +49,15 @@ export const MoreButton: React.FC<Props> = ({
   const activeTokenSet = useSelector(activeTokenSetSelector);
   const { deleteSingleToken } = useManageTokens();
 
-  const resolvedValue = useMemo(() => (
-    getAliasValue(token, tokensContext.resolvedTokens)
-  ), [token, tokensContext.resolvedTokens]);
+  const resolvedValue = useMemo(() => getAliasValue(token, tokensContext.resolvedTokens), [
+    token,
+    tokensContext.resolvedTokens,
+  ]);
 
   const properties = usePropertiesForTokenType(type, resolvedValue?.toString());
 
   // @TODO check type property typing
-  const visibleProperties = React.useMemo(() => (
-    properties.filter((p) => p.label)
-  ), [properties]);
+  const visibleProperties = React.useMemo(() => properties.filter((p) => p.label), [properties]);
 
   const handleEditClick = React.useCallback(() => {
     showForm({ name: token.name, token, status: EditTokenFormStatus.EDIT });
@@ -81,39 +72,43 @@ export const MoreButton: React.FC<Props> = ({
   }, [showForm, token]);
 
   // TODO: This should probably move to state or a hook
-  const setPluginValue = React.useCallback(async (value: SelectionValue) => {
-    dispatch.uiState.startJob({ name: BackgroundJobs.UI_APPLYNODEVALUE });
-    await setNodeData(value, tokensContext.resolvedTokens);
-    dispatch.uiState.completeJob(BackgroundJobs.UI_APPLYNODEVALUE);
-  }, [dispatch, tokensContext.resolvedTokens, setNodeData]);
+  const setPluginValue = React.useCallback(
+    async (value: SelectionValue) => {
+      dispatch.uiState.startJob({ name: BackgroundJobs.UI_APPLYNODEVALUE });
+      await setNodeData(value, tokensContext.resolvedTokens);
+      dispatch.uiState.completeJob(BackgroundJobs.UI_APPLYNODEVALUE);
+    },
+    [dispatch, tokensContext.resolvedTokens, setNodeData],
+  );
 
-  const activeStateProperties = React.useMemo(() => (
-    [...properties, ...DocumentationProperties]
-  ), [properties]);
+  const activeStateProperties = React.useMemo(() => [...properties, ...DocumentationProperties], [properties]);
   const active = useGetActiveState(activeStateProperties, type, token.name);
 
-  const handleClick = React.useCallback((givenProperties: PropertyObject, isActive = active) => {
-    track('Apply Token', { givenProperties });
-    const newProps: SelectionValue = {
-      [givenProperties.name]: isActive ? 'delete' : token.name,
-    };
-    if (givenProperties.clear) {
-      givenProperties.clear.map((item) => Object.assign(newProps, { [item]: 'delete' }));
-    }
+  const handleClick = React.useCallback(
+    (givenProperties: PropertyObject, isActive = active) => {
+      track('Apply Token', { givenProperties });
+      const newProps: SelectionValue = {
+        [givenProperties.name]: isActive ? 'delete' : token.name,
+      };
+      if (givenProperties.clear) {
+        givenProperties.clear.map((item) => Object.assign(newProps, { [item]: 'delete' }));
+      }
 
-    setPluginValue(newProps);
-  }, [active, token.name, setPluginValue]);
+      setPluginValue(newProps);
+    },
+    [active, token.name, setPluginValue],
+  );
 
   const handleTokenClick = React.useCallback(() => {
     handleClick(properties[0]);
   }, [properties, handleClick]);
 
   return (
-    <ContextMenu>
-      <ContextMenuTrigger id={`${token.name}-button}`}>
+    <ContextMenu.ContextMenu>
+      <ContextMenu.ContextMenuTrigger id={`${token.name}-button}`}>
         <TokenButtonContent type={type} active={active} onClick={handleTokenClick} token={token} />
-      </ContextMenuTrigger>
-      <ContextMenuContent sideOffset={5} collisionTolerance={30}>
+      </ContextMenu.ContextMenuTrigger>
+      <ContextMenu.ContextMenuContent sideOffset={5} collisionTolerance={30}>
         {visibleProperties.map((property) => (
           <MoreButtonProperty
             key={property.name}
@@ -123,36 +118,36 @@ export const MoreButton: React.FC<Props> = ({
             disabled={property.disabled}
           />
         ))}
-        <ContextMenu>
-          <ContextMenuTriggerItem>
+        <ContextMenu.ContextMenu>
+          <ContextMenu.ContextMenuTriggerItem>
             Documentation Tokens
             <RightSlot>
               <ChevronRightIcon />
             </RightSlot>
-          </ContextMenuTriggerItem>
-          <ContextMenuContent sideOffset={2} alignOffset={-5} collisionTolerance={30}>
+          </ContextMenu.ContextMenuTriggerItem>
+          <ContextMenu.ContextMenuContent sideOffset={2} alignOffset={-5} collisionTolerance={30}>
             {DocumentationProperties.map((property) => (
-              <MoreButtonProperty
-                key={property.name}
-                value={token.name}
-                property={property}
-                onClick={handleClick}
-              />
+              <MoreButtonProperty key={property.name} value={token.name} property={property} onClick={handleClick} />
             ))}
-          </ContextMenuContent>
-        </ContextMenu>
-        <ContextMenuSeparator />
+          </ContextMenu.ContextMenuContent>
+        </ContextMenu.ContextMenu>
+        <ContextMenu.ContextMenuSeparator />
 
-        <ContextMenuItem onSelect={handleEditClick} disabled={editProhibited}>
+        <ContextMenu.ContextMenuItem onSelect={handleEditClick} disabled={editProhibited}>
           Edit Token
-        </ContextMenuItem>
-        <ContextMenuItem onSelect={handleDuplicateClick} disabled={editProhibited}>
+        </ContextMenu.ContextMenuItem>
+        <ContextMenu.ContextMenuItem onSelect={handleDuplicateClick} disabled={editProhibited}>
           Duplicate Token
-        </ContextMenuItem>
-        <ContextMenuItem onSelect={handleDeleteClick} disabled={editProhibited}>
+        </ContextMenu.ContextMenuItem>
+        <ContextMenu.ContextMenuItem disabled={editProhibited}>
+          <CopyToClipboard text={token.name}>
+            <span>Copy token path</span>
+          </CopyToClipboard>
+        </ContextMenu.ContextMenuItem>
+        <ContextMenu.ContextMenuItem onSelect={handleDeleteClick} disabled={editProhibited}>
           Delete Token
-        </ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+        </ContextMenu.ContextMenuItem>
+      </ContextMenu.ContextMenuContent>
+    </ContextMenu.ContextMenu>
   );
 };
