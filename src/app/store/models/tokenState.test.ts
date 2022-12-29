0e3eb3d9755c1960eef5203d969855c372579907
hiroshi <hinatatadashi0823@gmail.com>
2022-12-29 17:21:36 +1000

    add tokenState test coverage

--- src/app/store/models/tokenState.test.ts
+++ src/app/store/models/tokenState.test.ts
@@ -570,6 +570,100 @@ describe('editToken', () => {
       parent: 'global',
       value: '1',
       type: 'sizing',
+      $extensions: {
+        'com.figmatokens': {
+          modify: {
+            type: 'lighten',
+            value: '0.5',
+            space: 'sRGB',
+          },
+        },
+      },
+    });
+    const { tokens } = store.getState().tokenState;
+    expect(tokens.global).toEqual([
+      {
+        name: 'primary',
+        value: '1',
+      },
+      {
+        name: 'primary-copy',
+        value: '1',
+        type: 'sizing',
+        $extensions: {
+          'com.figmatokens': {
+            modify: {
+              type: 'lighten',
+              value: '0.5',
+              space: 'sRGB',
+            },
+          },
+        },
+      },
+      {
+        name: 'alias',
+        value: '$primary',
+      },
+      {
+        name: 'primary50',
+        value: '0.50',
+      },
+      {
+        name: 'alias50',
+        value: '$primary50',
+      },
+      {
+        name: 'header 1',
+        type: 'typography',
+        value: {
+          fontWeight: '400',
+          fontSize: '16',
+        },
+      },
+      {
+        name: 'header 1',
+        type: 'typography',
+        value: {
+          fontWeight: '400',
+          fontSize: '16',
+        },
+      },
+      {
+        name: 'shadow.mixed',
+        type: 'boxShadow',
+        description: 'the one with mixed shadows',
+        value: shadowArray,
+      },
+      {
+        name: 'font.big',
+        type: 'sizing',
+        value: '24px',
+      },
+      {
+        name: 'font.small',
+        type: 'sizing',
+        value: '12px',
+      },
+      {
+        name: 'font.medium',
+        type: 'fontSizes',
+        value: '18px',
+      },
+      {
+        name: 'font.alias',
+        type: 'sizing',
+        value: '$font.small',
+      },
+    ]);
+  });
+
+  it('can duplicate token without extension', () => {
+    store.dispatch.tokenState.duplicateToken({
+      newName: 'primary-copy',
+      oldName: 'primary',
+      parent: 'global',
+      value: '1',
+      type: 'sizing',
     });
     const { tokens } = store.getState().tokenState;
     expect(tokens.global).toEqual([
@@ -964,5 +1058,45 @@ describe('editToken', () => {
     });
     const { activeTokenSet } = store.getState().tokenState;
     expect(activeTokenSet).toBe('global');
+  });
+
+  it('can set tokens', () => {
+    store.dispatch.tokenState.setTokens({
+      global: [
+        {
+          type: TokenTypes.COLOR,
+          value: '#ff0000',
+          name: 'red',
+        },
+        {
+          type: TokenTypes.COLOR,
+          value: '#000000',
+          name: 'black',
+        },
+        {
+          type: TokenTypes.BORDER_RADIUS,
+          value: '12px',
+          name: 'rounded.md',
+        },
+      ],
+    });
+    const { tokens } = store.getState().tokenState;
+    expect(tokens.global).toEqual([
+      {
+        type: TokenTypes.COLOR,
+        value: '#ff0000',
+        name: 'red',
+      },
+      {
+        type: TokenTypes.COLOR,
+        value: '#000000',
+        name: 'black',
+      },
+      {
+        type: TokenTypes.BORDER_RADIUS,
+        value: '12px',
+        name: 'rounded.md',
+      },
+    ]);
   });
 });
