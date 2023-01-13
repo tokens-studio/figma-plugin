bbd32c4ff6ec9623b164f724c565acb1cb03b213
razvanpop <rop_pazvan@yahoo.com>
2023-01-13 09:28:19 +0200

    revert to sets array

--- src/app/components/SecondScreenSync/index.tsx
+++ src/app/components/SecondScreenSync/index.tsx
@@ -6,7 +6,6 @@ import { useAuth } from '@/context/AuthContext';
 import { secondScreenSelector } from '@/selectors/secondScreenSelector';
 import supabase from '@/supabase';
 import { Dispatch } from '@/app/store';
-import stringifyTokens from '@/utils/stringifyTokens';
 
 export default function SecondScreenSync() {
   const secondScreenOn = useSelector(secondScreenSelector);
@@ -63,15 +62,8 @@ export default function SecondScreenSync() {
         .eq('owner_email', email);
     }
 
-    const setsObject: Record<any, any> = {};
-    Object.keys(tokens).forEach((setName) => {
-      const tokensString = stringifyTokens(tokens, setName);
-
-      setsObject[setName] = JSON.parse(tokensString);
-    });
-
     if (secondScreenOn && user) {
-      const data = JSON.stringify({ sets: setsObject, themes, usedTokenSets });
+      const data = JSON.stringify({ sets: tokens, themes, usedTokenSets });
       updateRemoteData(user.email, data);
     }
   }, [tokens, themes, secondScreenOn, user, usedTokenSets]);
