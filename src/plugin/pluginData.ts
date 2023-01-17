870bbca0d6feb95da8c55c500b0883e4d55cc140
hiroshi <hinatatadashi0823@gmail.com>
2023-01-17 09:23:30 +0900

    save Plugindata when set as none

--- src/plugin/pluginData.ts
+++ src/plugin/pluginData.ts
@@ -92,6 +92,17 @@ export async function removePluginData({ nodes, key, shouldRemoveValues = true }
   }));
 }
 
+export async function setNonePluginData({ nodes, key }: { nodes: readonly (BaseNode | SceneNode)[], key: Properties }) {
+  return Promise.all(nodes.map(async (node) => {
+    node.setPluginData(key, 'none');
+    await defaultNodeManager.updateNode(node, (tokens) => (
+      omit(tokens, key)
+    ));
+    removeValuesFromNode(node, key);
+    store.successfulNodes.add(node);
+  }));
+}
+
 export async function updatePluginData({
   entries, values, shouldOverride = false, shouldRemove = true, tokensMap,
 }: { entries: readonly NodeManagerNode[], values: NodeTokenRefMap, shouldOverride?: boolean, shouldRemove?: boolean, tokensMap?: Map<string, AnyTokenList[number]> }) {
@@ -142,7 +153,7 @@ export async function updatePluginData({
             await removePluginData({ nodes: [node], key: key as Properties, shouldRemoveValues: shouldRemove });
             break;
           case 'none':
-            await removeValuesFromNode(node, key as Properties);
+            await setNonePluginData({ nodes: [node], key: key as Properties });
             break;
           // Pre-Version 53 had horizontalPadding and verticalPadding.
           case 'horizontalPadding':
