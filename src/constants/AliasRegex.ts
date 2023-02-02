03a0c3793bca852cec6f580f46ead07afdd05f8f
hiroshi <hinatatadashi0823@gmail.com>
2023-02-02 22:22:58 +0900

    refresh code

--- src/constants/AliasRegex.ts
+++ src/constants/AliasRegex.ts
@@ -3,6 +3,3 @@ export const checkAliasStartRegex = /(\$[^\s,]+\w)|({([^]*))/g;
 
 // evaluates tokens such as $foo or {foo}
 export const AliasRegex = /(\$[^\s,]+\w)|({([^}]*)})/g;
-
-// evaluate whether string contains {
-export const checkAlias = /({([^]*))/g;
