0fef570e827c7a59626489c626a409d4e6aca383
hiroshi <hinatatadashi0823@gmail.com>
2023-03-07 23:33:56 +0900

    resolve regex

--- src/constants/AliasRegex.ts
+++ src/constants/AliasRegex.ts
@@ -4,4 +4,4 @@ export const checkAliasStartRegex = /(\$[^\s,]+\w)|({([^]*))/g;
 // evaluates tokens such as $foo or {foo}
 export const AliasRegex = /(?:\$([^\s,]+\w))|(?:{([^}]*)})/g;
 
-export const AliasDollarRegex = /(?:\$([^\s,]+\w))/g;
+export const AliasDollarRegex = /(?:\$(\w[\w\.\-]*))/g;
--- src/utils/findReferences.tsx
+++ src/utils/findReferences.tsx
@@ -66,7 +66,5 @@ export const getRootReferences = (tokenValue: string) => {
     }
   }
 
-  const tokenDollarReferences = findDollarReferences(tokenValue) ?? [];
-  array.push(...tokenDollarReferences);
-  return array;
+  return array.concat(findDollarReferences(tokenValue) || []);
 };
