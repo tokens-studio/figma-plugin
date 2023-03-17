5d30b11afabb5f97dec4b1d5e28fb40add68cbaf
hiroshi <hinatatadashi0823@gmail.com>
2023-03-17 18:56:00 +0900

    add setTextDecoration as none in remote ValuesFromNode

--- src/plugin/removeValuesFromNode.ts
+++ src/plugin/removeValuesFromNode.ts
@@ -147,7 +147,12 @@ export default function removeValuesFromNode(node: BaseNode, prop: Properties) {
         node.dashPattern = [0, 0];
       }
       break;
-
+    case 'textDecoration':
+      if ('textDecoration' in node && typeof node.textDecoration !== 'undefined') {
+        console.log('hehe');
+        node.textDecoration = 'NONE';
+      }
+      break;
     default:
       break;
   }
