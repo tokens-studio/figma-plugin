404c86085896c5f3475c0ddef88a8f69bccbb553
Jan Six <six7@github.com>
2023-03-19 17:42:32 +0100

    fix: inherit type level being exposed

--- src/utils/convertTokensToObject.ts
+++ src/utils/convertTokensToObject.ts
@@ -1,6 +1,7 @@
 import set from 'set-value';
 import { appendTypeToToken } from '@/app/components/createTokenObj';
 import { AnyTokenList, AnyTokenSet } from '@/types/tokens';
+import { getGroupTypeName } from './stringifyTokens';
 
 export default function convertTokensToObject(tokens: Record<string, AnyTokenList>) {
   const tokenObj = Object.entries(tokens).reduce<Record<string, AnyTokenSet<false>>>((acc, [key, val]) => {
@@ -8,7 +9,16 @@ export default function convertTokensToObject(tokens: Record<string, AnyTokenLis
     val.forEach((token) => {
       const tokenWithType = appendTypeToToken(token);
       const { name, ...tokenWithoutName } = tokenWithType;
-      set(tokenGroupObj, token.name, tokenWithoutName);
+      if (tokenWithoutName.inheritTypeLevel) {
+        const {
+          type, inheritTypeLevel, ...tokenWithoutType
+        } = tokenWithoutName;
+        // set type of group level
+        set(tokenGroupObj, getGroupTypeName(token.name, inheritTypeLevel), tokenWithoutName.type);
+        set(tokenGroupObj, token.name, tokenWithoutType);
+      } else {
+        set(tokenGroupObj, token.name, tokenWithoutName);
+      }
     });
     acc[key] = tokenGroupObj;
     return acc;
