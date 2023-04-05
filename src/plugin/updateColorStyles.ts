827ae8b299dc1ed3827edb70d07d098737acfff0
hiroshi <hinatatadashi0823@gmail.com>
2023-04-06 03:59:29 +0900

    update styles by styleId

--- src/plugin/updateColorStyles.ts
+++ src/plugin/updateColorStyles.ts
@@ -1,16 +1,16 @@
 import { SingleColorToken } from '@/types/tokens';
 import setColorValuesOnTarget from './setColorValuesOnTarget';
-import { getPaintStylesKeyMap } from '@/utils/getPaintStylesKeyMap';
+import { getPaintStylesIdMap } from '@/utils/getPaintStylesIdMap';
 
 // Iterate over colorTokens to create objects that match figma styles
 // @returns A map of token names and their respective style IDs (if created or found)
-export default function updateColorStyles(colorTokens: SingleColorToken<true, { path: string }>[], shouldCreate = false) {
-  const paintToKeyMap = getPaintStylesKeyMap();
+export default function updateColorStyles(colorTokens: SingleColorToken<true, { path: string, styleId: string }>[], shouldCreate = false) {
+  const paintToIdMap = getPaintStylesIdMap();
   const tokenToStyleMap: Record<string, string> = {};
 
   colorTokens.forEach((token) => {
-    if (paintToKeyMap.has(token.path)) {
-      const paint = paintToKeyMap.get(token.path)!;
+    if (paintToIdMap.has(token.styleId)) {
+      const paint = paintToIdMap.get(token.styleId)!;
       tokenToStyleMap[token.name] = paint.id;
       setColorValuesOnTarget(paint, token);
     } else if (shouldCreate) {
--- src/plugin/updateEffectStyles.ts
+++ src/plugin/updateEffectStyles.ts
@@ -1,16 +1,16 @@
 import { SingleBoxShadowToken } from '@/types/tokens';
-import { getEffectStylesKeyMap } from '@/utils/getEffectStylesKeyMap';
 import setEffectValuesOnTarget from './setEffectValuesOnTarget';
+import { getEffectStylesIdMap } from '@/utils/getEffectStylesIdMap';
 
 // Iterate over effectTokens to create objects that match figma styles
 // @returns A map of token names and their respective style IDs (if created or found)
-export default function updateEffectStyles(effectTokens: SingleBoxShadowToken<true, { path: string }>[], baseFontSize: string, shouldCreate = false) {
-  const effectStylesToKeyMap = getEffectStylesKeyMap();
+export default function updateEffectStyles(effectTokens: SingleBoxShadowToken<true, { path: string, styleId: string }>[], baseFontSize: string, shouldCreate = false) {
+  const effectStylesToIdMap = getEffectStylesIdMap();
   const tokenToStyleMap: Record<string, string> = {};
 
   effectTokens.forEach((token) => {
-    if (effectStylesToKeyMap.has(token.path)) {
-      const effectStyle = effectStylesToKeyMap.get(token.path)!;
+    if (effectStylesToIdMap.has(token.styleId)) {
+      const effectStyle = effectStylesToIdMap.get(token.styleId)!;
       tokenToStyleMap[token.name] = effectStyle.id;
       setEffectValuesOnTarget(effectStyle, token, baseFontSize);
     } else if (shouldCreate) {
