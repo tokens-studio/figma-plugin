8c517afad469d963c113956cfd5be19567cb7bce
hiroshi <hinatatadashi0823@gmail.com>
2023-04-10 12:23:04 +0900

    add filter to inspect tab

---
+++ src/app/components/InspectSearchOptionDropdown.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { CheckIcon, GearIcon } from '@radix-ui/react-icons';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItemIndicator,
+  DropdownMenuCheckboxItem,
+} from './DropdownMenu';
+import { inspectStateSelector } from '@/selectors';
+import { Dispatch } from '../store';
+import { isEqual } from '@/utils/isEqual';
+
+export default function InspectSearchOptionDropdown() {
+  const inspectState = useSelector(inspectStateSelector, isEqual);
+  const dispatch = useDispatch<Dispatch>();
+
+  const handleIsShowBrokenReferences = React.useCallback((checked: boolean) => {
+    dispatch.inspectState.toggleShowBrokenReferences(checked);
+  }, [dispatch.inspectState]);
+
+  const handleIsShowResolvedReferences = React.useCallback((checked: boolean) => {
+    dispatch.inspectState.toggleShowResolvedReferences(checked);
+  }, [dispatch.inspectState]);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger data-testid="inspect-search-option-dropdown">
+        <GearIcon />
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent side="left">
+        <DropdownMenuCheckboxItem
+          data-testid="show-broken-references"
+          checked={inspectState.isShowBrokenReferences}
+          onCheckedChange={handleIsShowBrokenReferences}
+        >
+          <DropdownMenuItemIndicator>
+            <CheckIcon />
+          </DropdownMenuItemIndicator>
+          Show broken references
+        </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          data-testid="show-resolved-references"
+          checked={inspectState.isShowResolvedReferences}
+          onCheckedChange={handleIsShowResolvedReferences}
+        >
+          <DropdownMenuItemIndicator>
+            <CheckIcon />
+          </DropdownMenuItemIndicator>
+          Show resolved references
+        </DropdownMenuCheckboxItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
