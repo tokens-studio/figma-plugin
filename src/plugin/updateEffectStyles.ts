fe0ceb81e82d6991354e41e265b38e24fa0602f4
hiroshi <hinatatadashi0823@gmail.com>
2023-04-12 03:22:15 +0900

    update styles by name and id

--- src/plugin/updateEffectStyles.ts
+++ src/plugin/updateEffectStyles.ts
@@ -1,16 +1,22 @@
 import { SingleBoxShadowToken } from '@/types/tokens';
 import setEffectValuesOnTarget from './setEffectValuesOnTarget';
 import { getEffectStylesIdMap } from '@/utils/getEffectStylesIdMap';
+import { getEffectStylesKeyMap } from '@/utils/getEffectStylesKeyMap';
 
 // Iterate over effectTokens to create objects that match figma styles
 // @returns A map of token names and their respective style IDs (if created or found)
 export default function updateEffectStyles(effectTokens: SingleBoxShadowToken<true, { path: string, styleId: string }>[], baseFontSize: string, shouldCreate = false) {
   const effectStylesToIdMap = getEffectStylesIdMap();
+  const effectStylesToKeyMap = getEffectStylesKeyMap();
   const tokenToStyleMap: Record<string, string> = {};
 
   effectTokens.forEach((token) => {
     if (effectStylesToIdMap.has(token.styleId)) {
       const effectStyle = effectStylesToIdMap.get(token.styleId)!;
+      tokenToStyleMap[token.name] = effectStyle.id;
+      setEffectValuesOnTarget(effectStyle, token, baseFontSize);
+    } else if (effectStylesToKeyMap.has(token.path)) {
+      const effectStyle = effectStylesToKeyMap.get(token.path)!;
       tokenToStyleMap[token.name] = effectStyle.id;
       setEffectValuesOnTarget(effectStyle, token, baseFontSize);
     } else if (shouldCreate) {
