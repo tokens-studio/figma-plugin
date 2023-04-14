0ceee389e3fe2bec34ba552831dbe1ba09c05bad
Sword Edge <hinatatadashi0823@gmail.com>
2023-04-15 05:48:04 +0900

    1559 ability to reorder composition token values (#1805)
    https://user-images.githubusercontent.com/25951419/231633239-1ba2730a-0cfc-43e1-835a-4b183f1b909c.mp4

--- src/app/components/CompositionTokenForm.tsx
+++ src/app/components/CompositionTokenForm.tsx
@@ -12,6 +12,9 @@ import { NodeTokenRefMap } from '@/types/NodeTokenRefMap';
 import { TokenTypes } from '@/constants/TokenTypes';
 import { filterValidCompositionTokenTypes } from '@/utils/filterValidCompositionTokenTypes';
 import { ResolveTokenValuesResult } from '@/plugin/tokenHelpers';
+import { ReorderGroup } from '@/motion/ReorderGroup';
+import { SingleCompositionTokenContent } from './SingleCompositionTokenContent';
+import { DragItem } from './StyledDragger/DragItem';
 
 export default function CompositionTokenForm({
   internalEditToken,
@@ -25,6 +28,7 @@ export default function CompositionTokenForm({
   const seed = useUIDSeed();
   const [orderObj, setOrderObj] = React.useState<NodeTokenRefMap>({});
   const [error, setError] = React.useState(false);
+
   const propertiesMenu = React.useMemo(() => (
     filterValidCompositionTokenTypes(Object.keys(Properties)).map((key: string) => (
       String(Properties[key as CompositionTokenProperty])
@@ -65,6 +69,18 @@ export default function CompositionTokenForm({
     setOrderObj(newOrderObj);
   }, []);
 
+  const handleReorder = React.useCallback((reorderedItems: string[]) => {
+    const newOrderObj: NodeTokenRefMap = {};
+    const internalEditTokenValue = internalEditToken.value || internalEditToken.schema.schemas.value.properties;
+    reorderedItems.forEach((key, index) => {
+      newOrderObj[key as keyof typeof Properties] = String(index);
+    });
+    const rearrangedTokenValue = Object.assign({}, ...Object.keys(internalEditTokenValue).sort((a, b) => Number(newOrderObj[a as CompositionTokenProperty]) - Number(newOrderObj[b as CompositionTokenProperty]))
+      .map((x) => ({ [x as CompositionTokenProperty]: internalEditTokenValue[x as CompositionTokenProperty] })));
+    setOrderObj(newOrderObj);
+    setTokenValue(rearrangedTokenValue as NodeTokenRefMap);
+  }, [internalEditToken, setTokenValue]);
+
   return (
     <div>
       <Box css={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
@@ -79,26 +95,11 @@ export default function CompositionTokenForm({
       </Box>
       <Box css={{ display: 'flex', flexDirection: 'column', gap: '$4' }}>
         {
-          Object.entries(arrangedTokenValue).length < 1 ? (
-            <SingleCompositionTokenForm
-              index={0}
-              property=""
-              propertyValue=""
-              tokenValue={arrangedTokenValue}
-              properties={propertiesMenu}
-              resolvedTokens={resolvedTokens}
-              setTokenValue={setTokenValue}
-              onRemove={removeToken}
-              setOrderObj={handleOrderObj}
-              setError={setError}
-            />
-          ) : (
-            Object.entries(arrangedTokenValue).map(([property, propertyValue], index) => (
+            Object.entries(arrangedTokenValue).length < 1 ? (
               <SingleCompositionTokenForm
-                key={`single-style-${seed(index)}`}
-                index={index}
-                property={property}
-                propertyValue={propertyValue}
+                index={0}
+                property=""
+                propertyValue=""
                 tokenValue={arrangedTokenValue}
                 properties={propertiesMenu}
                 resolvedTokens={resolvedTokens}
@@ -107,9 +108,34 @@ export default function CompositionTokenForm({
                 setOrderObj={handleOrderObj}
                 setError={setError}
               />
-            ))
-          )
-        }
+            ) : (
+              <ReorderGroup
+                layoutScroll
+                values={Object.keys(arrangedTokenValue)}
+                onReorder={handleReorder}
+              >
+                {
+                  Object.entries(arrangedTokenValue).map(([property, propertyValue], index) => (
+                    <DragItem<string> key={property} item={property}>
+                      <SingleCompositionTokenContent
+                        key={`single-style-${seed(index)}`}
+                        index={index}
+                        property={property}
+                        propertyValue={propertyValue}
+                        tokenValue={arrangedTokenValue}
+                        properties={propertiesMenu}
+                        resolvedTokens={resolvedTokens}
+                        setTokenValue={setTokenValue}
+                        onRemove={removeToken}
+                        setOrderObj={handleOrderObj}
+                        setError={setError}
+                      />
+                    </DragItem>
+                  ))
+                }
+              </ReorderGroup>
+            )
+          }
       </Box>
     </div>
   );
--- src/app/components/SingleCompositionTokenForm.tsx
+++ src/app/components/SingleCompositionTokenForm.tsx
@@ -83,56 +83,55 @@ export default function SingleCompositionTokenForm({
   }, [onRemove, property]);
 
   return (
-    <Box>
-      <Box css={{
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        gap: '$3',
-        '& > .relative ': {
-          flex: '2',
-        },
-      }}
-      >
-        <DropdownMenu open={menuOpened} onOpenChange={handleToggleMenu}>
-          <DropdownMenuTrigger
-            data-cy="composition-token-dropdown"
-            bordered
-            css={{
-              width: '130px', whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden', height: '$10',
-            }}
-          >
-            {property || 'Choose a property'}
-          </DropdownMenuTrigger>
-          <DropdownMenuContent sideOffset={2} className="content scroll-container" css={{ maxHeight: '$dropdownMaxHeight' }}>
-            {' '}
-            <DropdownMenuRadioGroup value={property}>
-              {properties.length > 0
+    <Box css={{
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      gap: '$3',
+      '& > .relative ': {
+        flex: '2',
+      },
+    }}
+    >
+      <DropdownMenu open={menuOpened} onOpenChange={handleToggleMenu}>
+        <DropdownMenuTrigger
+          data-cy="composition-token-dropdown"
+          bordered
+          css={{
+            width: '130px', whiteSpace: 'nowrap', textOverflow: 'ellipsis', overflow: 'hidden', height: '$10',
+          }}
+        >
+          {property || 'Choose a property'}
+        </DropdownMenuTrigger>
+        <DropdownMenuContent sideOffset={2} className="content scroll-container" css={{ maxHeight: '$dropdownMaxHeight' }}>
+          {' '}
+          <DropdownMenuRadioGroup value={property}>
+            {properties.length > 0
                 && properties.map((property, index) => <DropdownMenuRadioElement key={`property-${seed(index)}`} item={property} index={index} itemSelected={onPropertySelected} />)}
-            </DropdownMenuRadioGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-        <Box css={{ flexGrow: 1 }}>
-          <DownshiftInput
-            value={propertyValue}
-            type={propertyType}
-            resolvedTokens={resolvedTokens}
-            handleChange={onPropertyValueChanged}
-            setInputValue={handleDownShiftInputChange}
-            placeholder={
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <Box css={{ flexGrow: 1 }}>
+        <DownshiftInput
+          value={propertyValue}
+          type={propertyType}
+          resolvedTokens={resolvedTokens}
+          handleChange={onPropertyValueChanged}
+          setInputValue={handleDownShiftInputChange}
+          placeholder={
             propertyType === 'color' ? '#000000, hsla(), rgba() or {alias}' : 'Value or {alias}'
           }
-            suffix
-          />
-        </Box>
-        <Box css={{ width: '$5', marginRight: '$3' }}>
-          <IconButton
-            tooltip="Remove this style"
-            dataCy="button-style-remove-multiple"
-            onClick={handleRemove}
-            icon={<IconMinus />}
-          />
-        </Box>
+          suffix
+        />
+      </Box>
+      <Box css={{ width: '$5', marginRight: '$3' }}>
+        <IconButton
+          tooltip="Remove this style"
+          dataCy="button-style-remove-multiple"
+          onClick={handleRemove}
+          icon={<IconMinus />}
+        />
       </Box>
     </Box>
   );
