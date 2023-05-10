46c54d6621b0d2995fc0caba90e38cdeb2081c69
Sword Edge <hinatatadashi0823@gmail.com>
2023-05-11 04:58:02 +0900

    1803 allow to select alldetach all styles in manage themes (#1868)
    https://user-images.githubusercontent.com/25951419/236976372-aabee7cc-d21d-4dfb-8f89-1ac996da0f7d.mp4

--- src/app/store/models/reducers/tokenState/disconnectStyleFromTheme.ts
+++ src/app/store/models/reducers/tokenState/disconnectStyleFromTheme.ts
@@ -1,9 +1,8 @@
-import omit from 'just-omit';
 import type { TokenState } from '../../tokenState';
 
 type Payload = {
   id: string
-  key: string
+  key: string | string[]
 };
 
 export function disconnectStyleFromTheme(state: TokenState, data: Payload): TokenState {
@@ -12,13 +11,22 @@ export function disconnectStyleFromTheme(state: TokenState, data: Payload): Toke
   if (
     themeObjectIndex === -1
     || !state.themes[themeObjectIndex].$figmaStyleReferences
-    || !(data.key in state.themes[themeObjectIndex].$figmaStyleReferences!)
+    || (typeof data.key === 'string' && !(data.key in state.themes[themeObjectIndex].$figmaStyleReferences!))
+    || (Array.isArray(data.key) && !data.key.some((key) => (key in state.themes[themeObjectIndex].$figmaStyleReferences!)))
   ) return state;
 
   const updatedThemes = [...state.themes];
+  const updatedFigmaStyleReferences = state.themes[themeObjectIndex].$figmaStyleReferences ?? {};
+  if (typeof data.key === 'string') {
+    delete updatedFigmaStyleReferences[data.key];
+  } else {
+    data.key.forEach((key) => {
+      delete updatedFigmaStyleReferences[key];
+    });
+  }
   updatedThemes.splice(themeObjectIndex, 1, {
     ...state.themes[themeObjectIndex],
-    $figmaStyleReferences: omit(state.themes[themeObjectIndex].$figmaStyleReferences ?? {}, data.key),
+    $figmaStyleReferences: { ...updatedFigmaStyleReferences },
   });
 
   return {
