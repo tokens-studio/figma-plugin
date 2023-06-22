6b51869889c087c7af030a1584f271631f825f8d
hiroshi <hinatatadashi0823@gmail.com>
2023-06-22 20:59:29 +0900

    apply alpha value to variable

--- src/plugin/setColorValuesOnVariable.ts
+++ src/plugin/setColorValuesOnVariable.ts
@@ -2,8 +2,11 @@ import { convertToFigmaColor } from './figmaTransforms/colors';
 
 export default function setColorValuesOnVariable(variable: Variable, mode: string, value: string) {
   try {
-    const { color } = convertToFigmaColor(value);
-    variable.setValueForMode(mode, color);
+    const { color, opacity } = convertToFigmaColor(value);
+    variable.setValueForMode(mode, {
+      ...color,
+      a: opacity,
+    });
   } catch (e) {
     console.error('Error setting colorVariable', e);
   }
