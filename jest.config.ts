fd525dbcb00632f348f454cc9cc04e0bf428b611
SorsOps <80043879+sorsOps@users.noreply.github.com>
2023-07-19 11:43:34 +0200

    Add explicit coverage

--- jest.config.ts
+++ jest.config.ts
@@ -22,7 +22,7 @@ export default {
   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: ['src/**/*.[tj]s?(x)'],
 
   // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
