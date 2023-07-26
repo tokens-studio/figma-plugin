20610d90f66afbe1d2d9cf8dbff232f70ea48131
Jan Six <six.jan@gmail.com>
2023-07-26 10:04:46 +0200

    regenerate pr

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,67 +1,64 @@
-import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useTranslation } from 'react-i18next';
-import { Link1Icon, LinkBreak1Icon } from '@radix-ui/react-icons';
-import Box from './Box';
-import { Tabs } from '@/constants/Tabs';
-import Stack from './Stack';
-import { TabButton } from './TabButton';
-import { NavbarUndoButton } from './NavbarUndoButton';
-import Minimize from '@/icons/minimize.svg';
-import useMinimizeWindow from './useMinimizeWindow';
-import IconButton from './IconButton';
-import { activeTabSelector } from '@/selectors';
-import { Dispatch } from '../store';
-import TokenFlowButton from './TokenFlowButton';
-import { secondScreenSelector } from '@/selectors/secondScreenSelector';
-import { licenseKeySelector } from '@/selectors/licenseKeySelector';
-import { licenseKeyErrorSelector } from '@/selectors/licenseKeyErrorSelector';
+import React, { useMemo } from 'react';
+import { Dispatch } from '@/app/store';
 
-const Navbar: React.FC = () => {
-  const activeTab = useSelector(activeTabSelector);
-  const secondScreenisEnabled = useSelector(secondScreenSelector);
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from '../DropdownMenu';
+import { IconChevronDown } from '@/icons';
+import { languages } from '@/i18n';
+import { languageSelector } from '@/selectors';
+import { track } from '@/utils/analytics';
+
+export const LanguageSelector = () => {
   const dispatch = useDispatch<Dispatch>();
-  const { handleResize } = useMinimizeWindow();
-  const { t } = useTranslation(['navbar']);
-  const existingKey = useSelector(licenseKeySelector);
-  const licenseKeyError = useSelector(licenseKeyErrorSelector);
 
-  const handleSwitch = useCallback(
-    (tab: Tabs) => {
-      dispatch.uiState.setActiveTab(tab);
+  const currentLang = useSelector(languageSelector);
+  const displayLang = useMemo(() => languages.find((l) => l.code === currentLang)?.title, [currentLang]);
+
+  const handleValueChange = React.useCallback(
+    (value: string) => {
+      track('setLanguage', { value });
+      dispatch.settings.setLanguage(value);
     },
-    [dispatch.uiState],
+    [dispatch.settings],
   );
 
   return (
-    <Box
-      css={{
-        position: 'sticky',
-        top: 0,
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        background: '$bgDefault',
-        borderBottom: '1px solid $borderMuted',
-        zIndex: 1,
-        transform: 'translateY(-1px)',
-      }}
-    >
-      <Stack gap={0} direction="row" align="center" justify="between">
-        <Stack gap={0} direction="row" align="center" justify="start">
-          <TabButton name={Tabs.TOKENS} activeTab={activeTab} label={t('tokens')} onSwitch={handleSwitch} />
-          <TabButton name={Tabs.INSPECTOR} activeTab={activeTab} label={t('inspect')} onSwitch={handleSwitch} />
-          <TabButton name={Tabs.SETTINGS} activeTab={activeTab} label={t('settings')} onSwitch={handleSwitch} />
-        </Stack>
-        <NavbarUndoButton />
-      </Stack>
-      <Stack direction="row" align="center" justify="end" gap={1} css={{ paddingRight: '$2', flexBasis: 'min-content' }}>
-        { (existingKey && !licenseKeyError) && <TabButton endEnhancer={<Box css={{ color: secondScreenisEnabled ? '$fgSuccess' : '$dangerFg' }}>{secondScreenisEnabled ? <Link1Icon /> : <LinkBreak1Icon />}</Box>} name={Tabs.SECONDSCREEN} activeTab={activeTab} label={t('secondScreen')} onSwitch={handleSwitch} />}
-        <TokenFlowButton />
-        <IconButton size="large" tooltip={t('minimize') as string} onClick={handleResize} icon={<Minimize />} />
-      </Stack>
-    </Box>
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        css={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: '$textMuted',
+          '&:hover, &:focus-visible': { backgroundColor: '$bgSubtle' },
+        }}
+        data-testid="choose-language"
+      >
+        {displayLang}
+        <IconChevronDown />
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent side="top">
+        <DropdownMenuRadioGroup value={currentLang} onValueChange={handleValueChange}>
+          {languages.map((lang) => (
+
+            <DropdownMenuRadioItem
+              key={lang.code}
+              value={lang.code}
+              data-testid="apply-to-selection"
+            >
+              {lang.title}
+            </DropdownMenuRadioItem>
+          ))}
+
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
-
-export default Navbar;
