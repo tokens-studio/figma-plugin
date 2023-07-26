0c89865a73d481d7e50aaee226e2823f95dc213d
Jan Six <six.jan@gmail.com>
2023-07-26 10:11:30 +0200

    move condition

--- src/plugin/sendDocumentChange.ts
+++ src/plugin/sendDocumentChange.ts
@@ -3,6 +3,9 @@ import { defaultNodeManager } from './NodeManager';
 import { sendSelectionChange } from './sendSelectionChange';
 
 export async function sendDocumentChange(event: DocumentChangeEvent) {
+  if (event.documentChanges.length === 1 && event.documentChanges[0].type === 'PROPERTY_CHANGE' && event.documentChanges[0].id === '0:0') {
+    return;
+  }
   const changeNodeIds = event.documentChanges.filter((change) => change.origin === 'REMOTE' && change.type === 'PROPERTY_CHANGE').map((change) => change.id);
   if (!changeNodeIds.length) {
     return;
