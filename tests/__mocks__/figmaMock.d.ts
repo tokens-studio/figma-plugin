790caef3da8f92c8040fc7c0a47fc88a67111ff2
Jan Six <six.jan@gmail.com>
2023-07-30 11:19:16 +0200

    fix tests

--- tests/__mocks__/figmaMock.d.ts
+++ tests/__mocks__/figmaMock.d.ts
@@ -15,6 +15,7 @@ export const mockCreateEffectStyle: jest.Mock;
 export const mockGetStyleById: jest.Mock;
 export const mockImportStyleByKeyAsync: jest.Mock;
 export const mockUiOn: jest.Mock;
+export const mockCodegenOn: jest.Mock;
 export const mockRootSetSharedPluginData: jest.Mock;
 export const mockRootGetSharedPluginData: jest.Mock;
 export const mockGetNodeById: jest.Mock;
