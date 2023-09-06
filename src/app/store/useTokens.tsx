b91485c0b1222c07310829a87422fe50318995e8
hiroshi <hinatatadashi0823@gmail.com>
2023-09-06 20:36:52 +0900

    add renameVariableIdsToTheme test

--- src/app/store/useTokens.tsx
+++ src/app/store/useTokens.tsx
@@ -161,7 +161,6 @@ export default function useTokens() {
   }, []);
 
   const handleRemap = useCallback(async (type: Properties | TokenTypes, name: string, newTokenName: string, resolvedTokens: SingleToken[]) => {
-    const settings = settingsStateSelector(store.getState());
     track('remapToken', { fromInspect: true });
 
     wrapTransaction({ name: 'remapToken' }, async () => AsyncMessageChannel.ReactInstance.message({
@@ -173,7 +172,7 @@ export default function useTokens() {
       tokens: resolvedTokens,
       settings,
     }));
-  }, [confirm]);
+  }, [settings]);
 
   const handleBulkRemap = useCallback(async (newName: string, oldName: string, updateMode = UpdateMode.SELECTION) => {
     track('bulkRemapToken', { fromInspect: true });
