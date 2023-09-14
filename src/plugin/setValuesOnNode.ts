5d34bfd9ecd2280c717be6b827efe0349c391aa3
Jan Six <six.jan@gmail.com>
2023-09-14 08:36:56 +0200

    fixes min width causing apply to fail

--- src/plugin/setValuesOnNode.ts
+++ src/plugin/setValuesOnNode.ts
@@ -262,7 +262,7 @@ export default async function setValuesOnNode(
       }
 
       // min width, max width, min height, max height only are applicable to autolayout frames or their direct children
-      if (node.type !== 'DOCUMENT' && node.type !== 'PAGE' && (isAutoLayout(node) || (node.parent && node.parent.type !== 'DOCUMENT' && node.parent.type !== 'PAGE' && isAutoLayout(node.parent)))) {
+      if (node.type !== 'DOCUMENT' && node.type !== 'PAGE' && node.type !== 'INSTANCE' && (isAutoLayout(node) || (node.parent && node.parent.type !== 'DOCUMENT' && node.parent.type !== 'PAGE' && isAutoLayout(node.parent)))) {
         // SIZING: MIN WIDTH
         if ('minWidth' in node && typeof values.minWidth !== 'undefined' && typeof data.minWidth !== 'undefined' && isPrimitiveValue(values.minWidth)) {
           if (!(await tryApplyVariableId(node, 'minWidth', data.minWidth, figmaVariableReferences))) {
