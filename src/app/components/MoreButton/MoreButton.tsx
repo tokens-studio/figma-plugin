e636161db7fc33606a2d4d3d4ffb2116287246dc
hiroshi <robinhoodie0823@gmail.com>
2023-09-25 22:04:12 -0400

    fix: fix shortcut to edit when ctrl + click on windows

--- src/app/components/MoreButton/MoreButton.tsx
+++ src/app/components/MoreButton/MoreButton.tsx
@@ -122,7 +122,7 @@ export const MoreButton: React.FC<Props> = ({ token, type, showForm }) => {
   );
 
   const handleTokenClick = React.useCallback((event: React.MouseEvent<HTMLButtonElement>) => {
-    if (event.metaKey || (event.ctrlKey && event.altKey)) {
+    if (event.metaKey || event.ctrlKey) {
       handleEditClick();
     } else {
       handleClick(properties[0]);
