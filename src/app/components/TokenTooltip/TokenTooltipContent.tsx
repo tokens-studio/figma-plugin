6d2cd42747cd83c0e6691ce36a7edee4b33b73a7
hiroshi <robinhoodie0823@gmail.com>
2023-10-11 20:20:40 -0400

    clean console logs

--- src/app/components/TokenTooltip/TokenTooltipContent.tsx
+++ src/app/components/TokenTooltip/TokenTooltipContent.tsx
@@ -11,7 +11,6 @@ type Props = {
 };
 
 export const TokenTooltipContent: React.FC<Props> = ({ token }) => {
-  console.log('token in TokenToolTipContent: ', token);
   const tokensContext = React.useContext(TokensContext);
 
   const failedToResolve = React.useMemo(() => (
