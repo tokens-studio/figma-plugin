cb710778d3ed4688bcf8609d4a2efea918167b01
hiroshi <robinhoodie0823@gmail.com>
2023-10-19 05:33:23 -0400

    clean logs

--- src/plugin/createVariableMode.ts
+++ src/plugin/createVariableMode.ts
@@ -6,7 +6,6 @@ export default function createVariableMode(collection: VariableCollection, mode:
   } catch (e) {
     if (e instanceof Error) {
       const limit = /Limited to (\d) modes only/.exec(e.message);
-      console.log('limit in createVariableMode: ', limit);
       if (limit) {
         const isMoreThanOne = limit[1].length > 1;
         notifyUI(`Your Figma plan only allows creation of ${limit[1]} ${isMoreThanOne ? 'modes' : 'mode'}`, { error: true });
