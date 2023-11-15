708c8bae517c2c2a33035d6e19df1df7e5ca9c0f
Jan Six <six.jan@gmail.com>
2023-11-15 15:01:25 +0100

    fix transform ignore patterns

--- jest.config.ts
+++ jest.config.ts
@@ -176,7 +176,7 @@ export default {
   // transform: null,
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['/node_modules/(?!(@figma-plugin)/)', '\\.pnp\\.[^\\/]+$'],
+  transformIgnorePatterns: ['/node_modules/(?!(@figma-plugin|@supernovaio/supernova-sdk|react-colorful)/)', '\\.pnp\\.[^\\/]+$'],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
