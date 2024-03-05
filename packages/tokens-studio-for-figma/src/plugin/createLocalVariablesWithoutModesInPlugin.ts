3deb0ee0fbd13637c64dab307279f6dc645abfba
hiroshi <robinhoodie0823@gmail.com>
2024-03-05 08:09:19 -0400

    update function to create variables without modes

--- packages/tokens-studio-for-figma/src/plugin/createLocalVariablesWithoutModesInPlugin.ts
+++ packages/tokens-studio-for-figma/src/plugin/createLocalVariablesWithoutModesInPlugin.ts
@@ -7,6 +7,7 @@ import { ReferenceVariableType } from './setValuesOnVariable';
 import updateVariablesToReference from './updateVariablesToReference';
 import createVariableMode from './createVariableMode';
 import { notifyUI } from './notifiers';
+import { ThemeObject } from '@/types';
 
 export type LocalVariableInfo = {
   collectionId: string;
@@ -15,50 +16,34 @@ export type LocalVariableInfo = {
 };
 export default async function createLocalVariablesWithoutModesInPlugin(tokens: Record<string, AnyTokenList>, settings: SettingsState) {
   // Big O (n * m * x): (n: amount of themes, m: amount of variableCollections, x: amount of modes)
-  const themeInfo = await AsyncMessageChannel.PluginInstance.message({
-    type: AsyncMessageTypes.GET_THEME_INFO,
-  });
   const allVariableCollectionIds: Record<string, LocalVariableInfo> = {};
   let referenceVariableCandidates: ReferenceVariableType[] = [];
-  themeInfo.themes.forEach((theme) => {
-    const collection = figma.variables.getLocalVariableCollections().find((vr) => vr.name === (theme.group ?? theme.name));
+
+  Object.entries(tokens).forEach(([tokenSetKey, tokenList]) => {
+    const collection = figma.variables.getLocalVariableCollections().find((vr) => vr.name === tokenSetKey);
     if (collection) {
-      const mode = collection.modes.find((m) => m.name === theme.name);
-      const modeId: string = mode?.modeId ?? createVariableMode(collection, theme.name);
-      if (modeId) {
-        const allVariableObj = updateVariables({
-          collection, mode: modeId, theme, tokens, settings,
-        });
-        allVariableCollectionIds[theme.id] = {
-          collectionId: collection.id,
-          modeId,
-          variableIds: allVariableObj.variableIds,
-        };
-        referenceVariableCandidates = referenceVariableCandidates.concat(allVariableObj.referenceVariableCandidate);
-      }
+
     } else {
-      const newCollection = figma.variables.createVariableCollection(theme.group ?? theme.name);
-      newCollection.renameMode(newCollection.modes[0].modeId, theme.name);
+      const newCollection = figma.variables.createVariableCollection(tokenSetKey);
+      newCollection.renameMode(newCollection.modes[0].modeId, 'Default mode');
+      console.log('newCollection: ', newCollection);
       const allVariableObj = updateVariables({
-        collection: newCollection, mode: newCollection.modes[0].modeId, theme, tokens, settings,
+        collection: newCollection,
+        mode: newCollection.modes[0].modeId,
+        theme: {} as ThemeObject,
+        tokens,
+        settings
       });
-      allVariableCollectionIds[theme.id] = {
-        collectionId: newCollection.id,
-        modeId: newCollection.modes[0].modeId,
-        variableIds: allVariableObj.variableIds,
-      };
-      referenceVariableCandidates = referenceVariableCandidates.concat(allVariableObj.referenceVariableCandidate);
     }
   });
-  const figmaVariables = figma.variables.getLocalVariables();
-  updateVariablesToReference(figmaVariables, referenceVariableCandidates);
-  if (figmaVariables.length === 0) {
-    notifyUI('No variables were created');
-  } else {
-    notifyUI(`${figma.variables.getLocalVariableCollections().length} collections and ${figmaVariables.length} variables created`);
-  }
+
+  // if (figmaVariables.length === 0) {
+  //   notifyUI('No variables were created');
+  // } else {
+  //   notifyUI(`${figma.variables.getLocalVariableCollections().length} collections and ${figmaVariables.length} variables created`);
+  // }
   return {
     allVariableCollectionIds,
-    totalVariables: figmaVariables.length,
+    totalVariables: 0,
   };
 }
