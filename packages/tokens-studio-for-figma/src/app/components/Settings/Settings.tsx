f2ccd24c6c26e21178fd8846cce9f0b3f48014ef
Jan Six <six.jan@gmail.com>
2024-04-09 07:59:10 +0200

    fix privacy policy link

--- packages/tokens-studio-for-figma/src/app/components/Settings/Settings.tsx
+++ packages/tokens-studio-for-figma/src/app/components/Settings/Settings.tsx
@@ -12,7 +12,7 @@ import { LanguageSelector } from '../LanguageSelector';
 import { Dispatch } from '../../store';
 import {
   storeTokenIdInJsonEditorSelector,
-  uiStateSelector
+  uiStateSelector,
 } from '@/selectors';
 import AddLicenseKey from '../AddLicenseKey/AddLicenseKey';
 import { Divider } from '../Divider';
@@ -170,7 +170,7 @@ function Settings() {
                       {' '}
                       {t('forMoreInformationPleaseSeeOur')}
                       {' '}
-                      <Link href="https://tokens.studio/privacy">{t('privacyPolicy')}</Link>
+                      <Link href="https://tokens.studio/privacy" target="_blank">{t('privacyPolicy')}</Link>
                     </Box>
                   </ExplainerModal>
                   <Box
