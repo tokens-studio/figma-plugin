ea0ccee4c979429be3b009c8463c3eb1dc70c741
georgebuciuman <georgebuciuman@gmail.com>
2024-04-17 05:41:28 +0300

    feat: [819] Add support for themes and groups operations (#2633)
    ### Why does this PR exist?
    
    Closes https://github.com/tokens-studio/studio-app/issues/819
    
    ### What does this pull request do?
    
    Adds support for operations on themes and groups:
    - create theme group and theme
    - update theme groups and themes
    - delete theme groups and themes
    - add variable ids and styles ids to themes
    - update variable ids and styles ids for themes

---
+++ packages/tokens-studio-for-figma/src/storage/tokensStudio/updateThemeGroupsInTokensStudio/deleteTheme.ts
@@ -0,0 +1,30 @@
+import { ThemeObjectsList } from '@/types';
+import { getThemeGroupsToUpdate } from './getThemeGroupsToUpdate';
+
+type Props = {
+  action: any;
+  themes: ThemeObjectsList;
+  prevThemes: ThemeObjectsList;
+  groupIdsMap: Record<string, string>;
+};
+
+export const deleteTheme = ({ action, themes, prevThemes, groupIdsMap }: Props) => {
+  const { payload: themeUrn } = action;
+
+  let themeGroupsToUpdate: Record<string, ThemeObjectsList> = {};
+  let themeGroupsToDelete: string[] = [];
+
+  const themeGroupId = prevThemes.find((theme) => theme.id === themeUrn)?.groupId;
+
+  if (themeGroupId) {
+    const themesToUpdate = themes.filter(({ groupId }) => groupId === themeGroupId);
+
+    if (themesToUpdate.length) {
+      themeGroupsToUpdate = getThemeGroupsToUpdate(themesToUpdate, groupIdsMap);
+    } else {
+      themeGroupsToDelete = [themeGroupId];
+    }
+  }
+
+  return { themeGroupsToUpdate, themeGroupsToDelete };
+};
