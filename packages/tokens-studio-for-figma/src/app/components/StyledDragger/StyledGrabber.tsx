699bdb2a1dd0083d4bb0df1e7dab4f52b86fdb66
Celia Usero Navarro <c.usero.navarro@gmail.com>
2024-05-03 19:23:23 +0100

    fix(styled-grabber): add styling variant based on canReorder prop

--- packages/tokens-studio-for-figma/src/app/components/StyledDragger/StyledGrabber.tsx
+++ packages/tokens-studio-for-figma/src/app/components/StyledDragger/StyledGrabber.tsx
@@ -2,14 +2,23 @@ import Box from '../Box';
 import { styled } from '@/stitches.config';
 
 export const StyledGrabber = styled(Box, {
-  cursor: 'grab',
   display: 'flex',
   alignItems: 'center',
   width: '$4',
   height: '100%',
   color: '$fgSubtle',
   opacity: 0,
-  '&:hover': {
-    opacity: 1,
+  variants: {
+    canReorder: {
+      true: {
+        cursor: 'grab',
+        '&:hover': {
+          opacity: 1,
+        },
+      },
+      false: {
+        pointerEvents: 'none',
+      },
+    },
   },
 });
