1fab0e30a5eb03a88a8d462d0242fbd333ad2d4d
macintoshhelper <6757532+macintoshhelper@users.noreply.github.com>
2024-05-16 15:37:02 +0300

    prettify browser dev preview UI

--- packages/tokens-studio-for-figma/src/app/asyncMessageHandlers/preview.tsx
+++ packages/tokens-studio-for-figma/src/app/asyncMessageHandlers/preview.tsx
@@ -2,71 +2,74 @@ import React, {
   // useState, useEffect, useRef, useCallback,
   ReactNode,
 } from 'react';
-import { Box, Text } from '@tokens-studio/ui';
+import { Box, Stack, Text } from '@tokens-studio/ui';
 import { useStore } from 'react-redux';
 
-import { AsyncMessageChannel } from '@/AsyncMessageChannel';
 import { RootState } from '../store';
+import { AsyncMessageChannel } from '../../AsyncMessageChannel';
 // eslint-disable-next-line
-const PREVIEW_ENV = process.env.PREVIEW_ENV
+const PREVIEW_ENV = process.env.PREVIEW_ENV;
 
 function PreviewApp({ children }: { children: ReactNode }) {
   const isConnected = AsyncMessageChannel.ReactInstance.isWsConnected;
 
   const store = useStore<RootState>();
 
+  const previewHeader = (
+    <Stack
+      direction="row"
+      css={{
+        color: '$fgMuted',
+        backgroundColor: '$bgSurface',
+        padding: '$4',
+        borderRadius: '12px',
+        marginBottom: '40px',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <Text css={{ fontSize: '20px' }}>Web Preview</Text>
+      <Stack css={{ flex: 1 }}><span /></Stack>
+      <Stack
+        direction="row"
+        css={{
+          paddingTop: 4,
+          paddingBottom: 4,
+          paddingLeft: 8,
+          paddingRight: 8,
+          alignItems: 'center',
+          border: `1px solid ${isConnected ? '$successFg' : '$dangerFg'}`,
+          backgroundColor: isConnected ? '$successBg' : '$dangerBg',
+          borderRadius: '4px',
+        }}
+      >
+        <Text css={{ fontSize: '16px', color: isConnected ? '$successFg' : '$dangerFg' }}>
+          {isConnected ? 'Connected' : 'Disconnected'}
+        </Text>
+      </Stack>
+    </Stack>
+  );
+
   return (
     <Box css={{
-      justifyContent: 'center', alignItems: 'center', padding: '40px', backgroundColor: '$bgSubtle', height: '100vw',
+      justifyContent: 'center', alignItems: 'center', padding: '40px', backgroundColor: '$bgSubtle', height: '100vh',
     }}
     >
       {PREVIEW_ENV === 'browser' ? (
         <>
+          {previewHeader}
           <Box css={{
-            color: '$fgMuted',
-            backgroundColor: '$bgSurface',
-            padding: '$4',
-            borderRadius: '12px',
-            marginBottom: '40px',
-            justifyContent: 'center',
-            alignItems: 'center',
+            width: store?.getState()?.settings?.uiWindow?.width || '400px',
+            height: store?.getState()?.settings?.uiWindow?.height || '300px',
           }}
           >
-            <Box css={{ flexDirection: 'row' }}>
-              <Text css={{ fontSize: '20px' }}>
-                Connection Status:
-                <Text>
-                  {isConnected ? 'Connected' : 'Disconnected'}
-                </Text>
-              </Text>
-            </Box>
+            {children}
           </Box>
-          {isConnected && (
-            <Box css={{
-              width: store?.getState()?.settings?.uiWindow?.width || '400px',
-              height: store?.getState()?.settings?.uiWindow?.height || '300px',
-            }}
-            >
-              {children}
-            </Box>
-          )}
         </>
       ) : (
         <>
           {/* <span /> */}
-          <Box css={{
-            backgroundColor: '$bgSubtle', color: '$fgMuted', padding: '$4', borderRadius: '12px',
-          }}
-          >
-            <Box css={{ flexDirection: 'row' }}>
-              <Text css={{ fontSize: '20px' }}>
-                Connection Status:
-                <Text>
-                  {isConnected ? 'Connected' : 'Disconnected'}
-                </Text>
-              </Text>
-            </Box>
-          </Box>
+          {previewHeader}
         </>
       )}
     </Box>
