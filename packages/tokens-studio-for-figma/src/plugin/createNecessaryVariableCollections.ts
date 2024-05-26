98bbe78884adf3e6857989aec20efe382f65a132
Jan Six <six.jan@gmail.com>
2024-05-26 09:39:30 +0200

    update to use simpler reduce

--- packages/tokens-studio-for-figma/src/plugin/createNecessaryVariableCollections.ts
+++ packages/tokens-studio-for-figma/src/plugin/createNecessaryVariableCollections.ts
@@ -7,15 +7,19 @@ export async function createNecessaryVariableCollections(themes: ThemeObjectsLis
 
   const collectionsToCreateOrUpdate = themes.filter((theme) => selectedThemes.includes(theme.id));
 
-  const updatedCollections = collectionsToCreateOrUpdate.reduce((acc, currentTheme) => {
+  const updatedCollections = collectionsToCreateOrUpdate.reduce<VariableCollection[]>((acc, currentTheme) => {
     const nameOfCollection = currentTheme.group ?? currentTheme.name; // If there is a group, use that as the collection name, otherwise use the theme name (e.g. for when creating with sets we use the theme name)
-    const existingCollection = acc[nameOfCollection] || allCollections.find((vr) => vr.id === currentTheme.$figmaCollectionId || vr.name === nameOfCollection); // Check if we already have a collection with the same name, if not find one by the id of $themes or as a fallback by name
+    const existingCollection = acc.find((collection) => collection.name === nameOfCollection)
+    || allCollections.find((vr) => vr.id === currentTheme.$figmaCollectionId
+    || vr.name === nameOfCollection);
 
-    // If we found an existing collection, check if the mode exists, if not create it
     if (existingCollection) {
+      // Check if we already have a collection with the same name, if not find one by the id of $themes or as a fallback by name
+      // We do this because we might've found the collection by id, but the name might've changed
       if (existingCollection.name !== nameOfCollection) {
         existingCollection.name = nameOfCollection;
       }
+      // If we found an existing collection, check if the mode exists, if not create it
       const mode = existingCollection.modes.find((m) => m.modeId === currentTheme.$figmaModeId || m.name === currentTheme.name);
 
       if (mode) {
@@ -25,15 +29,15 @@ export async function createNecessaryVariableCollections(themes: ThemeObjectsLis
       } else {
         createVariableMode(existingCollection, currentTheme.name);
       }
-      acc[nameOfCollection] = existingCollection;
+      acc.push(existingCollection);
       return acc;
     }
     // If no existing collection is found, create a new one and rename the default mode
     const newCollection = figma.variables.createVariableCollection(nameOfCollection);
 
     newCollection.renameMode(newCollection.modes[0].modeId, currentTheme.name);
-    acc[nameOfCollection] = newCollection;
+    acc.push(newCollection);
     return acc;
-  }, {});
-  return Object.values(updatedCollections);
+  }, []);
+  return updatedCollections;
 }
