9b705a8ddc191a5cdc2be9ec8e54b82a9cdc8fc3
Akshay Gupta <gravity.akshay@gmail.com>
2024-06-10 15:26:56 +0530

    fix: box-shadow token input form color swatch to update on references

--- packages/tokens-studio-for-figma/src/app/components/SingleBoxShadowDownShiftInput.tsx
+++ packages/tokens-studio-for-figma/src/app/components/SingleBoxShadowDownShiftInput.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { ResolveTokenValuesResult } from '@/utils/tokenHelpers';
 import DownshiftInput from './DownshiftInput';
 import { ColorPickerTrigger } from './ColorPickerTrigger';
+import { getAliasValue } from '@/utils/alias';
 
 export default function SingleBoxShadowDownShiftInput({
   name,
@@ -14,17 +15,31 @@ export default function SingleBoxShadowDownShiftInput({
   handleToggleInputHelper,
   onSubmit,
 }: {
-  name: string,
+  name: string;
   value: string;
   type: string;
   resolvedTokens: ResolveTokenValuesResult[];
   handleChange: (property: string, value: string) => void;
   setInputValue: (newInputValue: string, property: string) => void;
   handleToggleInputHelper?: () => void;
-  onSubmit: () => void
+  onSubmit: () => void;
 }) {
   const { t } = useTranslation(['tokens']);
-  const handleBoxshadowDownShiftInputChange = React.useCallback((newInputValue: string) => setInputValue(newInputValue, name), [name, setInputValue]);
+  const [resolvedColor, setResolvedColor] = React.useState<string>(value ? String(value) : '');
+
+  React.useEffect(() => {
+    if (name === 'color' && value && value.startsWith('{')) {
+      const aliasValue = getAliasValue(value, resolvedTokens);
+      setResolvedColor(aliasValue ? String(aliasValue) : '');
+    } else {
+      setResolvedColor(typeof value === 'string' ? value : '');
+    }
+  }, [value, resolvedTokens, name]);
+
+  const handleBoxshadowDownShiftInputChange = React.useCallback(
+    (newInputValue: string) => setInputValue(newInputValue, name),
+    [name, setInputValue],
+  );
   return (
     <DownshiftInput
       name={name}
@@ -37,11 +52,9 @@ export default function SingleBoxShadowDownShiftInput({
       setInputValue={handleBoxshadowDownShiftInputChange}
       placeholder={name === 'color' ? t('colorOrAlias') : t('valueOrAlias')}
       prefix={
-        name === 'color' && value && (
-          <ColorPickerTrigger
-            background={value}
-            onClick={handleToggleInputHelper}
-          >
+        name === 'color' &&
+        resolvedColor && (
+          <ColorPickerTrigger background={resolvedColor} onClick={handleToggleInputHelper}>
             {value}
           </ColorPickerTrigger>
         )
