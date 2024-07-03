09b3428b1b2770cd30ccf2aa21ecb9a7d9d2ad5a
Celia Usero Navarro <c.usero.navarro@gmail.com>
2024-07-03 14:52:25 +0200

    review: change to background style attribute

--- packages/tokens-studio-for-figma/src/utils/color/getColorSwatchStyle.ts
+++ packages/tokens-studio-for-figma/src/utils/color/getColorSwatchStyle.ts
@@ -1,5 +1,5 @@
 export default function getColorSwatchStyle(tokenValue: string) {
   return tokenValue.includes(';')
     ? { background: tokenValue.replace(/;/g, '') }
-    : { backgroundColor: tokenValue };
+    : { background: tokenValue };
 }
