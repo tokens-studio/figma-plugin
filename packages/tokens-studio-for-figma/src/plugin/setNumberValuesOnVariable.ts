04ec1243dcbafb1de248645318dd11b7e523fae5
Celia Usero Navarro <c.usero.navarro@gmail.com>
2024-08-08 11:57:52 +0100

    chore: move error to throw inside try / catch

--- packages/tokens-studio-for-figma/src/plugin/setNumberValuesOnVariable.ts
+++ packages/tokens-studio-for-figma/src/plugin/setNumberValuesOnVariable.ts
@@ -1,10 +1,8 @@
 export default function setNumberValuesOnVariable(variable: Variable, mode: string, value: number) {
-  if (isNaN(value)) {
-    console.warn('Skipping setting numberVariable due to invalid value: NaN', value);
-    return;
-  }
-
   try {
+    if (isNaN(value)) {
+      throw new Error(`Skipping due to invalid value: ${value}`);
+    }
     variable.setValueForMode(mode, value);
   } catch (e) {
     console.error('Error setting numberVariable', e);
