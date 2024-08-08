d05c98274358637053f55c8c4ea806c2c36b7a41
Jan Six <six.jan@gmail.com>
2024-08-08 09:35:44 +0200

    fixes endless loader

--- packages/tokens-studio-for-figma/src/plugin/setNumberValuesOnVariable.ts
+++ packages/tokens-studio-for-figma/src/plugin/setNumberValuesOnVariable.ts
@@ -1,4 +1,9 @@
 export default function setNumberValuesOnVariable(variable: Variable, mode: string, value: number) {
+  if (isNaN(value)) {
+    console.warn('Skipping setting numberVariable due to invalid value: NaN', value);
+    return;
+  }
+
   try {
     variable.setValueForMode(mode, value);
   } catch (e) {
