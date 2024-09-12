6b871515a988e1f440b79a27fe25210f7b08015d
macintoshhelper <6757532+macintoshhelper@users.noreply.github.com>
2024-09-12 17:15:25 +0300

    fix(exports): sync provider package build fix

--- packages/sync-providers/tsup.config.ts
+++ packages/sync-providers/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/**/*.ts'],
   // entry: ['src/**/*.ts'],
   dts: true,
   bundle: true,
