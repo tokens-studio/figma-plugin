abac5ba6b0b8423e6ef0a4531f2c80699817e349
Akshay Gupta <gravity.akshay@gmail.com>
2024-10-10 14:11:44 +0530

    add a delay timeout when user clicks on Save

--- packages/tokens-studio-for-figma/src/app/components/StorageItemForm/TokensStudioForm.tsx
+++ packages/tokens-studio-for-figma/src/app/components/StorageItemForm/TokensStudioForm.tsx
@@ -35,6 +35,7 @@ export default function TokensStudioForm({
   const syncGuideUrl = 'tokens-studio';
   const [isMasked, setIsMasked] = React.useState(true);
   const [showTeaser, setShowTeaser] = React.useState(true);
+  const [isLoading, setIsLoading] = React.useState(false);
 
   const toggleMask = React.useCallback(() => {
     setIsMasked((prev) => !prev);
@@ -57,7 +58,13 @@ export default function TokensStudioForm({
           ...validationResult.data,
           internalId: validationResult.data.internalId || generateId(24),
         } as ValidatedFormValues;
+        setIsLoading(true); 
+
         onSubmit(formFields);
+
+        setTimeout(() => {
+          setIsLoading(false);
+        }, 5000);
       }
     },
     [values, onSubmit],
@@ -138,7 +145,7 @@ export default function TokensStudioForm({
           <Button variant="secondary" onClick={onCancel}>
             {t('cancel')}
           </Button>
-          <Button variant="primary" type="submit" disabled={!values.secret && !values.name}>
+          <Button variant="primary" type="submit" disabled={isLoading || (!values.secret && !values.name)}>
             {t('save')}
           </Button>
         </Stack>
