04f3fa67de8d7e72ee7459cee2ce3693be6232a8
Jan Six <six.jan@gmail.com>
2024-11-21 08:18:11 +0100

    Check variable value before update (#3188)
    * properly check variable values before updating
    
    * add changeset
    
    * fix tests

--- packages/tokens-studio-for-figma/src/plugin/setBooleanValuesOnVariable.ts
+++ packages/tokens-studio-for-figma/src/plugin/setBooleanValuesOnVariable.ts
@@ -1,9 +1,15 @@
 export default function setBooleanValuesOnVariable(variable: Variable, mode: string, value: string) {
   try {
-    if (value === 'true') {
-      variable.setValueForMode(mode, true);
-    } else {
-      variable.setValueForMode(mode, false);
+    const existingVariableValue = variable.valuesByMode[mode];
+    if (typeof existingVariableValue !== 'boolean') return;
+
+    const existingValueString = existingVariableValue.toString();
+    if (existingValueString !== value) {
+      if (value === 'true') {
+        variable.setValueForMode(mode, value === 'true');
+      } else {
+        variable.setValueForMode(mode, false);
+      }
     }
   } catch (e) {
     console.error('Error setting booleanVariable', e);
