cc9e5ea5c15c87cff943b0c8fde6dceefa84cfac
Akshay Gupta <gravity.akshay@gmail.com>
2025-02-11 01:04:06 +0530

    filter trigger only for other users

--- packages/tokens-studio-for-figma/src/plugin/sendDocumentChange.ts
+++ packages/tokens-studio-for-figma/src/plugin/sendDocumentChange.ts
@@ -2,22 +2,30 @@ import { sendSelectionChange } from './sendSelectionChange';
 import { ValuesProperty } from '@/figmaStorage/ValuesProperty';
 import { postToUI } from './notifiers';
 import { MessageFromPluginTypes } from '@/types/messages';
+import { CheckForChangesProperty } from '@/figmaStorage/CheckForChangesProperty';
 
 export async function sendDocumentChange(event: DocumentChangeEvent) {
-  const relevantChanges = event.documentChanges.filter((change) => change.type === 'PROPERTY_CHANGE' && change.properties?.includes('pluginData'));
-  // console.log('relevantChanges', relevantChanges);
+  const relevantChanges = event.documentChanges.filter((change) => change.type === 'PROPERTY_CHANGE'
+    && change.origin === 'REMOTE'
+    && change.properties?.includes('pluginData'));
 
-  if (relevantChanges.length > 0) {
+  if (relevantChanges.length === 0) return;
+
+  const hasChanges = await CheckForChangesProperty.read();
+
+  if (hasChanges && event.documentChanges[0].origin === 'REMOTE') {
     const sharedTokens = await ValuesProperty.read();
-    console.log('sharedTokens', sharedTokens?.global);
+    await CheckForChangesProperty.write(false);
+
     postToUI({
       type: MessageFromPluginTypes.SYNC_TOKENS,
-      tokens: sharedTokens?.global,
+      tokens: sharedTokens ?? {},
     });
   }
 
   if (event.documentChanges.length === 1 && event.documentChanges[0].type === 'PROPERTY_CHANGE' && event.documentChanges[0].id === '0:0') {
     return;
   }
+
   await sendSelectionChange();
 }
