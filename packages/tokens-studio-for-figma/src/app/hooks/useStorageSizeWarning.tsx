4fc8232fe5bb981bdc610a8cfb904daba21cd595
Akshay Gupta <gravity.akshay@gmail.com>
2025-04-16 23:38:59 +0530

    redirect to settings tab

--- packages/tokens-studio-for-figma/src/app/hooks/useStorageSizeWarning.tsx
+++ packages/tokens-studio-for-figma/src/app/hooks/useStorageSizeWarning.tsx
@@ -1,17 +1,22 @@
 import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import useConfirm from '@/app/hooks/useConfirm';
+import { Dispatch } from '@/app/store';
+import { Tabs } from '@/constants/Tabs';
 
-export function useStorageSizeWarning(tokensSize: number) {
+export function useStorageSizeWarning(tokensSize: number) { // TODO: customise message displayed based on the tokensSize received
   const { confirm } = useConfirm();
   const { t } = useTranslation(['tokens']);
-  return useCallback(() => {
-    if (tokensSize >= 90) {
-      confirm({
-        text: t('storageLimitWarning.title'),
-        description: t('storageLimitWarning.description'),
-        confirmAction: t('storageLimitWarning.switchToRemote'),
-      });
+  const dispatch = useDispatch<Dispatch>();
+  return useCallback(async () => {
+    const confirmed = await confirm({
+      text: t('storageLimitWarning.title'),
+      description: t('storageLimitWarning.description'),
+      confirmAction: t('storageLimitWarning.switchToRemote'),
+    });
+    if (confirmed) {
+      dispatch.uiState.setActiveTab(Tabs.SETTINGS);
     }
-  }, [confirm, tokensSize, t]);
+  }, [confirm, t, dispatch]);
 }
