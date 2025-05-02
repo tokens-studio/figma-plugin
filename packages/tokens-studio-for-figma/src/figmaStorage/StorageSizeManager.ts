f99bc0fe323910a0bd1186c938a54df60a54075c
Akshay Gupta <gravity.akshay@gmail.com>
2025-05-02 18:11:04 +0530

    check only for /tokens/ while fetching keys

--- packages/tokens-studio-for-figma/src/figmaStorage/StorageSizeManager.ts
+++ packages/tokens-studio-for-figma/src/figmaStorage/StorageSizeManager.ts
@@ -3,7 +3,7 @@ export const SAFE_STORAGE_LIMIT = 4 * 1024 * 1024; // 4MB in bytes
 export async function getStorageSize(): Promise<number> {
   const keys = await figma.clientStorage.keysAsync();
   // Get all keys that contain either tokens or themes
-  const storageKeys = keys.filter((key) => key.includes('tokens') || key.includes('themes'));
+  const storageKeys = keys.filter((key) => key.includes('/tokens/'));
   let totalSize = 0;
   for (const key of storageKeys) {
     const value = await figma.clientStorage.getAsync(key);
