fc5b7aab15b347f3e9f11c33bf6b6162a0640ca7
Akshay Gupta <gravity.akshay@gmail.com>
2025-05-05 11:56:42 +0530

    update text message display

--- packages/tokens-studio-for-figma/src/app/hooks/useStorageSizeWarning.tsx
+++ packages/tokens-studio-for-figma/src/app/hooks/useStorageSizeWarning.tsx
@@ -5,7 +5,7 @@ import useConfirm from '@/app/hooks/useConfirm';
 import { Dispatch } from '@/app/store';
 import { Tabs } from '@/constants/Tabs';
 
-export function useStorageSizeWarning() { // TODO: customise message displayed based on the tokensSize received
+export function useStorageSizeWarning() {
   const { confirm } = useConfirm();
   const { t } = useTranslation(['tokens']);
   const dispatch = useDispatch<Dispatch>();
