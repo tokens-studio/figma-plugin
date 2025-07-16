6150981b50705968faa0756b012a2568e5fe3ceb
Akshay Gupta <gravity.akshay@gmail.com>
2025-07-16 12:29:07 +0530

    revert border radius variable logic application

--- packages/tokens-studio-for-figma/src/plugin/applyBorderRadiusValuesOnNode.ts
+++ packages/tokens-studio-for-figma/src/plugin/applyBorderRadiusValuesOnNode.ts
@@ -23,7 +23,16 @@ export async function applyBorderRadiusValuesOnNode(
       switch (individualBorderRadius.length) {
         case 1:
           if ('cornerRadius' in node) {
-            node.cornerRadius = transformValue(String(values.borderRadius), 'borderRadius', baseFontSize);
+            if (
+              !(
+                (await tryApplyVariableId(node, 'topLeftRadius', data.borderRadius))
+                && (await tryApplyVariableId(node, 'topRightRadius', data.borderRadius))
+                && (await tryApplyVariableId(node, 'bottomRightRadius', data.borderRadius))
+                && (await tryApplyVariableId(node, 'bottomLeftRadius', data.borderRadius))
+              )
+            ) {
+              node.cornerRadius = transformValue(String(values.borderRadius), 'borderRadius', baseFontSize);
+            }
           }
           break;
         case 2:
