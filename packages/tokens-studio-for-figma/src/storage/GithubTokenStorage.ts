9207b7bac46c2e20970f4d1052026d38c88914a7
Akshay Gupta <gravity.akshay@gmail.com>
2025-08-01 22:51:49 +0530

    fix test in GithubTokenStorage

--- packages/tokens-studio-for-figma/src/storage/GithubTokenStorage.ts
+++ packages/tokens-studio-for-figma/src/storage/GithubTokenStorage.ts
@@ -202,12 +202,14 @@ export class GithubTokenStorage extends GitTokenStorage {
               path
               && fileContent?.data
               && !Array.isArray(fileContent?.data)
+              && typeof fileContent.data === 'string'
+              && IsJSONString(fileContent.data)
             ) {
               const filePath = path.startsWith(normalizedPath) ? path : `${normalizedPath}/${path}`;
               let name = filePath.substring(this.path.length).replace(/^\/+/, '');
               name = name.replace('.json', '');
 
-              const parsed = JSON.parse(fileContent.data as unknown as string) as GitMultiFileObject;
+              const parsed = JSON.parse(fileContent.data) as GitMultiFileObject;
               // @README we will need to ensure these reserved names
 
               if (name === SystemFilenames.THEMES) {
