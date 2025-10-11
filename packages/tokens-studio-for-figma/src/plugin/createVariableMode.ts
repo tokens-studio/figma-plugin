970dd2df0cc11496e675fa11b2cc2143f9b5b0a3
Jan Six <six.jan@gmail.com>
2025-10-11 10:55:50 +0200

    mode limit fix number

--- packages/tokens-studio-for-figma/src/plugin/createVariableMode.ts
+++ packages/tokens-studio-for-figma/src/plugin/createVariableMode.ts
@@ -8,7 +8,7 @@ export default function createVariableMode(collection: VariableCollection, mode:
     if (e instanceof Error) {
       const limit = /Limited to (\d) modes only/.exec(e.message);
       if (limit) {
-        const isMoreThanOne = limit[1].length > 1;
+        const isMoreThanOne = Number(limit[1]) > 1;
         notifyUI(`Your Figma plan only allows creation of ${limit[1]} ${isMoreThanOne ? 'modes' : 'mode'}`, { error: true });
       } else {
         console.error('Error', e.message, e.stack);
