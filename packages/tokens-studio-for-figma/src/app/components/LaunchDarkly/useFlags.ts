654ef74b282ab9e436f087bfa64705a770188c73
Akshay Gupta <gravity.akshay@gmail.com>
2025-10-21 12:29:33 +0400

    fix cypress test

--- packages/tokens-studio-for-figma/src/app/components/LaunchDarkly/useFlags.ts
+++ packages/tokens-studio-for-figma/src/app/components/LaunchDarkly/useFlags.ts
@@ -9,11 +9,21 @@ declare global {
 }
 
 export const useFlags = process.env.LAUNCHDARKLY_FLAGS
-  ? () => (
-    Object.fromEntries(process.env.LAUNCHDARKLY_FLAGS!.split(',').map((flag) => (
-      [flag, true]
-    )))
-  )
+  ? () => {
+    // Get flags from environment variable
+    const envFlags = Object.fromEntries(
+      process.env.LAUNCHDARKLY_FLAGS!.split(',').map((flag) => [flag, true]),
+    );
+
+    // Check if Cypress has set additional feature flags and merge them
+    // eslint-disable-next-line no-underscore-dangle
+    if (typeof window !== 'undefined' && window.__CYPRESS_FEATURE_FLAGS__) {
+      // eslint-disable-next-line no-underscore-dangle
+      return { ...envFlags, ...window.__CYPRESS_FEATURE_FLAGS__ };
+    }
+
+    return envFlags;
+  }
   : () => {
     // Check if Cypress has set feature flags
     // eslint-disable-next-line no-underscore-dangle
