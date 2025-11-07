30b661ea9028493c1edb75b7ca4a00e8bf671f63
Keegan Edwin <93133012+keeganedwin@users.noreply.github.com>
2025-11-07 11:45:25 +0530

    Update fontWeight.ts
    Replacing the values with what is documented in our https://docs.tokens.studio/manage-tokens/token-types/typography/font-weight#numeric-weights

--- packages/tokens-studio-for-figma/src/plugin/figmaTransforms/fontWeight.ts
+++ packages/tokens-studio-for-figma/src/plugin/figmaTransforms/fontWeight.ts
@@ -4,25 +4,28 @@ export function convertFontWeightToFigma(value: string, shouldOutputForVariables
   }
 
   switch (value) {
-    case '100':
-      return ['Thin', 'Hairline'];
-    case '200':
-      return ['ExtraLight', 'Extra Light', 'UltraLight', 'Ultra Light'];
-    case '300':
-      return ['Light'];
-    case '400':
-      return ['Regular', 'Normal'];
-    case '500':
-      return ['Medium'];
-    case '600':
-      return ['SemiBold', 'Semibold', 'Semi Bold', 'DemiBold', 'Demi Bold'];
-    case '700':
-      return ['Bold'];
-    case '800':
-      return ['ExtraBold', 'Extra Bold', 'UltraBold', 'Ultra Bold'];
-    case '900':
-      return ['Black', 'Heavy'];
-    default:
-      return [value];
-  }
+    case '100':
+      return ['Thin', 'Hairline'];
+    case '200':
+      return ['ExtraLight', 'Extra Light', 'UltraLight', 'Ultra Light', 'ultraleicht'];
+    case '300':
+      return ['Light', 'leicht'];
+    case '400':
+      return ['Regular', 'Normal', 'buch'];
+    case '500':
+      return ['Medium', 'kraeftig', 'kräftig'];
+    case '600':
+      return ['Semibold', 'SemiBold', 'Semi Bold', 'DemiBold', 'Demi Bold', 'halbfett'];
+    case '700':
+      return ['Bold', 'dreiviertelfett'];
+    case '800':
+      return ['ExtraBold', 'Extra Bold', 'UltraBold', 'Ultra Bold', 'fett'];
+    case '900':
+      return ['Black', 'Heavy', 'extrafett'];
+    case '950':
+      return ['Extra Black', 'Ultra Black'];
+    default:
+      return [value];
+  }
 }
+
