955b8472839753b5d73ae3368750b769a7f2a25a
Akshay Gupta <gravity.akshay@gmail.com>
2025-12-03 14:17:38 +0530

    remove user name to mix panel and hash user ids (#3697)
    * remove user name to mix panel and hash user ids
    
    * add changeset
    
    * add crypto browser package
    
    * remove user name from test
    
    * update analytics test
    
    * fix tests
    
    * update .env example file
    
    Signed-off-by: Akshay Gupta <gravity.akshay@gmail.com>
    
    * update .env example file
    
    Signed-off-by: Akshay Gupta <gravity.akshay@gmail.com>
    
    * remove changeset
    
    Signed-off-by: Akshay Gupta <gravity.akshay@gmail.com>
    
    ---------
    
    Signed-off-by: Akshay Gupta <gravity.akshay@gmail.com>

--- packages/tokens-studio-for-figma/src/utils/analytics.test.tsx
+++ packages/tokens-studio-for-figma/src/utils/analytics.test.tsx
@@ -5,6 +5,7 @@ import {
   mockTrack, mockInit, mockIdentify, mockPeopleSet,
 } from '../../tests/__mocks__/mixpanelMock';
 import pjs from '../../package.json';
+import crypto from 'crypto';
 
 describe('without mixpanel env', () => {
   it('should not track when mixpanel env isnt set', () => {
@@ -14,31 +15,35 @@ describe('without mixpanel env', () => {
     track('test', { data: 'foo' });
     identify({ userId: '123456', figmaId: 'figma-123', name: 'John Doe' });
     setUserData({ likes: 'Apples' });
-    expect(mockInit).not.toBeCalled();
-    expect(mockTrack).not.toBeCalled();
-    expect(mockIdentify).not.toBeCalled();
-    expect(mockPeopleSet).not.toBeCalled();
+    expect(mockInit).not.toHaveBeenCalled();
+    expect(mockTrack).not.toHaveBeenCalled();
+    expect(mockIdentify).not.toHaveBeenCalled();
+    expect(mockPeopleSet).not.toHaveBeenCalled();
   });
 });
 describe('with mixpanel env', () => {
+  const TEST_SECRET = 'test-secret-for-hashing';
   beforeAll(() => {
     process.env.MIXPANEL_ACCESS_TOKEN = '123';
+    process.env.FIGMA_ID_HASH_SECRET = TEST_SECRET;
   });
 
   describe('track', () => {
     it('should track when called', () => {
       track('test', { data: 'foo' });
-      expect(mockTrack).toBeCalledWith('test', { data: 'foo' });
+      expect(mockTrack).toHaveBeenCalledWith('test', { data: 'foo' });
     });
   });
   describe('identify', () => {
     it('should identify user', () => {
-      identify({ userId: '123456', figmaId: 'figma-123', name: 'John Doe' });
-      expect(mockIdentify).toBeCalledWith('figma-123');
-      expect(mockPeopleSet).toBeCalledWith({
-        USER_ID: '123456',
-        FIGMA_USER_ID: 'figma-123',
-        NAME: 'John Doe',
+      const expectedFigmaHash = crypto.createHmac('sha256', TEST_SECRET).update('figma-123').digest('hex');
+      const expectedUserIdHash = crypto.createHmac('sha256', TEST_SECRET).update('123456').digest('hex');
+
+      identify({ userId: '123456', figmaId: 'figma-123' });
+      expect(mockIdentify).toHaveBeenCalledWith(expectedFigmaHash);
+      expect(mockPeopleSet).toHaveBeenCalledWith({
+        USER_ID: expectedUserIdHash,
+        FIGMA_USER_ID: expectedFigmaHash,
         version: pjs.version,
       });
     });
@@ -46,13 +51,13 @@ describe('with mixpanel env', () => {
   describe('setUserData', () => {
     it('should append user data', () => {
       setUserData({ likes: 'Apples' });
-      expect(mockPeopleSet).toBeCalledWith({ likes: 'Apples' });
+      expect(mockPeopleSet).toHaveBeenCalledWith({ likes: 'Apples' });
     });
   });
   describe('initializeAnalytics', () => {
     it('should initialize mixpanel', () => {
       initializeAnalytics();
-      expect(mockInit).toBeCalled();
+      expect(mockInit).toHaveBeenCalled();
     });
   });
 });
