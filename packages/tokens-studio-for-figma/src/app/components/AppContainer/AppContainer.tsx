cb79537cde5a6185c044877d4302aeb298f05d39
Akshay Gupta <gravity.akshay@gmail.com>
2025-12-10 18:19:39 +0530

    remove unused import

--- packages/tokens-studio-for-figma/src/app/components/AppContainer/AppContainer.tsx
+++ packages/tokens-studio-for-figma/src/app/components/AppContainer/AppContainer.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import App from '../App';
 import FigmaLoading from '../FigmaLoading';
 import { AsyncMessageTypes, StartupMessage } from '@/types/AsyncMessages';
