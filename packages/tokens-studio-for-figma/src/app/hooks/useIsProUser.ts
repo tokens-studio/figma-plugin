5d45dc51c95dd304f1205cc7238914829362ae27
Akshay Gupta <gravity.akshay@gmail.com>
2026-03-04 20:10:53 +0530

    use storage type selector instead of activeapiprovider selector

--- packages/tokens-studio-for-figma/src/app/hooks/useIsProUser.ts
+++ packages/tokens-studio-for-figma/src/app/hooks/useIsProUser.ts
@@ -3,16 +3,16 @@ import { useMemo } from 'react';
 import { licenseKeySelector } from '@/selectors/licenseKeySelector';
 import { licenseKeyErrorSelector } from '@/selectors/licenseKeyErrorSelector';
 import { tokensStudioPATSelector } from '@/selectors/tokensStudioPATSelector';
-import { activeApiProviderSelector } from '@/selectors/activeApiProviderSelector';
+import { storageTypeSelector } from '@/selectors/storageTypeSelector';
 import { StorageProviderType } from '@/constants/StorageProviderType';
 
 export function useIsProUser() {
   const existingKey = useSelector(licenseKeySelector);
   const licenseKeyError = useSelector(licenseKeyErrorSelector);
   const validPAT = useSelector(tokensStudioPATSelector);
-  const activeProvider = useSelector(activeApiProviderSelector);
+  const storageType = useSelector(storageTypeSelector);
 
   return useMemo(() => (
-    Boolean(existingKey && !licenseKeyError) || Boolean(validPAT && activeProvider === StorageProviderType.TOKENS_STUDIO)
-  ), [existingKey, licenseKeyError, validPAT, activeProvider]);
+    Boolean(existingKey && !licenseKeyError) || Boolean(validPAT && storageType?.provider === StorageProviderType.TOKENS_STUDIO)
+  ), [existingKey, licenseKeyError, validPAT, storageType]);
 }
