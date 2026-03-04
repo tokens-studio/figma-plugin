4cc2b0f31b860da586c3c3ed9e234e95c440441f
Akshay Gupta <gravity.akshay@gmail.com>
2026-03-04 20:09:54 +0530

    initial commit

--- packages/tokens-studio-for-figma/src/app/hooks/useIsProUser.ts
+++ packages/tokens-studio-for-figma/src/app/hooks/useIsProUser.ts
@@ -3,13 +3,16 @@ import { useMemo } from 'react';
 import { licenseKeySelector } from '@/selectors/licenseKeySelector';
 import { licenseKeyErrorSelector } from '@/selectors/licenseKeyErrorSelector';
 import { tokensStudioPATSelector } from '@/selectors/tokensStudioPATSelector';
+import { activeApiProviderSelector } from '@/selectors/activeApiProviderSelector';
+import { StorageProviderType } from '@/constants/StorageProviderType';
 
 export function useIsProUser() {
   const existingKey = useSelector(licenseKeySelector);
   const licenseKeyError = useSelector(licenseKeyErrorSelector);
   const validPAT = useSelector(tokensStudioPATSelector);
+  const activeProvider = useSelector(activeApiProviderSelector);
 
   return useMemo(() => (
-    Boolean(existingKey && !licenseKeyError) || Boolean(validPAT)
-  ), [existingKey, licenseKeyError, validPAT]);
+    Boolean(existingKey && !licenseKeyError) || Boolean(validPAT && activeProvider === StorageProviderType.TOKENS_STUDIO)
+  ), [existingKey, licenseKeyError, validPAT, activeProvider]);
 }
