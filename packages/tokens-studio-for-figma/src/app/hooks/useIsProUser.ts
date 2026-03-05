56fe59b45b903bb04b8fc92f47e862b0606875ad
Akshay Gupta <gravity.akshay@gmail.com>
2026-03-05 16:27:11 +0530

    fix studio free trial sync users (#3796)
    * initial commit
    
    * use storage type selector instead of activeapiprovider selector

--- packages/tokens-studio-for-figma/src/app/hooks/useIsProUser.ts
+++ packages/tokens-studio-for-figma/src/app/hooks/useIsProUser.ts
@@ -3,13 +3,16 @@ import { useMemo } from 'react';
 import { licenseKeySelector } from '@/selectors/licenseKeySelector';
 import { licenseKeyErrorSelector } from '@/selectors/licenseKeyErrorSelector';
 import { tokensStudioPATSelector } from '@/selectors/tokensStudioPATSelector';
+import { storageTypeSelector } from '@/selectors/storageTypeSelector';
+import { StorageProviderType } from '@/constants/StorageProviderType';
 
 export function useIsProUser() {
   const existingKey = useSelector(licenseKeySelector);
   const licenseKeyError = useSelector(licenseKeyErrorSelector);
   const validPAT = useSelector(tokensStudioPATSelector);
+  const storageType = useSelector(storageTypeSelector);
 
   return useMemo(() => (
-    Boolean(existingKey && !licenseKeyError) || Boolean(validPAT)
-  ), [existingKey, licenseKeyError, validPAT]);
+    Boolean(existingKey && !licenseKeyError) || Boolean(validPAT && storageType?.provider === StorageProviderType.TOKENS_STUDIO)
+  ), [existingKey, licenseKeyError, validPAT, storageType]);
 }
