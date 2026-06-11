9ca3d5c0c80719e813ebb777f1fdf84bacfc1861
Akshay Gupta <gravity.akshay@gmail.com>
2026-06-11 13:24:26 +0530

    remove color modifier for studio users

--- packages/tokens-studio-for-figma/src/app/components/ColorTokenForm.tsx
+++ packages/tokens-studio-for-figma/src/app/components/ColorTokenForm.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { useUIDSeed } from 'react-uid';
 import { useTranslation } from 'react-i18next';
 import {
@@ -22,6 +23,8 @@ import { convertModifiedColorToHex } from '@/utils/convertModifiedColorToHex';
 import { ColorPickerTrigger } from './ColorPickerTrigger';
 import ProBadge from './ProBadge';
 import { useIsProUser } from '../hooks/useIsProUser';
+import { storageTypeSelector } from '@/selectors/storageTypeSelector';
+import { StorageProviderType } from '@/constants/StorageProviderType';
 
 const defaultValue = '0';
 
@@ -50,6 +53,8 @@ export default function ColorTokenForm({
   const [inputMixHelperOpen, setInputMixHelperOpen] = React.useState(false);
   const [modifyVisible, setModifyVisible] = React.useState(false);
   const isProUser = useIsProUser();
+  const storageType = useSelector(storageTypeSelector);
+  const isTokensStudioOAuth = storageType?.provider === StorageProviderType.TOKENS_STUDIO_OAUTH;
 
   React.useEffect(() => {
     if (internalEditToken?.$extensions?.['studio.tokens']?.modify) {
@@ -214,96 +219,100 @@ export default function ColorTokenForm({
       {inputHelperOpen && (
         <ColorPicker value={internalEditToken.value} onChange={handleColorValueChange} />
       )}
-      <Box css={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <Box css={{ display: 'flex', gap: '$3', alignItems: 'center' }}>
-          <Heading size="small">{t('modify')}</Heading>
-          <ProBadge campaign="modify-color" compact />
-        </Box>
-        {
-          !modifyVisible ? (
-            <IconButton
-              tooltip={t('addNewModifier')}
-              data-testid="button-add-new-modify"
-              onClick={addModify}
-              disabled={!isProUser}
-              icon={<IconPlus />}
-              variant="invisible"
-            />
-          ) : (
-            <IconButton
-              tooltip={t('removeModifier')}
-              data-testid="button-remove=modify"
-              onClick={removeModify}
-              disabled={!isProUser}
-              icon={<IconMinus />}
-              variant="invisible"
-            />
-          )
-        }
-      </Box>
-      {
-        modifyVisible && isProUser && (
-          <>
-            <Box css={{
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              gap: '$3',
-              '& > .relative ': {
-                flex: '2',
-              },
-            }}
-            >
-              <Select data-testid="colortokenform-operation-selector" value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.type || 'Choose an operation'} onValueChange={onOperationSelected}>
-                <Select.Trigger data-testid="colortokenform-operation-button" label="Operation" value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.type || 'Choose an operation'} />
-                <Select.Content>
-                  {Object.values(ColorModifierTypes).map((operation, index) => <Select.Item key={`operation-${seed(index)}`} value={operation}>{operation}</Select.Item>)}
-                </Select.Content>
-              </Select>
-              <Select data-testid="colortokenform-colorspace-selector" value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.space || 'Choose a color space'} onValueChange={onColorSpaceSelected}>
-                <Select.Trigger css={{ flexGrow: 1 }} label="Space" value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.space || 'Choose a color space'} />
-                <Select.Content>
-                  {Object.values(ColorSpaceTypes).map((colorSpace, index) => <Select.Item key={`colorspace-${seed(index)}`} value={colorSpace}>{colorSpace}</Select.Item>)}
-                </Select.Content>
-              </Select>
+      {!isTokensStudioOAuth && (
+        <>
+          <Box css={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+            <Box css={{ display: 'flex', gap: '$3', alignItems: 'center' }}>
+              <Heading size="small">{t('modify')}</Heading>
+              <ProBadge campaign="modify-color" compact />
             </Box>
             {
-              internalEditToken?.$extensions?.['studio.tokens']?.modify?.type === ColorModifierTypes.MIX && (
-                <>
-                  <DownshiftInput
-                    value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.color}
-                    type={TokenTypes.COLOR}
-                    resolvedTokens={resolvedTokens}
-                    handleChange={handleMixColorInputChange}
-                    setInputValue={handleMixColorChange}
-                    placeholder="#000000, hsla(), rgba() or {alias}"
-                    prefix={(
-                      <ColorPickerTrigger onClick={handleToggleMixInputHelper} background={String(resolvedMixValue)} />
-                    )}
-                    suffix
-                    onSubmit={onSubmit}
-                  />
-                  {inputMixHelperOpen && (
-                    <ColorPicker value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.color} onChange={handleMixColorChange} />
-                  )}
-                </>
+              !modifyVisible ? (
+                <IconButton
+                  tooltip={t('addNewModifier')}
+                  data-testid="button-add-new-modify"
+                  onClick={addModify}
+                  disabled={!isProUser}
+                  icon={<IconPlus />}
+                  variant="invisible"
+                />
+              ) : (
+                <IconButton
+                  tooltip={t('removeModifier')}
+                  data-testid="button-remove=modify"
+                  onClick={removeModify}
+                  disabled={!isProUser}
+                  icon={<IconMinus />}
+                  variant="invisible"
+                />
               )
             }
-            <DownshiftInput
-              value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.value}
-              type={TokenTypes.OTHER}
-              resolvedTokens={resolvedTokens}
-              handleChange={handleModifyValueChange}
-              setInputValue={handleModifyValueDownShiftInputChange}
-              placeholder={t('value0to1OrAlias')}
-              suffix
-              label={getLabel}
-              inlineLabel
-              onSubmit={onSubmit}
-            />
-          </>
-        )
-      }
+          </Box>
+          {
+            modifyVisible && isProUser && (
+              <>
+                <Box css={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  alignItems: 'center',
+                  gap: '$3',
+                  '& > .relative ': {
+                    flex: '2',
+                  },
+                }}
+                >
+                  <Select data-testid="colortokenform-operation-selector" value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.type || 'Choose an operation'} onValueChange={onOperationSelected}>
+                    <Select.Trigger data-testid="colortokenform-operation-button" label="Operation" value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.type || 'Choose an operation'} />
+                    <Select.Content>
+                      {Object.values(ColorModifierTypes).map((operation, index) => <Select.Item key={`operation-${seed(index)}`} value={operation}>{operation}</Select.Item>)}
+                    </Select.Content>
+                  </Select>
+                  <Select data-testid="colortokenform-colorspace-selector" value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.space || 'Choose a color space'} onValueChange={onColorSpaceSelected}>
+                    <Select.Trigger css={{ flexGrow: 1 }} label="Space" value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.space || 'Choose a color space'} />
+                    <Select.Content>
+                      {Object.values(ColorSpaceTypes).map((colorSpace, index) => <Select.Item key={`colorspace-${seed(index)}`} value={colorSpace}>{colorSpace}</Select.Item>)}
+                    </Select.Content>
+                  </Select>
+                </Box>
+                {
+                  internalEditToken?.$extensions?.['studio.tokens']?.modify?.type === ColorModifierTypes.MIX && (
+                    <>
+                      <DownshiftInput
+                        value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.color}
+                        type={TokenTypes.COLOR}
+                        resolvedTokens={resolvedTokens}
+                        handleChange={handleMixColorInputChange}
+                        setInputValue={handleMixColorChange}
+                        placeholder="#000000, hsla(), rgba() or {alias}"
+                        prefix={(
+                          <ColorPickerTrigger onClick={handleToggleMixInputHelper} background={String(resolvedMixValue)} />
+                        )}
+                        suffix
+                        onSubmit={onSubmit}
+                      />
+                      {inputMixHelperOpen && (
+                        <ColorPicker value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.color} onChange={handleMixColorChange} />
+                      )}
+                    </>
+                  )
+                }
+                <DownshiftInput
+                  value={internalEditToken?.$extensions?.['studio.tokens']?.modify?.value}
+                  type={TokenTypes.OTHER}
+                  resolvedTokens={resolvedTokens}
+                  handleChange={handleModifyValueChange}
+                  setInputValue={handleModifyValueDownShiftInputChange}
+                  placeholder={t('value0to1OrAlias')}
+                  suffix
+                  label={getLabel}
+                  inlineLabel
+                  onSubmit={onSubmit}
+                />
+              </>
+            )
+          }
+        </>
+      )}
       {(checkIfContainsAlias(internalEditToken.value) || internalEditToken?.$extensions?.['studio.tokens']?.modify) && (
         <Box css={{
           display: 'flex', gap: '$3', background: '$bgSubtle', color: '$fgMuted', padding: '$3', borderRadius: '$2',
