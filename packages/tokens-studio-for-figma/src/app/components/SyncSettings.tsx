31062fb3cda653ba7f44b291a6f140e322293a51
Akshay Gupta <gravity.akshay@gmail.com>
2026-07-01 11:47:58 +0530

    remive unused beta badge

--- packages/tokens-studio-for-figma/src/app/components/SyncSettings.tsx
+++ packages/tokens-studio-for-figma/src/app/components/SyncSettings.tsx
@@ -16,7 +16,6 @@ import useRemoteTokens from '../store/remoteTokens';
 import { StorageTypeCredentials } from '@/types/StorageType';
 import LocalStorageItem from './LocalStorageItem';
 import { getProviderIcon } from '@/utils/getProviderIcon';
-import { StyledBetaBadge } from './StyledBetaBadge';
 import { useAuthStore } from '@/app/store/useAuthStore';
 
 const SyncSettings = () => {
@@ -197,7 +196,6 @@ const SyncSettings = () => {
                             >
                               <Box css={{ color: '$fgMuted' }}>{getProviderIcon(provider.type)}</Box>
                               {provider.text}
-                              {provider.beta && <StyledBetaBadge>BETA</StyledBetaBadge>}
                             </Box>
                           </Stack>
                           <Button
